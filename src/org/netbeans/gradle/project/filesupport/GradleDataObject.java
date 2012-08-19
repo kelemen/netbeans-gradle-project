0aa48b49037ac554f60300184cd98d18a3568f53
Attila Kelemen <attila.kelemen85@gmail.com>
2012-08-20 00:43:38 +0200

    Added action references to gradle files.

--- src/org/netbeans/gradle/project/filesupport/GradleDataObject.java
+++ src/org/netbeans/gradle/project/filesupport/GradleDataObject.java
@@ -1,6 +1,9 @@
 package org.netbeans.gradle.project.filesupport;
 
 import java.io.IOException;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.MIMEResolver;
 import org.openide.loaders.DataNode;
@@ -21,6 +24,49 @@ import org.openide.text.DataEditorSupport;
         iconBase = "org/netbeans/gradle/project/resources/gradle.png",
         displayName = "#LBL_GradleFile",
         position = 300)
+@ActionReferences({
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.OpenAction"),
+        position = 100,
+        separatorAfter = 200),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id =@ActionID(category = "Edit", id = "org.openide.actions.CutAction"),
+        position = 300),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "Edit", id = "org.openide.actions.CopyAction"),
+        position = 400,
+        separatorAfter = 500),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "Edit", id = "org.openide.actions.DeleteAction"),
+        position = 600),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.RenameAction"),
+        position = 700,
+        separatorAfter = 800),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.SaveAsTemplateAction"),
+        position = 900,
+        separatorAfter = 1000),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.FileSystemAction"),
+        position = 1100,
+        separatorAfter = 1200),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.ToolsAction"),
+        position = 1300),
+    @ActionReference(
+        path = "Loaders/text/x-gradle/Actions",
+        id = @ActionID(category = "System", id = "org.openide.actions.PropertiesAction"),
+        position = 1400)
+})
 @SuppressWarnings("serial")
 public final class GradleDataObject extends MultiDataObject {
     public GradleDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
