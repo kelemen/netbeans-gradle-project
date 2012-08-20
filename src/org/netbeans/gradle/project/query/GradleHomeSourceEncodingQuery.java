cccbf1e49c6bcbde415a9d6052846af6e145bac5
Attila Kelemen <attila.kelemen85@gmail.com>
2012-08-21 00:17:13 +0200

    The sources of Gradle assumed to be UTF-8 encoded.

---
+++ src/org/netbeans/gradle/project/query/GradleHomeSourceEncodingQuery.java
@@ -0,0 +1,29 @@
+package org.netbeans.gradle.project.query;
+
+import java.nio.charset.Charset;
+import org.netbeans.gradle.project.properties.GradleOptionsPanelController;
+import org.netbeans.spi.queries.FileEncodingQueryImplementation;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.util.lookup.ServiceProviders;
+
+@ServiceProviders({@ServiceProvider(service = FileEncodingQueryImplementation.class)})
+public final class GradleHomeSourceEncodingQuery extends FileEncodingQueryImplementation {
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+
+    @Override
+    public Charset getEncoding(FileObject file) {
+        FileObject gradleHome = GradleOptionsPanelController.getGradleHomeFileObject();
+        if (gradleHome == null) {
+            return null;
+        }
+
+        FileObject srcDir = GradleFileUtils.getSrcDirOfGradle(gradleHome);
+        if (srcDir == null) {
+            return null;
+        }
+
+        return FileUtil.isParentOf(srcDir, file) ? UTF8 : null;
+    }
+}
