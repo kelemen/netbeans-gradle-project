95670862fe5c50658fb3e8487993cd3e8ac6dbd0
Attila Kelemen <attila.kelemen85@gmail.com>
2012-08-25 00:53:11 +0200

    The containing folder is displayed in the caption of the tab of an opened "build.gradle".

--- src/org/netbeans/gradle/project/filesupport/GradleDataObject.java
+++ src/org/netbeans/gradle/project/filesupport/GradleDataObject.java
@@ -1,10 +1,23 @@
 package org.netbeans.gradle.project.filesupport;
 
 import java.io.IOException;
+import org.netbeans.core.api.multiview.MultiViews;
+import org.netbeans.gradle.project.GradleProjectConstants;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
+import org.openide.cookies.CloseCookie;
+import org.openide.cookies.EditCookie;
+import org.openide.cookies.EditorCookie;
+import org.openide.cookies.OpenCookie;
+import org.openide.cookies.PrintCookie;
+import org.openide.cookies.SaveCookie;
+import org.openide.filesystems.FileChangeAdapter;
+import org.openide.filesystems.FileChangeListener;
+import org.openide.filesystems.FileEvent;
+import org.openide.filesystems.FileLock;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.filesystems.MIMEResolver;
 import org.openide.loaders.DataNode;
 import org.openide.loaders.DataObject;
@@ -13,11 +26,13 @@ import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.MultiFileLoader;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
+import org.openide.text.CloneableEditorSupport;
 import org.openide.text.DataEditorSupport;
+import org.openide.windows.CloneableOpenSupport;
 
 @MIMEResolver.ExtensionRegistration(
         displayName = "#LBL_GradleFile",
-        mimeType = "text/x-gradle",
+        mimeType = GradleDataObject.GRADLE_MIME_TYPE,
         extension = {"gradle", "Gradle", "GRADLE"})
 @DataObject.Registration(
         mimeType = "text/x-gradle",
@@ -69,10 +84,13 @@ import org.openide.text.DataEditorSupport;
 })
 @SuppressWarnings("serial")
 public final class GradleDataObject extends MultiDataObject {
-    public GradleDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
+    public static final String GRADLE_MIME_TYPE = "text/x-gradle";
+    private static final String GROOVY_MIME_TYPE = "text/x-groovy";
+
+    public GradleDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException {
         super(pf, loader);
-        registerEditor("text/x-gradle", false);
-        getLookup().lookup(DataEditorSupport.class).setMIMEType("text/x-groovy");
+
+        getCookieSet().add(new GradleDataEditor());
     }
 
     @Override
@@ -83,5 +101,121 @@ public final class GradleDataObject extends MultiDataObject {
     @Override
     protected Node createNodeDelegate() {
         return new DataNode(this, Children.LEAF, getLookup());
+    }
+
+    private class GradleDataEditor
+    extends
+            DataEditorSupport
+    implements
+            EditorCookie.Observable,
+            OpenCookie,
+            EditCookie,
+            PrintCookie,
+            CloseCookie {
+
+        private final SaveCookie save;
+        private final FileChangeListener listener;
+
+        public GradleDataEditor() {
+            super(GradleDataObject.this, null, new GradleEnv(GradleDataObject.this));
+
+            save = new SaveCookie() {
+                @Override
+                public void save() throws IOException {
+                    saveDocument();
+                }
+
+                @Override
+                public String toString() {
+                    return getPrimaryFile().getNameExt();
+                }
+            };
+            listener = new FileChangeAdapter() {
+                @Override
+                public void fileChanged(FileEvent fe) {
+                    updateTitles();
+                }
+            };
+
+            getPrimaryFile().addFileChangeListener(FileUtil.weakFileChangeListener(listener, getPrimaryFile()));
+            setMIMEType(GROOVY_MIME_TYPE);
+        }
+
+        @Override
+        protected Pane createPane() {
+            return (CloneableEditorSupport.Pane)MultiViews.createCloneableMultiView(GROOVY_MIME_TYPE, getDataObject());
+        }
+
+        @Override
+        protected boolean notifyModified() {
+            if (!super.notifyModified()) {
+                return false;
+            }
+            if (getLookup().lookup(SaveCookie.class) == null) {
+                getCookieSet().add(save);
+                setModified(true);
+            }
+            return true;
+        }
+
+        @Override
+        protected void notifyUnmodified() {
+            super.notifyUnmodified();
+            if (getLookup().lookup(SaveCookie.class) == save) {
+                getCookieSet().remove(save);
+                setModified(false);
+            }
+        }
+
+        @Override
+        protected String messageName() {
+            return annotateWithFolder(super.messageName());
+        }
+
+        @Override
+        protected String messageHtmlName() {
+            String name = super.messageHtmlName();
+            return name != null ? annotateWithFolder(name) : null;
+        }
+
+        private String annotateWithFolder(String name) {
+            if (GradleProjectConstants.BUILD_FILE_NAME.equals(getPrimaryFile().getNameExt())) {
+                FileObject parent = getPrimaryFile().getParent();
+                if (parent != null) {
+                    String folderName = parent.getNameExt();
+                    return name + " [" + folderName + "]";
+                }
+            }
+
+            return name;
+        }
+
+        @Override
+        protected boolean asynchronousOpen() {
+            return true;
+        }
+    }
+
+    private static class GradleEnv extends DataEditorSupport.Env {
+        private static final long serialVersionUID = 136529845402150749L;
+
+        public GradleEnv(MultiDataObject dataObject) {
+            super(dataObject);
+        }
+
+        @Override
+        protected FileObject getFile() {
+            return getDataObject().getPrimaryFile();
+        }
+
+        @Override
+        protected FileLock takeLock() throws IOException {
+            return ((MultiDataObject)getDataObject()).getPrimaryEntry().takeLock();
+        }
+
+        @Override
+        public CloneableOpenSupport findCloneableOpenSupport() {
+            return getDataObject().getLookup().lookup(GradleDataEditor.class);
+        }
     }
 }
