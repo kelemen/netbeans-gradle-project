ad1c3774493cc8630b57307a2ae24db8649c15d1
Attila Kelemen <attila.kelemen85@gmail.com>
2012-10-22 17:00:43 +0200

    The handling of the output is more fail-safe.

--- src/org/netbeans/gradle/project/output/SmartOutputHandler.java
+++ src/org/netbeans/gradle/project/output/SmartOutputHandler.java
@@ -2,9 +2,13 @@ package org.netbeans.gradle.project.output;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openide.windows.OutputWriter;
 
 public final class SmartOutputHandler implements LineOutputWriter.Handler {
+    private static final Logger LOGGER = Logger.getLogger(SmartOutputHandler.class.getName());
+
     public static interface Visitor {
         public void visitLine(String line);
     }
@@ -36,17 +40,47 @@ public final class SmartOutputHandler implements LineOutputWriter.Handler {
 
     @Override
     public void writeLine(String line) throws IOException {
-        for (Visitor visitor: visitors) {
-            visitor.visitLine(line);
-        }
+        Throwable error = null;
 
-        for (Consumer processor: processors) {
-            if (processor.tryConsumeLine(line, output)) {
-                return;
+        for (Visitor visitor: visitors) {
+            try {
+                visitor.visitLine(line);
+            } catch (Throwable ex) {
+                LOGGER.log(Level.SEVERE, "Unexpected failure while checking a line of the output.", error);
+                error = ex;
             }
         }
 
-        output.println(line);
+        for (Consumer processor: processors) {
+            try {
+                if (processor.tryConsumeLine(line, output)) {
+                    return;
+                }
+            } catch (Throwable ex) {
+                LOGGER.log(Level.SEVERE, "Unexpected failure while analysing a line of the output.", error);
+                error = ex;
+            }
+        }
+
+        try {
+            output.println(line);
+        } catch (Throwable ex) {
+            LOGGER.log(Level.SEVERE, "Unexpected failure while printing a line of the output.", error);
+            error = ex;
+        }
+
+        if (error != null) {
+            if (error instanceof IOException) {
+                throw (IOException)error;
+            }
+            if (error instanceof RuntimeException) {
+                throw (RuntimeException)error;
+            }
+            if (error instanceof Error) {
+                throw (Error)error;
+            }
+            throw new RuntimeException(error.getMessage(), error);
+        }
     }
 
     @Override
