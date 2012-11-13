kelemen/netbeans-gradle-project

commit d22d0b9c131a6a457edc47e94f2728d491adabfb

Minor refactoring.

src/org/netbeans/gradle/project/tasks/GradleTasks.java
@@ -11,7 +11,6 @@ import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -240,13 +239,7 @@ public final class GradleTasks {
                 final GradleTaskDef newTaskDef;
                 if (globalJvmArgs != null && !globalJvmArgs.isEmpty()) {
                     GradleTaskDef.Builder builder = new GradleTaskDef.Builder(taskDef);
-
-                    List<String> combinedJvmArgs = new ArrayList<String>(
-                            taskDef.getJvmArguments().size() + globalJvmArgs.size());
-                    combinedJvmArgs.addAll(taskDef.getJvmArguments());
-                    combinedJvmArgs.addAll(globalJvmArgs);
-                    builder.setJvmArguments(combinedJvmArgs);
-
+                    builder.addArguments(globalJvmArgs);
                     newTaskDef = builder.create();
                 }
                 else {

src/org/netbeans/gradle/project/tasks/GradleTaskDef.java
@@ -1,5 +1,6 @@
 package org.netbeans.gradle.project.tasks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -95,12 +96,32 @@ public final class GradleTaskDef {
             this.arguments = CollectionUtils.copyNullSafeList(arguments);
         }
 
+        private static <T> List<T> concatNullSafeLists(
+                List<? extends T> list1,
+                List<? extends T> list2) {
+            List<T> result = new ArrayList<T>(list1.size() + list2.size());
+            result.addAll(list1);
+            result.addAll(list2);
+            for (T element: result) {
+                if (element == null) throw new NullPointerException("element");
+            }
+            return result;
+        }
+
+        public void addArguments(List<String> toAdd) {
+            this.arguments = Collections.unmodifiableList(concatNullSafeLists(this.arguments, toAdd));
+        }
+
         public List<String> getJvmArguments() {
             return jvmArguments;
         }
 
         public void setJvmArguments(List<String> jvmArguments) {
             this.jvmArguments = CollectionUtils.copyNullSafeList(jvmArguments);
+        }
+
+        public void addJvmArguments(List<String> toAdd) {
+            this.jvmArguments = Collections.unmodifiableList(concatNullSafeLists(this.jvmArguments, toAdd));
         }
 
         public SmartOutputHandler.Visitor getStdOutListener() {

commit 855e35de399e81dcc66eb036b675d81407048d4b

Global JVM argument is no longer added to simple arguments. Bug introduced in d22d0b9c131a6a457edc47e94f2728d491adabfb.

src/org/netbeans/gradle/project/tasks/GradleTasks.java
@@ -239,7 +239,7 @@ public final class GradleTasks {
                 final GradleTaskDef newTaskDef;
                 if (globalJvmArgs != null && !globalJvmArgs.isEmpty()) {
                     GradleTaskDef.Builder builder = new GradleTaskDef.Builder(taskDef);
-                    builder.addArguments(globalJvmArgs);
+                    builder.addJvmArguments(globalJvmArgs);
                     newTaskDef = builder.create();
                 }
                 else {