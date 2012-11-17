kelemen/netbeans-gradle-project

commit 9906200452bb21fc88cd51b7911c7dc7d53c6862

An attempt to fix a possible dead-lock: #23

src/org/netbeans/gradle/project/persistent/XmlPropertiesPersister.java
@@ -30,25 +30,25 @@ public final class XmlPropertiesPersister implements PropertiesPersister {
         this.propertiesFile = propertiesFile;
     }
 
-    private void checkEDT() {
-        if (!SwingUtilities.isEventDispatchThread()) {
-            throw new IllegalStateException("This method may only be called from the EDT.");
+    private void checkCallingThread() {
+        if (!PropertiesPersister.PERSISTER_PROCESSOR.isRequestProcessorThread()) {
+            throw new IllegalStateException("This method may only be called from PropertiesPersister.PERSISTER_PROCESSOR.");
         }
     }
 
     @Override
     public void save(ProjectProperties properties, final Runnable onDone) {
-        checkEDT();
+        checkCallingThread();
 
         final PropertiesSnapshot snapshot = new PropertiesSnapshot(properties);
-        NbGradleProject.PROJECT_PROCESSOR.execute(new Runnable() {
+        PropertiesPersister.PERSISTER_PROCESSOR.execute(new Runnable() {
             @Override
             public void run() {
                 try {
                     XmlPropertyFormat.saveToXml(propertiesFile, snapshot);
                 } finally {
                     if (onDone != null) {
-                        onDone.run();
+                        SwingUtilities.invokeLater(onDone);
                     }
                 }
             }
@@ -63,7 +63,7 @@ public final class XmlPropertiesPersister implements PropertiesPersister {
 
     @Override
     public void load(final ProjectProperties properties, final Runnable onDone) {
-        checkEDT();
+        checkCallingThread();
 
         // We must listen for changes, so that we do not overwrite properties
         // modified later.

src/org/netbeans/gradle/project/persistent/PropertiesPersister.java
@@ -1,9 +1,12 @@
 package org.netbeans.gradle.project.persistent;
 
 import org.netbeans.gradle.project.properties.ProjectProperties;
+import org.openide.util.RequestProcessor;
 
 public interface PropertiesPersister {
-    // These methods may only be called from the EDT
+    // These methods may only be called from this processor
+    public static final RequestProcessor PERSISTER_PROCESSOR
+            = new RequestProcessor("Gradle-properties-persister", 1, true);
 
     public void save(ProjectProperties properties, Runnable onDone);
     public void load(ProjectProperties properties, Runnable onDone);

src/org/netbeans/gradle/project/properties/ProjectPropertiesManager.java
@@ -33,7 +33,7 @@ public final class ProjectPropertiesManager {
             final PropertiesPersister persister) {
 
         if (saveQueued.compareAndSet(false, true)) {
-            SwingUtilities.invokeLater(new Runnable() {
+            PropertiesPersister.PERSISTER_PROCESSOR.execute(new Runnable() {
                 @Override
                 public void run() {
                     saveQueued.set(false);
@@ -210,7 +210,7 @@ public final class ProjectPropertiesManager {
         final ProjectProperties properties = new MemProjectProperties();
         final PropertiesPersister persister = new XmlPropertiesPersister(propertiesFile);
 
-        SwingUtilities.invokeLater(new Runnable() {
+        PropertiesPersister.PERSISTER_PROCESSOR.execute(new Runnable() {
             @Override
             public void run() {
                 persister.load(properties, new Runnable() {

src/org/netbeans/gradle/project/properties/ProjectPropertiesProxy.java
@@ -18,6 +18,7 @@ import javax.swing.event.ChangeListener;
 import org.netbeans.api.java.platform.JavaPlatform;
 import org.netbeans.gradle.project.NbGradleProject;
 import org.netbeans.gradle.project.WaitableSignal;
+import org.netbeans.gradle.project.persistent.PropertiesPersister;
 import org.openide.util.ChangeSupport;
 
 public final class ProjectPropertiesProxy extends AbstractProjectProperties {
@@ -84,6 +85,15 @@ public final class ProjectPropertiesProxy extends AbstractProjectProperties {
     }
 
     public boolean tryWaitForLoaded() {
+        // Attempting to call this method from any of the threads below could
+        // cause a dead-lock.
+        if (NbGradleProject.PROJECT_PROCESSOR.isRequestProcessorThread()) {
+            throw new IllegalStateException("This method cannot be access from the PROJECT_PROCESSOR.");
+        }
+        if (PropertiesPersister.PERSISTER_PROCESSOR.isRequestProcessorThread()) {
+            throw new IllegalStateException("This method cannot be access from the PERSISTER_PROCESSOR.");
+        }
+
         getProperties();
         return loadedSignal.tryWaitForSignal();
     }

src/org/netbeans/gradle/project/query/GradleSourceEncodingQuery.java
@@ -1,6 +1,8 @@
 package org.netbeans.gradle.project.query;
 
 import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.netbeans.gradle.project.NbGradleProject;
 import org.netbeans.gradle.project.properties.ProjectProperties;
 import org.netbeans.spi.queries.FileEncodingQueryImplementation;
@@ -8,6 +10,8 @@ import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 
 public final class GradleSourceEncodingQuery extends FileEncodingQueryImplementation {
+    private static final Logger LOGGER = Logger.getLogger(GradleSourceEncodingQuery.class.getName());
+
     private final NbGradleProject project;
 
     public GradleSourceEncodingQuery(NbGradleProject project) {
@@ -18,10 +22,19 @@ public final class GradleSourceEncodingQuery extends FileEncodingQueryImplementa
     @Override
     public Charset getEncoding(FileObject file) {
         if (FileUtil.isParentOf(project.getProjectDirectory(), file)) {
-            ProjectProperties properties = project.tryGetLoadedProperties();
-            return properties != null
-                    ? properties.getSourceEncoding().getValue()
-                    : null;
+            try {
+                ProjectProperties properties = project.tryGetLoadedProperties();
+                if (properties != null) {
+                    return properties.getSourceEncoding().getValue();
+                }
+            } catch (IllegalStateException ex) {
+                LOGGER.log(Level.INFO, "The character encoding of a file has been requested from a wrong thread.", ex);
+            }
+
+            // We don't expect that anyone else can tell what the encoding of
+            // the file is, so return what we have (probably "UTF-8") is more
+            // reliable than the platform's default encoding.
+            return project.getProperties().getSourceEncoding().getValue();
         }
         else {
             return null;

commit 79c35f104f78493a63945c25d70de793f090fff5

A fix for a small ordering issue introduced accidentally in 9906200452bb21fc88cd51b7911c7dc7d53c6862

src/org/netbeans/gradle/project/persistent/XmlPropertiesPersister.java
@@ -41,7 +41,7 @@ public final class XmlPropertiesPersister implements PropertiesPersister {
         checkCallingThread();
 
         final PropertiesSnapshot snapshot = new PropertiesSnapshot(properties);
-        PropertiesPersister.PERSISTER_PROCESSOR.execute(new Runnable() {
+        NbGradleProject.PROJECT_PROCESSOR.execute(new Runnable() {
             @Override
             public void run() {
                 try {