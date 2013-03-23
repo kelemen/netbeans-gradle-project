b4fdc78c4aa63be9be664e9e99a318f0775a71a5
Attila Kelemen <attila.kelemen85@gmail.com>
2013-03-23 17:55:49 +0100

    ChangeLF plugin is now shown in the project settings (if installed).

--- src/org/netbeans/gradle/project/properties/GradleCustomizer.java
+++ src/org/netbeans/gradle/project/properties/GradleCustomizer.java
@@ -3,6 +3,12 @@ package org.netbeans.gradle.project.properties;
 import java.awt.Dialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import org.netbeans.gradle.project.NbGradleProject;
@@ -10,12 +16,13 @@ import org.netbeans.gradle.project.NbStrings;
 import org.netbeans.spi.project.ui.CustomizerProvider;
 import org.netbeans.spi.project.ui.support.ProjectCustomizer;
 import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
 
 public final class GradleCustomizer implements CustomizerProvider {
-    private static final String GRADLE_CATEGORY_NAME = "gradle";
+    private static final Logger LOGGER = Logger.getLogger(GradleCustomizer.class.getName());
 
-    private static final ProjectCustomizer.Category GRADLE_CATEGORY
-            = ProjectCustomizer.Category.create(GRADLE_CATEGORY_NAME, NbStrings.getGradleProjectCategoryName(), null);
+    private static final String GRADLE_CATEGORY_NAME = GradleCustomizer.class.getName() + ".gradle";
 
     private final NbGradleProject project;
 
@@ -24,22 +31,54 @@ public final class GradleCustomizer implements CustomizerProvider {
         this.project = project;
     }
 
+    private static ProjectCustomizer.CompositeCategoryProvider[] getExternalCustomizers() {
+        Lookup customizerLookup = Lookups.forPath("Projects/org-netbeans-modules-apisupport-project/Customizer");
+        Collection<? extends ProjectCustomizer.CompositeCategoryProvider> externalCategoriesCollection
+                = customizerLookup.lookupAll(ProjectCustomizer.CompositeCategoryProvider.class);
+        return externalCategoriesCollection
+                .toArray(new ProjectCustomizer.CompositeCategoryProvider[externalCategoriesCollection.size()]);
+    }
+
+    private ProjectCustomizer.CompositeCategoryProvider[] getAllCustomizers() {
+        ProjectCustomizer.CompositeCategoryProvider[] externalCategories
+                = getExternalCustomizers();
+        List<ProjectCustomizer.CompositeCategoryProvider> allCategoriesList
+                = new ArrayList<ProjectCustomizer.CompositeCategoryProvider>(externalCategories.length + 1);
+        allCategoriesList.add(new MainCustomizer(project));
+        allCategoriesList.addAll(Arrays.asList(externalCategories));
+
+        return allCategoriesList.toArray(new ProjectCustomizer.CompositeCategoryProvider[allCategoriesList.size()]);
+    }
+
     @Override
     public void showCustomizer() {
-        ProjectCustomizer.Category[] categories = new ProjectCustomizer.Category[] {
-            GRADLE_CATEGORY
-        };
+        final Lookup lookup = Lookups.fixed(project);
 
-        final ProjectPropertiesPanel panel = new ProjectPropertiesPanel(project);
+        final ProjectCustomizer.CompositeCategoryProvider[] customizers
+                = getAllCustomizers();
+
+        final ProjectCustomizer.Category[] categories =
+                new ProjectCustomizer.Category[customizers.length];
+        for (int i = 0; i < categories.length; i++) {
+            categories[i] = customizers[i].createCategory(lookup);
+        }
 
         ProjectCustomizer.CategoryComponentProvider panelProvider = new ProjectCustomizer.CategoryComponentProvider() {
             @Override
             public JComponent create(ProjectCustomizer.Category category) {
                 String name = category.getName();
-                if (GRADLE_CATEGORY_NAME.equals(name)) {
-                    return panel;
+                if (name == null) {
+                    LOGGER.log(Level.WARNING, "null category name.");
+                    return new JPanel();
                 }
 
+                for (int i = 0; i < categories.length; i++) {
+                    if (name.equals(categories[i].getName())) {
+                        return customizers[i].createComponent(category, lookup);
+                    }
+                }
+
+                LOGGER.log(Level.WARNING, "Requested category cannot be found {0}.", name);
                 return new JPanel();
             }
         };
@@ -47,7 +86,7 @@ public final class GradleCustomizer implements CustomizerProvider {
         ActionListener okListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                panel.saveProperties();
+                // no-op
             }
         };
 
@@ -61,5 +100,35 @@ public final class GradleCustomizer implements CustomizerProvider {
         dlg.setTitle(NbStrings.getProjectPropertiesDlgTitle(project.getDisplayName()));
         dlg.setModal(true);
         dlg.setVisible(true);
+    }
+
+    private static final class MainCustomizer
+    implements
+            ProjectCustomizer.CompositeCategoryProvider {
+
+        private final ProjectPropertiesPanel panel;
+
+        public MainCustomizer(NbGradleProject project) {
+            this.panel = new ProjectPropertiesPanel(project);
+        }
+
+        @Override
+        public ProjectCustomizer.Category createCategory(Lookup context) {
+            return ProjectCustomizer.Category.create(
+                    GRADLE_CATEGORY_NAME,
+                    NbStrings.getGradleProjectCategoryName(),
+                    null);
+        }
+
+        @Override
+        public JComponent createComponent(ProjectCustomizer.Category category, Lookup context) {
+            category.setOkButtonListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    panel.saveProperties();
+                }
+            });
+            return panel;
+        }
     }
 }
