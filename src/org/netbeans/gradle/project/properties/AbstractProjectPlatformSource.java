d717b0da05d840f6525c602e354b86c32349b20e
Attila Kelemen <attila.kelemen85@gmail.com>
2013-04-24 23:24:59 +0200

    Added ProjectPlatformSource including some related refactoring.

---
+++ src/org/netbeans/gradle/project/properties/AbstractProjectPlatformSource.java
@@ -0,0 +1,123 @@
+package org.netbeans.gradle.project.properties;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import javax.swing.event.ChangeListener;
+import org.netbeans.gradle.project.api.event.ListenerRef;
+import org.netbeans.gradle.project.api.query.GradleProjectPlatformQuery;
+import org.netbeans.gradle.project.api.query.ProjectPlatform;
+import org.openide.util.ChangeSupport;
+
+
+public abstract class AbstractProjectPlatformSource
+implements
+        PropertySource<ProjectPlatform> {
+
+    private final Lock changesLock;
+    private final ChangeSupport changes;
+    private ListenerRef subListenerRef;
+    private final AtomicReference<GradleProjectPlatformQuery> queryRef;
+
+    public AbstractProjectPlatformSource() {
+        this.changesLock = new ReentrantLock();
+        this.changes = new ChangeSupport(this);
+        this.subListenerRef = null;
+        this.queryRef = new AtomicReference<GradleProjectPlatformQuery>(null);
+    }
+
+    protected final GradleProjectPlatformQuery getCurrentQuery() {
+        return queryRef.get();
+    }
+
+    protected final void firePlatformChange() {
+        changes.fireChange();
+    }
+
+    protected final GradleProjectPlatformQuery trySetQuery(GradleProjectPlatformQuery query) {
+        if (query == null) throw new NullPointerException("query");
+
+        if (queryRef.compareAndSet(null, query)) {
+            ListenerRef toRemove = null;
+            InitLaterListenerRef toAdd = null;
+
+            changesLock.lock();
+            try {
+                if (changes.hasListeners()) {
+                    toRemove = subListenerRef;
+                    toAdd = new InitLaterListenerRef();
+                    subListenerRef = toAdd;
+                }
+            } finally {
+                changesLock.unlock();
+            }
+
+            if (toRemove != null) {
+                toRemove.unregister();
+            }
+            if (toAdd != null) {
+                toAdd.init(query.addPlatformChangeListener(new Runnable() {
+                    @Override
+                    public void run() {
+                        firePlatformChange();
+                    }
+                }));
+                firePlatformChange();
+            }
+        }
+
+        return queryRef.get();
+    }
+
+    @Override
+    public final void addChangeListener(ChangeListener listener) {
+        ListenerRef toRemove = null;
+        InitLaterListenerRef toAdd = null;
+        GradleProjectPlatformQuery query = queryRef.get();
+
+        changesLock.lock();
+        try {
+            if (!changes.hasListeners() && query != null) {
+                toRemove = subListenerRef;
+                toAdd = new InitLaterListenerRef();
+                subListenerRef = toAdd;
+            }
+            changes.addChangeListener(listener);
+        } finally {
+            changesLock.unlock();
+        }
+
+        if (toRemove != null) {
+            toRemove.unregister();
+        }
+        if (toAdd != null) {
+            assert query != null;
+            toAdd.init(query.addPlatformChangeListener(new Runnable() {
+                @Override
+                public void run() {
+                    changes.fireChange();
+                }
+            }));
+        }
+    }
+
+    @Override
+    public final void removeChangeListener(ChangeListener listener) {
+        ListenerRef toRemove = null;
+
+        changesLock.lock();
+        try {
+            changes.removeChangeListener(listener);
+            if (!changes.hasListeners()) {
+                toRemove = subListenerRef;
+                subListenerRef = null;
+            }
+        } finally {
+            changesLock.unlock();
+        }
+
+        if (toRemove != null) {
+            toRemove.unregister();
+        }
+    }
+}
--- src/org/netbeans/gradle/project/properties/ProjectQueryPlatformSource.java
+++ src/org/netbeans/gradle/project/properties/ProjectQueryPlatformSource.java
@@ -1,23 +1,12 @@
-
 package org.netbeans.gradle.project.properties;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import javax.swing.event.ChangeListener;
-import org.netbeans.gradle.project.api.event.ListenerRef;
 import org.netbeans.gradle.project.api.query.GradleProjectPlatformQuery;
 import org.netbeans.gradle.project.api.query.ProjectPlatform;
-import org.openide.util.ChangeSupport;
 
-public final class ProjectQueryPlatformSource implements PropertySource<ProjectPlatform> {
-    private final GradleProjectPlatformQuery query;
+public final class ProjectQueryPlatformSource extends AbstractProjectPlatformSource {
     private final String name;
     private final String version;
     private final boolean defaultValue;
-
-    private final Lock changesLock;
-    private final ChangeSupport changes;
-    private ListenerRef subListenerRef;
 
     public ProjectQueryPlatformSource(
             GradleProjectPlatformQuery query,
@@ -28,72 +17,20 @@ public final class ProjectQueryPlatformSource implements PropertySource<ProjectP
         if (name == null) throw new NullPointerException("name");
         if (version == null) throw new NullPointerException("version");
 
-        this.query = query;
         this.name = name;
         this.version = version;
         this.defaultValue = defaultValue;
-        this.changesLock = new ReentrantLock();
-        this.changes = new ChangeSupport(this);
-        this.subListenerRef = null;
+
+        trySetQuery(query);
     }
 
     @Override
     public ProjectPlatform getValue() {
-        return query.tryFindPlatformByName(name, version);
+        return getCurrentQuery().tryFindPlatformByName(name, version);
     }
 
     @Override
     public boolean isDefault() {
         return defaultValue;
-    }
-
-    @Override
-    public void addChangeListener(ChangeListener listener) {
-        ListenerRef toRemove = null;
-        InitLaterListenerRef toAdd = null;
-
-        changesLock.lock();
-        try {
-            if (!changes.hasListeners()) {
-                toRemove = subListenerRef;
-                toAdd = new InitLaterListenerRef();
-                subListenerRef = toAdd;
-            }
-            changes.addChangeListener(listener);
-        } finally {
-            changesLock.unlock();
-        }
-
-        if (toRemove != null) {
-            toRemove.unregister();
-        }
-        if (toAdd != null) {
-            toAdd.init(query.addPlatformChangeListener(new Runnable() {
-                @Override
-                public void run() {
-                    changes.fireChange();
-                }
-            }));
-        }
-    }
-
-    @Override
-    public void removeChangeListener(ChangeListener listener) {
-        ListenerRef toRemove = null;
-
-        changesLock.lock();
-        try {
-            changes.removeChangeListener(listener);
-            if (!changes.hasListeners()) {
-                toRemove = subListenerRef;
-                subListenerRef = null;
-            }
-        } finally {
-            changesLock.unlock();
-        }
-
-        if (toRemove != null) {
-            toRemove.unregister();
-        }
     }
 }
