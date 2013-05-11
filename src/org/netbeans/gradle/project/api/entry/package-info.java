87d85d6f873d17af2d9b282544cf26e314cf4323
Attila Kelemen <attila.kelemen85@gmail.com>
2013-05-11 23:37:40 +0200

    Added package-info for the "org.netbeans.gradle.project.api.entry" package.

---
+++ src/org/netbeans/gradle/project/api/entry/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * <B>Start from here</B> if you are planning to develop extensions for the
+ * Gradle plugin. This package contains interfaces serving as entry point for
+ * extension of a Gradle project. Also this package contains a few closely
+ * related classes.
+ * <P>
+ * The main entry point for extensions is the
+ * {@link org.netbeans.gradle.project.api.entry.GradleProjectExtensionQuery}.
+ *
+ * @see org.netbeans.gradle.project.api.entry.GradleProjectExtensionQuery
+ * @see org.netbeans.gradle.project.api.entry.GradleProjectPlatformQuery
+ */
+package org.netbeans.gradle.project.api.entry;
