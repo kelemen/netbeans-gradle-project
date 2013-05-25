9289d1881865bc7a1cac0e1c0a1eae8275c8b928
Attila Kelemen <attila.kelemen85@gmail.com>
2013-05-25 22:54:05 +0200

    Added API to access the "skip tests" global property.

---
+++ src/org/netbeans/gradle/project/api/config/GlobalConfig.java
@@ -0,0 +1,31 @@
+package org.netbeans.gradle.project.api.config;
+
+import org.netbeans.gradle.project.api.property.NbPropertySource;
+import org.netbeans.gradle.project.properties.GlobalGradleSettings;
+import org.netbeans.gradle.project.properties.NbPropertySourceWrapper;
+
+/**
+ * Contains the values of some of the properties set in the global settings.
+ */
+public final class GlobalConfig {
+    private static final NbPropertySource<Boolean> SKIP_TESTS
+            = new NbPropertySourceWrapper<Boolean>(GlobalGradleSettings.getSkipTests());
+
+    /**
+     * Returns the property indicating if tests should be skipped when executing
+     * tasks not directly related to testing (such as build). Extensions should
+     * consider this value when providing their own
+     * {@link org.netbeans.gradle.project.api.task.BuiltInGradleCommandQuery custom built-in tasks}.
+     *
+     * @return the property indicating if tests should be skipped when executing
+     *   tasks not directly related to testing (such as build). This method
+     *   never returns {@code null}.
+     */
+    public static NbPropertySource<Boolean> skipTests() {
+        return SKIP_TESTS;
+    }
+
+    private GlobalConfig() {
+        throw new AssertionError();
+    }
+}
