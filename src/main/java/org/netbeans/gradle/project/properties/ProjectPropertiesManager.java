kelemen/netbeans-gradle-project

commit 3538c66145a2ac581d0345a46bf85c62f2c646e2

Fixed a bug where the plugin may failed to use the Gradle version set in the project properties and used the one set in the global settings instead.

src/main/java/org/netbeans/gradle/project/properties/ProjectPropertiesManager.java
@@ -3,6 +3,7 @@ package org.netbeans.gradle.project.properties;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -24,8 +25,8 @@ public final class ProjectPropertiesManager {
     private static final Logger LOGGER = Logger.getLogger(ProjectPropertiesManager.class.getName());
 
     private static final Lock MAIN_LOCK = new ReentrantLock();
-    private static final Map<ProjectPropertySource, ProjectProperties> PROPERTIES
-            = new WeakValueHashMap<ProjectPropertySource, ProjectProperties>();
+    private static final Map<ProjectPropertySource, CachedProperties> PROPERTIES
+            = new WeakValueHashMap<ProjectPropertySource, CachedProperties>();
 
     private static void saveIfRequired(
             final NbGradleProject project,
@@ -115,31 +116,28 @@ public final class ProjectPropertiesManager {
         return new CombinedProjectPropertySource(propertySources, offset);
     }
 
-    private static ProjectProperties loadPropertiesAlways(
+    private static CachedProperties loadPropertiesAlways(
             final NbGradleProject project,
-            File propertiesFile,
-            final PropertiesLoadListener onLoadTask) {
+            File propertiesFile) {
 
-        final ProjectProperties properties = new MemProjectProperties();
+        final CachedProperties result = new CachedProperties(new MemProjectProperties());
         final PropertiesPersister persister = new XmlPropertiesPersister(propertiesFile);
         PropertiesPersister.PERSISTER_PROCESSOR.execute(new Runnable() {
             @Override
             public void run() {
-                persister.load(properties, false, new Runnable() {
+                persister.load(result.properties, false, new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            if (onLoadTask != null) {
-                                onLoadTask.loadedProperties(properties);
-                            }
+                            result.signalPropertiesLoaded();
                         } finally {
-                            setSaveOnChange(project, properties, persister);
+                            setSaveOnChange(project, result.properties, persister);
                         }
                     }
                 });
             }
         });
-        return properties;
+        return result;
     }
 
     private static class FileProjectPropertySource implements ProjectPropertySource {
@@ -154,7 +152,7 @@ public final class ProjectPropertiesManager {
 
         @Override
         public ProjectProperties load(PropertiesLoadListener onLoadTask) {
-            ProjectProperties result;
+            CachedProperties result;
             MAIN_LOCK.lock();
             try {
                 result = PROPERTIES.get(this);
@@ -163,8 +161,7 @@ public final class ProjectPropertiesManager {
             }
 
             if (result == null) {
-                final ProjectProperties newProperties
-                        = loadPropertiesAlways(project, propertiesFile, onLoadTask);
+                final CachedProperties newProperties = loadPropertiesAlways(project, propertiesFile);
 
                 MAIN_LOCK.lock();
                 try {
@@ -177,13 +174,14 @@ public final class ProjectPropertiesManager {
                     MAIN_LOCK.unlock();
                 }
             }
-            else {
-                if (onLoadTask != null) {
-                    onLoadTask.loadedProperties(result);
-                }
+
+            assert result != null;
+
+            if (onLoadTask != null) {
+                result.notifyOnLoad(onLoadTask);
             }
 
-            return result;
+            return result.properties;
         }
 
         @Override
@@ -215,7 +213,7 @@ public final class ProjectPropertiesManager {
 
         @Override
         public ProjectProperties load(final PropertiesLoadListener onLoadTask) {
-            ProjectProperties result;
+            CachedProperties result;
             MAIN_LOCK.lock();
             try {
                 result = PROPERTIES.get(this);
@@ -224,8 +222,8 @@ public final class ProjectPropertiesManager {
             }
 
             if (result == null) {
-                final AtomicReference<ProjectProperties> resultRef
-                        = new AtomicReference<ProjectProperties>(null);
+                final AtomicReference<CachedProperties> resultRef
+                        = new AtomicReference<CachedProperties>(null);
 
                 final AtomicInteger subTaskCount = new AtomicInteger();
                 // Setting the value of resultRef is counted as a subTask as well.
@@ -235,13 +233,13 @@ public final class ProjectPropertiesManager {
                     @Override
                     public void loadedProperties(ProjectProperties properties) {
                         if (subTaskCount.decrementAndGet() == 0 && onLoadTask != null) {
-                            ProjectProperties loadedProperties = resultRef.get();
+                            CachedProperties loadedProperties = resultRef.get();
                             if (loadedProperties == null) {
                                 String message = "Internal error while loading properties.";
                                 LOGGER.log(Level.SEVERE, message, new IllegalStateException(message));
                                 return;
                             }
-                            onLoadTask.loadedProperties(loadedProperties);
+                            loadedProperties.signalPropertiesLoaded();
                         }
                     }
                 };
@@ -261,27 +259,27 @@ public final class ProjectPropertiesManager {
                     newProperties = new FallbackProjectProperties(mainProperties, fallbackProperties);
                 }
 
+                CachedProperties cachedNewProperties = new CachedProperties(newProperties);
                 MAIN_LOCK.lock();
                 try {
                     result = PROPERTIES.get(this);
                     if (result == null) {
-                        PROPERTIES.put(this, newProperties);
-                        result = newProperties;
+                        PROPERTIES.put(this, cachedNewProperties);
+                        result = cachedNewProperties;
                     }
                 } finally {
                     MAIN_LOCK.unlock();
                 }
 
                 resultRef.set(result);
-                resultForwarder.loadedProperties(result);
-            }
-            else {
-                if (onLoadTask != null) {
-                    onLoadTask.loadedProperties(result);
-                }
+                resultForwarder.loadedProperties(result.properties);
             }
 
-            return result;
+            if (onLoadTask != null) {
+                result.notifyOnLoad(onLoadTask);
+            }
+
+            return result.properties;
         }
 
         @Override
@@ -384,6 +382,62 @@ public final class ProjectPropertiesManager {
 
             final NbCurrentProfileProjectPropertySource other = (NbCurrentProfileProjectPropertySource)obj;
             return this.project.equals(other.project);
+        }
+    }
+
+    private static final class CachedProperties {
+        public final ProjectProperties properties;
+
+        private final Lock loadLock;
+        private final List<PropertiesLoadListener> onLoadedTask;
+        private volatile boolean loaded;
+
+        public CachedProperties(ProjectProperties properties) {
+            assert properties != null;
+            this.properties = properties;
+            this.loadLock = new ReentrantLock();
+            this.onLoadedTask = new LinkedList<PropertiesLoadListener>();
+        }
+
+        public void signalPropertiesLoaded() {
+            PropertiesLoadListener[] toNotify;
+            loadLock.lock();
+            try {
+                loaded = true;
+                if (onLoadedTask.isEmpty()) {
+                    return;
+                }
+
+                toNotify = onLoadedTask.toArray(new PropertiesLoadListener[onLoadedTask.size()]);
+                onLoadedTask.clear();
+            } finally {
+                loadLock.unlock();
+            }
+
+            for (PropertiesLoadListener listener: toNotify) {
+                listener.loadedProperties(properties);
+            }
+        }
+
+        public void notifyOnLoad(PropertiesLoadListener listener) {
+            if (listener == null) throw new NullPointerException("listener");
+
+            if (loaded) {
+                listener.loadedProperties(properties);
+                return;
+            }
+
+            loadLock.lock();
+            try {
+                if (!loaded) {
+                    onLoadedTask.add(listener);
+                    return;
+                }
+            } finally {
+                loadLock.unlock();
+            }
+
+            listener.loadedProperties(properties);
         }
     }
 

src/main/java/org/netbeans/gradle/project/model/GradleModelLoader.java
@@ -40,6 +40,7 @@ import org.netbeans.gradle.project.api.entry.GradleProjectExtension;
 import org.netbeans.gradle.project.java.model.NbSourceRoot;
 import org.netbeans.gradle.project.properties.GlobalGradleSettings;
 import org.netbeans.gradle.project.properties.GradleLocation;
+import org.netbeans.gradle.project.properties.ProjectProperties;
 import org.netbeans.gradle.project.tasks.DaemonTask;
 import org.netbeans.gradle.project.tasks.GradleDaemonManager;
 import org.netbeans.gradle.project.tasks.GradleTasks;
@@ -84,7 +85,15 @@ public final class GradleModelLoader {
             result.useGradleUserHomeDir(gradleUserHome);
         }
 
-        GradleLocation gradleLocation = project.getProperties().getGradleLocation().getValue();
+        GradleLocation gradleLocation;
+        ProjectProperties projectProperties = project.tryGetLoadedProperties();
+        if (projectProperties == null) {
+            LOGGER.warning("Could not wait for retrieving the project properties. Using the globally defined one");
+            gradleLocation = GlobalGradleSettings.getGradleHome().getValue();
+        }
+        else {
+            gradleLocation = projectProperties.getGradleLocation().getValue();
+        }
 
         gradleLocation.applyLocation(new GradleLocation.Applier() {
             @Override

commit 5f17ace51f04368f16121815bf8318002827a9b8

Fixed ProjectPropertiesManager, so that it won't let the project properties be garbage collected (this bug was introduced in commit 3538c66145a2ac581d0345a46bf85c62f2c646e2).

src/main/java/org/netbeans/gradle/project/properties/ProjectPropertiesManager.java
@@ -1,11 +1,14 @@
 package org.netbeans.gradle.project.properties;
 
 import java.io.File;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
@@ -15,9 +18,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import org.netbeans.api.java.platform.JavaPlatform;
 import org.netbeans.gradle.project.NbGradleProject;
 import org.netbeans.gradle.project.WaitableSignal;
 import org.netbeans.gradle.project.api.config.ProfileDef;
+import org.netbeans.gradle.project.api.entry.ProjectPlatform;
 import org.netbeans.gradle.project.persistent.PropertiesPersister;
 import org.netbeans.gradle.project.persistent.XmlPropertiesPersister;
 
@@ -25,6 +30,8 @@ public final class ProjectPropertiesManager {
     private static final Logger LOGGER = Logger.getLogger(ProjectPropertiesManager.class.getName());
 
     private static final Lock MAIN_LOCK = new ReentrantLock();
+    // Don't forget that the value can't be wrapped, it must be the one to be
+    // returned, otherwise it might get garbage collected.
     private static final Map<ProjectPropertySource, CachedProperties> PROPERTIES
             = new WeakValueHashMap<ProjectPropertySource, CachedProperties>();
 
@@ -125,13 +132,13 @@ public final class ProjectPropertiesManager {
         PropertiesPersister.PERSISTER_PROCESSOR.execute(new Runnable() {
             @Override
             public void run() {
-                persister.load(result.properties, false, new Runnable() {
+                persister.load(result, false, new Runnable() {
                     @Override
                     public void run() {
                         try {
                             result.signalPropertiesLoaded();
                         } finally {
-                            setSaveOnChange(project, result.properties, persister);
+                            setSaveOnChange(project, result, persister);
                         }
                     }
                 });
@@ -181,7 +188,7 @@ public final class ProjectPropertiesManager {
                 result.notifyOnLoad(onLoadTask);
             }
 
-            return result.properties;
+            return result;
         }
 
         @Override
@@ -272,14 +279,14 @@ public final class ProjectPropertiesManager {
                 }
 
                 resultRef.set(result);
-                resultForwarder.loadedProperties(result.properties);
+                resultForwarder.loadedProperties(result);
             }
 
             if (onLoadTask != null) {
                 result.notifyOnLoad(onLoadTask);
             }
 
-            return result.properties;
+            return result;
         }
 
         @Override
@@ -385,9 +392,8 @@ public final class ProjectPropertiesManager {
         }
     }
 
-    private static final class CachedProperties {
-        public final ProjectProperties properties;
-
+    private static final class CachedProperties implements ProjectProperties {
+        private final ProjectProperties properties;
         private final Lock loadLock;
         private final List<PropertiesLoadListener> onLoadedTask;
         private volatile boolean loaded;
@@ -438,6 +444,76 @@ public final class ProjectPropertiesManager {
             }
 
             listener.loadedProperties(properties);
+        }
+
+        @Override
+        public MutableProperty<String> getSourceLevel() {
+            return properties.getSourceLevel();
+        }
+
+        @Override
+        public MutableProperty<ProjectPlatform> getPlatform() {
+            return properties.getPlatform();
+        }
+
+        @Override
+        public MutableProperty<JavaPlatform> getScriptPlatform() {
+            return properties.getScriptPlatform();
+        }
+
+        @Override
+        public MutableProperty<GradleLocation> getGradleLocation() {
+            return properties.getGradleLocation();
+        }
+
+        @Override
+        public MutableProperty<Charset> getSourceEncoding() {
+            return properties.getSourceEncoding();
+        }
+
+        @Override
+        public MutableProperty<List<PredefinedTask>> getCommonTasks() {
+            return properties.getCommonTasks();
+        }
+
+        @Override
+        public MutableProperty<LicenseHeaderInfo> getLicenseHeader() {
+            return properties.getLicenseHeader();
+        }
+
+        @Override
+        public MutableProperty<PredefinedTask> tryGetBuiltInTask(String command) {
+            return properties.tryGetBuiltInTask(command);
+        }
+
+        @Override
+        public Set<String> getKnownBuiltInCommands() {
+            return properties.getKnownBuiltInCommands();
+        }
+
+        @Override
+        public MutableProperty<Void> getAuxConfigListener() {
+            return properties.getAuxConfigListener();
+        }
+
+        @Override
+        public AuxConfigProperty getAuxConfig(String elementName, String namespace) {
+            return properties.getAuxConfig(elementName, namespace);
+        }
+
+        @Override
+        public void setAllAuxConfigs(Collection<AuxConfig> configs) {
+            properties.setAllAuxConfigs(configs);
+        }
+
+        @Override
+        public Collection<AuxConfigProperty> getAllAuxConfigs() {
+            return properties.getAllAuxConfigs();
+        }
+
+        @Override
+        public Collection<MutableProperty<?>> getAllProperties() {
+            return properties.getAllProperties();
         }
     }
 