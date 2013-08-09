f9dc44c2e78606d892e50f18e42728511065ba56
Attila Kelemen <attila.kelemen85@gmail.com>
2013-08-09 22:57:03 +0200

    Documented ContextAwareCommandAction and ContextAwareCommandFinalizer.

--- src/main/java/org/netbeans/gradle/project/api/task/ContextAwareCommandAction.java
+++ src/main/java/org/netbeans/gradle/project/api/task/ContextAwareCommandAction.java
@@ -5,10 +5,37 @@ import org.netbeans.api.project.Project;
 import org.openide.util.Lookup;
 
 /**
+ * Defines a custom action for a {@link GradleCommandTemplate Gradle command}
+ * which needs the {@link Lookup} context and the {@link Project} instance
+ * associated with the currently started task.
+ * <P>
+ * The method of this interface is called from a background thread (not the Event Dispatch Thread).
  *
+ * @see BuiltInGradleCommandQuery
+ * @see CustomCommandActions
+ * @see GradleCommandExecutor
  */
 public interface ContextAwareCommandAction {
-    /***/
+    /**
+     * Called before a Gradle command is executed and returns the
+     * {@link ContextAwareCommandFinalizer} which is to be notified if the
+     * Gradle command completes successfully.
+     * <P>
+     * If the command to be executed is a built-in task, then the
+     * {@code commandContext} is the {@code Lookup}, NetBeans passes to the
+     * {@link org.netbeans.spi.project.ActionProvider}. Otherwise it is an empty
+     * {@code Lookup}.
+     *
+     * @param project the Gradle project in which context the command is
+     *   executed. This is similar to executing a command from the command line
+     *   from the directory of this project. This argument cannot be {@code null}.
+     * @param commandContext the context when the command was started. This
+     *   argument cannot be {@code null}.
+     *
+     * @return the {@link ContextAwareCommandFinalizer} which is to be notified
+     *   if the Gradle command completes successfully. This method may never
+     *   return {@code null}.
+     */
     @Nonnull
     public ContextAwareCommandFinalizer startCommand(@Nonnull Project project, @Nonnull Lookup commandContext);
 }
--- src/main/java/org/netbeans/gradle/project/api/task/ContextAwareCommandFinalizer.java
+++ src/main/java/org/netbeans/gradle/project/api/task/ContextAwareCommandFinalizer.java
@@ -4,10 +4,26 @@ import javax.annotation.Nonnull;
 import org.openide.windows.OutputWriter;
 
 /**
+ * Defines the task to be executed after a {@link GradleCommandTemplate Gradle command}
+ * completes successfully. Instances of this interface are expected to be
+ * created by a {@link ContextAwareCommandAction}.
+ * <P>
+ * The method of this interface is called from a background thread (not the Event Dispatch Thread).
  *
+ * @see ContextAwareCommandAction
  */
 public interface ContextAwareCommandFinalizer {
-    /***/
+    /**
+     * Called when a Gradle command completes successfully.
+     *
+     * @param output the {@code OutputWriter} which can be used to write messages
+     *   to the output window associated with the Gradle command. This argument
+     *   cannot be {@code null}.
+     * @param errOutput the {@code OutputWriter} which can be used to write messages
+     *   to the output window associated with the Gradle command as an error.
+     *   This usually means that messages written to this writer will be printed
+     *   in red. This argument cannot be {@code null}.
+     */
     public void finalizeSuccessfulCommand(
             @Nonnull OutputWriter output,
             @Nonnull OutputWriter errOutput);
