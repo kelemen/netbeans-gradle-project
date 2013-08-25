9fbe8b8e0245fd2f37a225928b4a53f2dddaf3dc
Attila Kelemen <attila.kelemen85@gmail.com>
2013-08-25 22:05:42 +0200

    Added GradleBuildInfoQuery.

---
+++ netbeans-gradle-default-models/src/main/java/org/netbeans/gradle/model/GradleBuildInfoQuery.java
@@ -0,0 +1,27 @@
+package org.netbeans.gradle.model;
+
+/**
+ * Defines the query to be submitted for the Gradle daemon to query information
+ * from the Gradle build.
+ * <P>
+ * Note that the Tooling API always requires a project to be associated with
+ * the query. However, it is not necessarily evaluated, unless accessed directly
+ * or indirectly.
+ *
+ * @param <T> the type of the object extracted from the Gradle build. Note that
+ *   this type must be serializable in order to be able to transfer the
+ *   extracted information from the Gradle daemon to the caller.
+ *
+ * @see ProjectInfoBuilder
+ */
+public interface GradleBuildInfoQuery<T> extends GradleInfoQuery {
+    /**
+     * Returns the builder which will extract the information from the Gradle
+     * build. The builder will be serialized and be executed in the context
+     * of the Gradle daemon evaluating the associated project.
+     *
+     * @return the builder which will extract the information from the project
+     *   object. This method may never return {@code null}.
+     */
+    public BuildInfoBuilder<T> getInfoBuilder();
+}
