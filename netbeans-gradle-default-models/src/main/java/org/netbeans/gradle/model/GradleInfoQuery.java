c520100e824df3f41ed774b4da72ceaebb64a035
Attila Kelemen <attila.kelemen85@gmail.com>
2013-08-25 21:55:17 +0200

    Added GradleInfoQuery.

---
+++ netbeans-gradle-default-models/src/main/java/org/netbeans/gradle/model/GradleInfoQuery.java
@@ -0,0 +1,24 @@
+package org.netbeans.gradle.model;
+
+import java.io.File;
+import java.util.Set;
+
+/**
+ * Defines the base interface for query definition submitted for the Gradle
+ * daemon. The basic information required by queries is the class path required
+ * to execute the methods extracting the information from the Gradle build.
+ */
+public interface GradleInfoQuery {
+    /**
+     * Returns the class path required to execute the methods extracting the
+     * information from the Gradle build. This method is expected the return
+     * the same class path if returned multiple times.
+     * <P>
+     * The returned must not contain the <I>Tooling API of Gradle</I>.
+     *
+     * @return the class path required to execute the methods extracting the
+     *   information from the Gradle build. This method may never return
+     *   {@code null}.
+     */
+    public Set<File> getInfoClassPath();
+}
