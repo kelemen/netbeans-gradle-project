0597de72906ea4b0e82cabc02f95c8a10c5f6846
Attila Kelemen <attila.kelemen85@gmail.com>
2013-08-27 21:50:27 +0200

    Added JavaCompatibilityModelBuilder.

---
+++ netbeans-gradle-default-models/src/main/groovy/org/netbeans/gradle/model/java/JavaCompatibilityModelBuilder.groovy
@@ -0,0 +1,45 @@
+package org.netbeans.gradle.model.java;
+
+import org.gradle.api.Project;
+import org.netbeans.gradle.model.ProjectInfoBuilder;
+import org.netbeans.gradle.model.java.JavaCompatibilityModel;
+
+/**
+ * Defines a {@code ProjectInfoBuilder} which is able to extract
+ * {@link JavaCompatibilityModel} from a Gradle project.
+ * <P>
+ * Since this builder does not have any input argument, it is singleton and its
+ * instance can be accessed through {@code JavaCompatibilityModelBuilder.INSTANCE}.
+ */
+public enum JavaCompatibilityModelBuilder
+implements
+        ProjectInfoBuilder<JavaCompatibilityModel> {
+
+    /**
+     * The one and only instance of {@code JavaCompatibilityModelBuilder}.
+     */
+    INSTANCE;
+
+    /**
+     * Extracts and returns the {@code JavaCompatibilityModel} from the given
+     * project or returns {@code null} if the project does not uses the 'java'
+     * plugin.
+     *
+     * @param project the project from which the information is to be extracted.
+     *   This argument cannot be {@code null}.
+     *
+     * @return the {@code JavaCompatibilityModel} extracted from the given
+     *   project or {@code null} if the project does not applyies the "java"
+     *   plugin
+     */
+    public JavaCompatibilityModel getProjectInfo(Project project) {
+        if (!project.plugins.hasPlugin('java')) {
+            return null;
+        }
+
+        String srcLevel = project.sourceCompatibility.toString();
+        String targetLevel = project.targetCompatibility.toString();
+
+        return new JavaCompatibilityModel(srcLevel, targetLevel);
+    }
+}
