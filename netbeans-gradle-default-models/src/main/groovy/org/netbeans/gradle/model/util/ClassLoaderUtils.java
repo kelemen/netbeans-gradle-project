bc4a67187d1198fe3284414c50bbb3918345d6e6
Attila Kelemen <attila.kelemen85@gmail.com>
2013-08-31 18:11:35 +0200

    Added ClassLoaderUtils to find JARs of the classpath.

---
+++ netbeans-gradle-default-models/src/main/groovy/org/netbeans/gradle/model/util/ClassLoaderUtils.java
@@ -0,0 +1,83 @@
+package org.netbeans.gradle.model.util;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.concurrent.atomic.AtomicReference;
+
+public final class ClassLoaderUtils {
+    private static final AtomicReference<File> JAR_OF_THIS_PROJECT = new AtomicReference<File>(null);
+
+    public static File findClassPathOfClass(Class<?> cl) {
+        String className = cl.getName();
+        String classFileName = cl.getName().replace('.', '/') + ".class";
+        URL urlOfClassPath = cl.getClassLoader().getResource(classFileName);
+        if (urlOfClassPath == null) {
+            throw new IllegalArgumentException("Unable to locate classpath of " + cl);
+        }
+
+        File fileOfURL = extractPathFromURL(urlOfClassPath);
+
+        String protocol = urlOfClassPath.getProtocol();
+        if ("jar".equals(protocol)) {
+            return fileOfURL;
+        }
+        else if ("file".equals(protocol)) {
+            String[] pathParts = className.split("\\.");
+            pathParts[pathParts.length - 1] = pathParts[pathParts.length - 1] + ".class";
+
+            File root = fileOfURL;
+            for (int i = pathParts.length - 1; i >= 0; i--) {
+                String part = pathParts[i];
+                if (!root.getName().equalsIgnoreCase(part)) {
+                    throw new IllegalArgumentException("Unexpected path returned for class " + cl + ": " + urlOfClassPath);
+                }
+
+                root = root.getParentFile();
+                if (root == null) {
+                    throw new IllegalArgumentException("Too short path returned for class " + cl + ": " + urlOfClassPath);
+                }
+            }
+            return root;
+        }
+
+        throw new IllegalArgumentException("Unexpected protocol for URL: " + urlOfClassPath);
+    }
+
+    private static File urlToFile(URL url) {
+        try {
+            return new File(url.toURI());
+        } catch (URISyntaxException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    public static File extractPathFromURL(URL url) {
+        String protocol = url.getProtocol();
+        if ("jar".equals(protocol)) {
+            String[] pathParts = url.getPath().split("!", 2);
+            try {
+                URL jarURL = new URL(pathParts[0]);
+                return urlToFile(jarURL);
+            } catch (MalformedURLException ex) {
+                throw new IllegalArgumentException("Unexpected URL: " + url);
+            }
+        }
+        else if ("file".equals(protocol)) {
+            return urlToFile(url);
+        }
+        else {
+            throw new IllegalArgumentException("Unexpected URL: " + url);
+        }
+    }
+
+    public static File getLocationOfClassPath() {
+        File result = JAR_OF_THIS_PROJECT.get();
+        if (result == null) {
+            JAR_OF_THIS_PROJECT.set(findClassPathOfClass(ClassLoaderUtils.class));
+            result = JAR_OF_THIS_PROJECT.get();
+        }
+        return result;
+    }
+}
---
+++ netbeans-gradle-default-models/src/test/java/org/netbeans/gradle/model/util/ClassLoaderUtilsTest.java
@@ -0,0 +1,71 @@
+package org.netbeans.gradle.model.util;
+
+import java.io.File;
+import java.net.URL;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassLoaderUtilsTest {
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private static void verifyPathParts(File file, String... parts) {
+        File current = file;
+        for (int i = parts.length - 1; i >= 0; i--) {
+            assertNotNull("File is too short " + parts, current);
+            assertEquals("File must match file parts: " + file, parts[i], current.getName());
+            current = current.getParentFile();
+        }
+
+        assertTrue("File path is too long " + file, current == null || current.getParentFile() == null);
+    }
+
+    @Test
+    public void testExtractPathFromURLWithSpaces() throws Exception {
+        File file = ClassLoaderUtils.extractPathFromURL(new URL("file:///c/my%20dir/my-path"));
+        verifyPathParts(file, "c", "my dir", "my-path");
+    }
+
+    @Test
+    public void testExtractPathFromURLWithSpecialChars() throws Exception {
+        File file = ClassLoaderUtils.extractPathFromURL(new URL("file:///c/my-path/\u00e1b\u0678\u040c\u039e\u05de"));
+        verifyPathParts(file, "c", "my-path", "\u00e1b\u0678\u040c\u039e\u05de");
+    }
+
+    @Test
+    public void testGetLocationOfClassPath() {
+        File dir = ClassLoaderUtils.getLocationOfClassPath();
+        assertTrue("The project binaries must be in a directory and not a jar for this test to be executed",
+                dir.isDirectory());
+
+        String relPath = ClassLoaderUtils.class.getName().replace(".", File.separator) + ".class";
+        File classFile = new File(dir, relPath);
+
+        assertTrue("Class file must be on the class path: " + dir, classFile.isFile());
+    }
+
+    @Test
+    public void testFindClassPathOfClassForJar() {
+        File jarOfJUnit = ClassLoaderUtils.findClassPathOfClass(org.junit.Assert.class);
+        assertTrue("Jar must exist: " + jarOfJUnit, jarOfJUnit.isFile());
+        assertTrue("Must have extension jar: " + jarOfJUnit, jarOfJUnit.getName().endsWith(".jar"));
+    }
+}
