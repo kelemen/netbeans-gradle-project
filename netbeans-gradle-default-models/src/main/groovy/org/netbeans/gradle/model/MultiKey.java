cb4cf20cc5851dd0a02578921cfa9a1af7ede42c
Attila Kelemen <attila.kelemen85@gmail.com>
2013-09-02 19:50:30 +0200

    Added MultiKey to provide better support FetchedModels.

---
+++ netbeans-gradle-default-models/src/main/groovy/org/netbeans/gradle/model/MultiKey.java
@@ -0,0 +1,39 @@
+package org.netbeans.gradle.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class MultiKey {
+    private final Object[] keys;
+
+    private MultiKey(Object[] keys) {
+        this.keys = keys.clone();
+    }
+
+    public static MultiKey create(Object... keys) {
+        return new MultiKey(keys);
+    }
+
+    public static MultiKey createFromList(List<?> keys) {
+        return new MultiKey(keys.toArray());
+    }
+
+    public Object[] getKeys() {
+        return keys.clone();
+    }
+
+    @Override
+    public int hashCode() {
+        return 185 + Arrays.hashCode(keys);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (getClass() != obj.getClass()) return false;
+
+        final MultiKey other = (MultiKey)obj;
+        return Arrays.equals(this.keys, other.keys);
+    }
+}
