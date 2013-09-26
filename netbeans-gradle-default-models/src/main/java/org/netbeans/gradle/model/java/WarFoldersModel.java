32a2d2a55a3055ddc755e077ded0122a037cdb5c
Attila Kelemen <attila.kelemen85@gmail.com>
2013-09-26 05:53:01 +0200

    WarFoldersModel is now serializable.

--- netbeans-gradle-default-models/src/main/java/org/netbeans/gradle/model/java/WarFoldersModel.java
+++ netbeans-gradle-default-models/src/main/java/org/netbeans/gradle/model/java/WarFoldersModel.java
@@ -1,8 +1,11 @@
 package org.netbeans.gradle.model.java;
 
 import java.io.File;
+import java.io.Serializable;
 
-public final class WarFoldersModel {
+public final class WarFoldersModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private final File webAppDir;
 
     public WarFoldersModel(File webAppDir) {
