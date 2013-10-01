48ceb8a754ba4dc4d9a390cc91839424b4f10eb0
Attila Kelemen <attila.kelemen85@gmail.com>
2013-10-02 00:20:57 +0200

    JarOutputsModelBuilder now works even if the "jar" task is renamed.

--- netbeans-gradle-default-models/src/secondary/groovy/org/netbeans/gradle/model/java/JarOutputsModelBuilder.groovy
+++ netbeans-gradle-default-models/src/secondary/groovy/org/netbeans/gradle/model/java/JarOutputsModelBuilder.groovy
@@ -13,12 +13,28 @@ implements
         return project.getClass().classLoader.loadClass(className);
     }
 
+    private static Object findTaskByName(Project project, String taskName) {
+        def task = project.tasks.findByName(taskName);
+        if (task != null) {
+            return task;
+        }
+
+        String prefixedTaskName = ':' + taskName;
+        project.tasks.each {
+            if (it.path.endsWith(prefixedTaskName)) {
+                return it;
+            }
+        }
+
+        return null;
+    }
+
     public JarOutputsModel getProjectInfo(Project project) {
         if (!project.plugins.hasPlugin('java')) {
             return null;
         }
 
-        Class jarClass = project.jar.getClass();
+        Class jarClass = findTaskByName(project, 'jar').getClass();
 
         List result = new LinkedList();
         project.tasks.withType(jarClass, { jar ->
