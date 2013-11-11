f88c6fbfa5e9d16cf9e05c989401600e6c2c7ca3
Attila Kelemen <attila.kelemen85@gmail.com>
2013-11-11 20:50:12 +0100

    Added ProjectLookupObject.

---
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/others/ProjectLookupObject.java
@@ -0,0 +1,40 @@
+package org.netbeans.gradle.project.others;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.netbeans.api.project.Project;
+import org.openide.util.Lookup;
+
+public final class ProjectLookupObject {
+    private static final Logger LOGGER = Logger.getLogger(ProjectLookupObject.class.getName());
+
+    private final PluginClass pluginClass;
+
+    public ProjectLookupObject(PluginClass pluginClass) {
+        if (pluginClass == null) throw new NullPointerException("pluginClass");
+        this.pluginClass = pluginClass;
+    }
+
+    private static Map<Class<?>, Object> createArgs(Project project) {
+        Map<Class<?>, Object> result = new HashMap<Class<?>, Object>();
+        result.put(Project.class, project);
+        result.put(Lookup.class, Lookup.EMPTY);
+        return result;
+    }
+
+    public Object tryCreateInstance(Project project) {
+        Class<?> type = pluginClass.tryGetClass();
+        if (type == null) {
+            return null;
+        }
+
+        try {
+            return ReflectionHelper.tryCreateInstance(type, createArgs(project));
+        } catch (Throwable ex) {
+            LOGGER.log(Level.INFO, "Creating an instance of " + type + " has thrown an exception.", ex);
+            return null;
+        }
+    }
+}
