218693ce32b2e422fc109adb0f8959a807d10b9c
Attila Kelemen <attila.kelemen85@gmail.com>
2013-11-14 17:55:59 +0100

    Added API to be able to create non-existing source folders.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/query/JavaExtensionNodes.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/query/JavaExtensionNodes.java
@@ -1,14 +1,26 @@
 package org.netbeans.gradle.project.java.query;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import org.netbeans.api.project.SourceGroup;
+import org.netbeans.gradle.model.java.JavaSourceGroup;
+import org.netbeans.gradle.model.java.JavaSourceSet;
 import org.netbeans.gradle.project.GradleProjectSources;
 import org.netbeans.gradle.project.api.event.NbListenerRef;
 import org.netbeans.gradle.project.api.nodes.GradleProjectExtensionNodes;
 import org.netbeans.gradle.project.api.nodes.SingleNodeFactory;
 import org.netbeans.gradle.project.java.JavaExtension;
+import org.netbeans.gradle.project.java.JavaModelChangeListener;
 import org.netbeans.gradle.project.java.model.NamedSourceRoot;
+import org.netbeans.gradle.project.java.model.NbJavaModule;
 import org.netbeans.gradle.project.java.model.NbListedDir;
 import org.netbeans.gradle.project.java.nodes.JavaDependenciesNode;
 import org.netbeans.spi.java.project.support.ui.PackageView;
@@ -17,28 +29,63 @@ import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataFolder;
 import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
+import org.openide.util.ChangeSupport;
 
-public final class JavaExtensionNodes implements GradleProjectExtensionNodes {
+public final class JavaExtensionNodes
+implements
+        GradleProjectExtensionNodes,
+        JavaModelChangeListener {
+
+    private static final Logger LOGGER = Logger.getLogger(JavaExtensionNodes.class.getName());
+
     private final JavaExtension javaExt;
+    private final ChangeSupport nodeChanges;
 
     public JavaExtensionNodes(JavaExtension javaExt) {
         if (javaExt == null) throw new NullPointerException("javaExt");
         this.javaExt = javaExt;
+        this.nodeChanges = new ChangeSupport(this);
     }
 
     @Override
-    public NbListenerRef addNodeChangeListener(Runnable listener) {
-        if (listener == null) throw new NullPointerException("listener");
+    public void onModelChange() {
         // FIXME: We currently rely on the undocumented fact, that nodes are
         // always reloaded after a model reload.
+        //
+        // We will not call fireNodeChanges() until the implementation to
+        // request the reloading of nodes is more clever and merges multiple
+        // node reload requests into a single reload.
+        //
+        // Using UpdaterSwingTaskExecutor (always invoke later) should be
+        // sufficient.
+
+        // fireNodeChanges();
+    }
+
+    @Override
+    public NbListenerRef addNodeChangeListener(final Runnable listener) {
+        if (listener == null) throw new NullPointerException("listener");
+
+        final ChangeListener listenerWrapper = new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                listener.run();
+            }
+        };
+
+        nodeChanges.addChangeListener(listenerWrapper);
         return new NbListenerRef() {
+            private volatile boolean registered = true;
+
             @Override
             public boolean isRegistered() {
-                return false;
+                return registered;
             }
 
             @Override
             public void unregister() {
+                nodeChanges.removeChangeListener(listenerWrapper);
+                registered = false;
             }
         };
     }
@@ -89,6 +136,45 @@ public final class JavaExtensionNodes implements GradleProjectExtensionNodes {
                     return PackageView.createPackageView(group);
                 }
             });
+        }
+    }
+
+    private void fireNodeChanges() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                nodeChanges.fireChange();
+            }
+        });
+    }
+
+    private void createDir(File dir) {
+        if (!dir.mkdirs()) {
+            LOGGER.log(Level.INFO, "Failed to create new directory: {0}", dir);
+        }
+    }
+
+    private void createDirs(Collection<File> dirs) {
+        for (File dir: dirs ){
+            createDir(dir);
+        }
+    }
+
+    public void createDirectories() throws IOException {
+        NbJavaModule module = javaExt.getCurrentModel().getMainModule();
+
+        try {
+            for (JavaSourceSet sourceSet: module.getSources()) {
+                for (JavaSourceGroup sourceGroup: sourceSet.getSourceGroups()) {
+                    createDirs(sourceGroup.getSourceRoots());
+                }
+            }
+
+            for (NbListedDir listedDir: module.getListedDirs()) {
+                createDir(listedDir.getDirectory());
+            }
+        } finally {
+            fireNodeChanges();
         }
     }
 
