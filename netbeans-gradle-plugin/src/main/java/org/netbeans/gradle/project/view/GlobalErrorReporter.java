bbbb9a3309f2189aff84a962f5550bf5c23e1f5d
Attila Kelemen <attila.kelemen85@gmail.com>
2013-11-23 01:18:22 +0100

    Fixed the icon problem in the GTK L&F.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/view/GlobalErrorReporter.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/view/GlobalErrorReporter.java
@@ -2,13 +2,14 @@ package org.netbeans.gradle.project.view;
 
 import javax.swing.Icon;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import org.netbeans.gradle.project.NbIcons;
 import org.netbeans.gradle.project.NbStrings;
 import org.openide.awt.NotificationDisplayer;
 
 public final class GlobalErrorReporter {
-    private static final Icon ERROR_ICON = NbIcons.getUIErrorIcon();
+    private static final Icon ERROR_ICON = NbIcons.getPriorityHighIcon();
 
     public static void showIssue(final String message, Throwable error) {
         if (message == null) throw new NullPointerException("message");
@@ -20,7 +21,10 @@ public final class GlobalErrorReporter {
                     @Override
                     public void run() {
                         NotificationDisplayer displayer = NotificationDisplayer.getDefault();
-                        JLabel messageLabel = new JLabel(message);
+                        JLabel messageLabel = new JLabel(
+                                message,
+                                NbIcons.getUIErrorIcon(),
+                                SwingConstants.LEADING);
                         JLabel lineLabel = new JLabel(message);
 
                         displayer.notify(
