1ecfafd4f39a500cdfbb4655ac08bda8dedcaacd
Attila Kelemen <attila.kelemen85@gmail.com>
2013-11-30 04:28:42 +0100

    Documented ModelLoadResult.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/entry/ModelLoadResult.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/entry/ModelLoadResult.java
@@ -8,6 +8,14 @@ import org.netbeans.gradle.model.util.CollectionUtils;
 import org.openide.util.Lookup;
 
 /**
+ * Defines the result of the evaluation of a build script for a particular
+ * extension. That is, the argument for the
+ * {@link GradleProjectExtensionDef#parseModel(ModelLoadResult)}.
+ * <P>
+ * Instances of this class are immutable and therefore are safe to be shared
+ * by multiple threads concurrently. Though the contents of the lookup may
+ * change in general, the Gradle plugin only passes a lookup to the
+ * {@code parseModel} method which never changes.
  *
  * @see GradleProjectExtensionDef#parseModel(ModelLoadResult)
  */
@@ -17,7 +25,19 @@ public final class ModelLoadResult {
 
     private final Map<File, Lookup> evaluatedProjectsModel;
 
-    /***/
+    /**
+     * Creates a {@code ModelLoadResult} with the specified main project
+     * (given by its project directory) and retrieved models for all evaluated
+     * projects.
+     *
+     * @param mainProjectDir the project directory of the main project.
+     *   This argument cannot be {@code null}.
+     * @param evaluatedProjectsModel the map mapping projects (specified by
+     *   their project directory) to retrieved models. This map must contain a
+     *   {@code Lookup} for the specified {@code mainProjectDir}. This argument
+     *   cannot be {@code null} and neither can its keys and values be
+     *   {@code null}.
+     */
     public ModelLoadResult(
             @Nonnull File mainProjectDir,
             @Nonnull Map<File, Lookup> evaluatedProjectsModel) {
@@ -33,7 +53,16 @@ public final class ModelLoadResult {
         }
     }
 
-    /***/
+    /**
+     * Creates a {@code ModelLoadResult} with the specified main project
+     * (given by its project directory) assuming a single evaluated project.
+     * The evaluated projects map will contain a single entry: The main project.
+     *
+     * @param mainProjectDir the project directory of the main project.
+     *   This argument cannot be {@code null}.
+     * @param mainProjectModels the models retrieved for the main project.
+     *   This argument cannot be {@code null}.
+     */
     public ModelLoadResult(
             @Nonnull File mainProjectDir,
             @Nonnull Lookup mainProjectModels) {
@@ -56,7 +85,17 @@ public final class ModelLoadResult {
         this.evaluatedProjectsModel = evaluatedProjectsModel;
     }
 
-    /***/
+    /**
+     * Returns a {@code ModelLoadResult} with the same evaluated projects but
+     * with the given project as a main project. This {@code ModelLoadResult}
+     * must contain an entry for the specified project directory.
+     *
+     * @param projectDir the project directory of the main project of the
+     *   returned {@code ModelLoadResult}. This argument cannot be {@code null}.
+     * @return the {@code ModelLoadResult} with the same evaluated projects but
+     *   with the given project as a main project. This method never returns
+     *   {@code null}.
+     */
     @Nonnull
     public ModelLoadResult withMainProject(@Nonnull File projectDir) {
         if (projectDir == null) throw new NullPointerException("projectDir");
@@ -68,19 +107,41 @@ public final class ModelLoadResult {
         return new ModelLoadResult(projectDir, models, evaluatedProjectsModel);
     }
 
-    /***/
+    /**
+     * Returns the project directory of the main evaluated project.
+     * <P>
+     * Note: {@code getEvaluatedProjectsModel().get(getMainProjectDir())} is
+     * never {@code null}.
+     *
+     * @return the project directory of the main evaluated project. This method
+     *   never returns {@code null}.
+     */
     @Nonnull
     public File getMainProjectDir() {
         return mainProjectDir;
     }
 
-    /***/
+    /**
+     * Returns the retrieved models for the main project.
+     * <P>
+     * This method returns the same lookup as
+     * {@code getEvaluatedProjectsModel().get(getMainProjectDir())}.
+     *
+     * @return the retrieved models for the main project. This method never
+     *   returns {@code null}.
+     */
     @Nonnull
     public Lookup getMainProjectModels() {
         return mainProjectModels;
     }
 
-    /***/
+    /**
+     * Returns the retrieved models for all the evaluated projects, including
+     * the main project.
+     *
+     * @return the retrieved models for all the evaluated projects. This method
+     *   never returns {@code null}.
+     */
     @Nonnull
     public Map<File, Lookup> getEvaluatedProjectsModel() {
         return evaluatedProjectsModel;
