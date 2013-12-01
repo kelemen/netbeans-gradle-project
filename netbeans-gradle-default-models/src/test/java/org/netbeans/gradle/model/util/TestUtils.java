c47fc1d5f4e3fb18f924eacfcf39271c10705e6d
Attila Kelemen <attila.kelemen85@gmail.com>
2013-12-01 22:30:58 +0100

    It is now possible to test netbeans-gradle-default-models against multiple versions of Gradle.

--- netbeans-gradle-default-models/src/test/java/org/netbeans/gradle/model/util/TestUtils.java
+++ netbeans-gradle-default-models/src/test/java/org/netbeans/gradle/model/util/TestUtils.java
@@ -2,14 +2,49 @@ package org.netbeans.gradle.model.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
+import org.gradle.util.GradleVersion;
+import org.junit.Assume;
 import org.netbeans.gradle.model.BuildOperationArgs;
 import org.netbeans.gradle.model.OperationInitializer;
 
 public final class TestUtils {
-    private static final String MAIN_GRADLE_VERSION = "1.9";
+    private static final AtomicReference<String> TESTED_GRADLE_VERSION_REF = new AtomicReference<String>(null);
+
+    private static String normString(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        String result = value.trim();
+        if (result.length() == 0) {
+            return null;
+        }
+        return result;
+    }
+
+    private static String getTestedGradleVersion() {
+        String result = TESTED_GRADLE_VERSION_REF.get();
+        if (result == null) {
+            result = normString(System.getProperty("TESTED_GRADLE_DAEMON_VERSION"));
+            if (result == null) {
+                String skipTests = normString(System.getProperty("SKIP_GRADLE_DAEMON_TESTS"));
+                if ("true".equalsIgnoreCase(skipTests)) {
+                    result = "";
+                }
+                else {
+                    result = GradleVersion.current().getVersion();
+                }
+            }
+
+            TESTED_GRADLE_VERSION_REF.compareAndSet(null, result);
+            result = TESTED_GRADLE_VERSION_REF.get();
+        }
+        return result;
+    }
 
     private static void runTestForProject(
             String gradleVersion,
@@ -31,10 +66,15 @@ public final class TestUtils {
         }
     }
 
+    private static void assumeHasTestedGradle() {
+        Assume.assumeTrue(getTestedGradleVersion().length() != 0);
+    }
+
     public static void runTestsForProject(File projectDir, ProjectConnectionTask task) {
+        assumeHasTestedGradle();
+
         try {
-            // TODO: Run tests for other versions as well.
-            runTestForProject(MAIN_GRADLE_VERSION, projectDir, task);
+            runTestForProject(getTestedGradleVersion(), projectDir, task);
         } catch (Exception ex) {
             throw new AssertionError(ex);
         }
@@ -44,6 +84,7 @@ public final class TestUtils {
             File projectDir,
             String relativeProjectPath,
             ProjectConnectionTask task) {
+        assumeHasTestedGradle();
 
         try {
             File subDir;
