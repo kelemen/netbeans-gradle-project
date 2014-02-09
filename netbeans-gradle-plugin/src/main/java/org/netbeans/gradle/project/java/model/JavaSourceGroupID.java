3d050b109792e3b9522de38796524d4b327511a6
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-09 22:03:30 +0100

    Added equals for JavaSourceGroupID.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/model/JavaSourceGroupID.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/model/JavaSourceGroupID.java
@@ -26,4 +26,25 @@ public final class JavaSourceGroupID {
     public boolean isTest() {
         return sourceSetName.toLowerCase(Locale.US).contains("test");
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + sourceSetName.hashCode();
+        hash = 53 * hash + groupName.hashCode();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (getClass() != obj.getClass()) return false;
+
+        final JavaSourceGroupID other = (JavaSourceGroupID)obj;
+        if (!this.sourceSetName.equals(other.sourceSetName)) {
+            return false;
+        }
+        return this.groupName == other.groupName;
+    }
 }
