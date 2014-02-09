b5c78bb62285d2bae679a5ff99aa1cd54c823a80
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-09 03:17:12 +0100

    NbListedDir is now serializable.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/model/NbListedDir.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/model/NbListedDir.java
@@ -1,8 +1,11 @@
 package org.netbeans.gradle.project.java.model;
 
 import java.io.File;
+import java.io.Serializable;
 
-public final class NbListedDir {
+public final class NbListedDir implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private final String name;
     private final File directory;
 
