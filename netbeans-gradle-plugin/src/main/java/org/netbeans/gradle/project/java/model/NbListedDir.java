8fede302da826d717a20e125fd5e11d41ac628cb
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-09 19:13:34 +0100

    JavaExtensionNodes now notifies the project of the changes in the nodes.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/model/NbListedDir.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/model/NbListedDir.java
@@ -24,4 +24,25 @@ public final class NbListedDir implements Serializable {
     public File getDirectory() {
         return directory;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + name.hashCode();
+        hash = 23 * hash + directory.hashCode();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        if (getClass() != obj.getClass()) return false;
+
+        final NbListedDir other = (NbListedDir)obj;
+        if (!name.equals(other.name)) {
+            return false;
+        }
+        return directory == other.directory || directory.equals(other.directory);
+    }
 }
--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/query/JavaExtensionNodes.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/query/JavaExtensionNodes.java
@@ -1,16 +1,24 @@
 package org.netbeans.gradle.project.java.query;
 
+import java.io.File;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.project.SourceGroup;
+import org.netbeans.gradle.model.java.JavaSourceGroup;
+import org.netbeans.gradle.model.java.JavaSourceGroupName;
+import org.netbeans.gradle.model.java.JavaSourceSet;
 import org.netbeans.gradle.project.api.event.NbListenerRef;
 import org.netbeans.gradle.project.api.nodes.GradleProjectExtensionNodes;
 import org.netbeans.gradle.project.api.nodes.SingleNodeFactory;
 import org.netbeans.gradle.project.java.JavaExtension;
 import org.netbeans.gradle.project.java.JavaModelChangeListener;
 import org.netbeans.gradle.project.java.model.NamedSourceRoot;
+import org.netbeans.gradle.project.java.model.NbJavaModule;
 import org.netbeans.gradle.project.java.model.NbListedDir;
 import org.netbeans.gradle.project.java.nodes.JavaDependenciesNode;
 import org.netbeans.spi.java.project.support.ui.PackageView;
@@ -28,11 +36,13 @@ implements
 
     private final JavaExtension javaExt;
     private final ChangeSupport nodeChanges;
+    private final AtomicReference<NbJavaModule> lastModule;
 
     public JavaExtensionNodes(JavaExtension javaExt) {
         if (javaExt == null) throw new NullPointerException("javaExt");
         this.javaExt = javaExt;
         this.nodeChanges = new ChangeSupport(this);
+        this.lastModule = new AtomicReference<NbJavaModule>(null);
 
         javaExt.getSourceDirsHandler().addDirsCreatedListener(new Runnable() {
             @Override
@@ -44,17 +54,49 @@ implements
 
     @Override
     public void onModelChange() {
-        // FIXME: We currently rely on the undocumented fact, that nodes are
-        // always reloaded after a model reload.
-        //
-        // We will not call fireNodeChanges() until the implementation to
-        // request the reloading of nodes is more clever and merges multiple
-        // node reload requests into a single reload.
-        //
-        // Using UpdaterSwingTaskExecutor (always invoke later) should be
-        // sufficient.
+        NbJavaModule newModule = javaExt.getCurrentModel().getMainModule();
+        NbJavaModule prevModule = lastModule.getAndSet(newModule);
 
-        // fireNodeChanges();
+        if (hasRelevantDifferences(newModule, prevModule)) {
+            nodeChanges.fireChange();
+        }
+    }
+
+    private static boolean hasRelevantDifferences(NbJavaModule module1, NbJavaModule module2) {
+        if (module1 == module2) {
+            // In practice this happens only when they are nulls.
+            return false;
+        }
+        if (module1 == null || module2 == null) {
+            return true;
+        }
+
+        if (!module1.getListedDirs().equals(module2.getListedDirs())) {
+            return false;
+        }
+
+        Set<SourceRootID> ids1 = getSourceRootIDs(module1.getSources());
+        Set<SourceRootID> ids2 = getSourceRootIDs(module2.getSources());
+
+        return !ids1.equals(ids2);
+    }
+
+    private static Set<SourceRootID> getSourceRootIDs(List<JavaSourceSet> sourceSets) {
+        Set<SourceRootID> result = new HashSet<SourceRootID>();
+        for (JavaSourceSet sourceSet: sourceSets) {
+            getSourceRootIDs(sourceSet, result);
+        }
+        return result;
+    }
+
+    private static void getSourceRootIDs(JavaSourceSet sourceSet, Set<SourceRootID> ids) {
+        String sourceSetName = sourceSet.getName();
+        for (JavaSourceGroup group: sourceSet.getSourceGroups()) {
+            JavaSourceGroupName groupName = group.getGroupName();
+            for (File root: group.getSourceRoots()) {
+                ids.add(new SourceRootID(sourceSetName, groupName, root));
+            }
+        }
     }
 
     @Override
@@ -143,5 +185,48 @@ implements
         addDependencies(result);
 
         return result;
+    }
+
+    private static final class SourceRootID {
+        private final String sourceSetName;
+        private final JavaSourceGroupName groupName;
+        private final File sourceRoot;
+
+        public SourceRootID(String sourceSetName, JavaSourceGroupName groupName, File sourceRoot) {
+            assert sourceSetName != null;
+            assert groupName != null;
+            assert sourceRoot != null;
+
+            this.sourceSetName = sourceSetName;
+            this.groupName = groupName;
+            this.sourceRoot = sourceRoot;
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 67 * hash + sourceSetName.hashCode();
+            hash = 67 * hash + groupName.hashCode();
+            hash = 67 * hash + sourceRoot.hashCode();
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) return false;
+            if (obj == this) return true;
+            if (getClass() != obj.getClass()) return false;
+
+            final SourceRootID other = (SourceRootID)obj;
+
+            if (!sourceSetName.equals(other.sourceSetName)) {
+                return false;
+            }
+            if (groupName != other.groupName) {
+                return false;
+            }
+
+            return sourceRoot == other.sourceRoot || sourceRoot.equals(other.sourceRoot);
+        }
     }
 }
