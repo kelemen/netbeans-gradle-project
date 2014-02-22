13eddb0853f18ae8aad44b3d82b8d2f6119e8b66
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-22 20:30:01 +0100

    RerunHandler can now be implemented.

---
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/others/RerunHandler.java
@@ -0,0 +1,16 @@
+package org.netbeans.gradle.project.others;
+
+import java.util.Set;
+import javax.swing.event.ChangeListener;
+
+/**
+ * Has the same methods as org.netbeans.modules.gsf.testrunner.api.RerunHandler.
+ */
+public interface RerunHandler {
+    public void rerun();
+    public void rerun(Set<?> tests);
+    // Argument type is org.netbeans.modules.gsf.testrunner.api.RerunType.
+    public boolean enabled(Object type);
+    public void addChangeListener(ChangeListener listener);
+    public void removeChangeListener(ChangeListener listener);
+}
