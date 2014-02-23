52543510d83a17a92c78c9dcc36f9fc291717ceb
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-23 13:48:44 +0100

    Added another test to PluginClassImplementation.

--- netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/others/PluginClassImplementationTest.java
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/others/PluginClassImplementationTest.java
@@ -1,5 +1,7 @@
 package org.netbeans.gradle.project.others;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,20 +12,53 @@ public class PluginClassImplementationTest {
         return ReflectionHelper.constClassFinder(type);
     }
 
-    @Test
-    public void testTryGetAsPluginClass() {
-        Runnable wrapped = mock(Runnable.class);
-
-        PluginClassImplementation impl
-                = new PluginClassImplementation(classFinder(Runnable.class), new MyRunnable(wrapped));
-
-        Runnable instance = (Runnable)impl.tryGetAsPluginClass();
+    private static void verifyRunForwarded(Runnable instance, Runnable wrapped) {
         assertNotNull(instance);
         verifyZeroInteractions(wrapped);
 
         instance.run();
 
         verify(wrapped).run();
+    }
+
+    @Test
+    public void testDelegateToInterface() {
+        Runnable wrapped = mock(Runnable.class);
+
+        PluginClassImplementation impl
+                = new PluginClassImplementation(classFinder(Runnable.class), new MyRunnable(wrapped));
+
+        Runnable instance = (Runnable)impl.tryGetAsPluginClass();
+
+        verifyRunForwarded(instance, wrapped);
+    }
+
+    @Test
+    public void testDelegateToInterfaceWithException() throws Throwable {
+        Runnable2 wrapped = mock(Runnable2.class);
+        final InvocationHandler run2Handler = mock(InvocationHandler.class);
+
+        InvocationHandlerFactory exceptionalCase = new InvocationHandlerFactory() {
+            @Override
+            public InvocationHandler tryGetInvocationHandler(Object proxy, Method method, Object[] args) {
+                return "run2".equals(method.getName()) ? run2Handler : null;
+            }
+        };
+
+        PluginClassImplementation impl = new PluginClassImplementation(
+                classFinder(Runnable2.class),
+                new MyRunnable(wrapped),
+                exceptionalCase);
+
+        Runnable2 instance = (Runnable2)impl.tryGetAsPluginClass();
+
+        verifyRunForwarded(instance, wrapped);
+
+        verifyZeroInteractions(run2Handler);
+
+        instance.run2();
+        verify(wrapped, never()).run2();
+        verify(run2Handler).invoke(same(instance), any(Method.class), any(Object[].class));
     }
 
     // Do not implement Runnable, must be a public class
@@ -37,5 +72,9 @@ public class PluginClassImplementationTest {
         public void run() {
             wrapped.run();
         }
+    }
+
+    public interface Runnable2 extends Runnable {
+        public void run2();
     }
 }
