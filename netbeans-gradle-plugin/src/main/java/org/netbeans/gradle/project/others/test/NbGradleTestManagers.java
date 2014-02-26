4ecd2b6e6844496fa6a1ffebc48d09d887da47a0
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-26 20:36:55 +0100

    Removed setter from NbGradleTestManagers because it won't be used in tests.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/others/test/NbGradleTestManagers.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/others/test/NbGradleTestManagers.java
@@ -1,16 +1,10 @@
 package org.netbeans.gradle.project.others.test;
 
 public final class NbGradleTestManagers {
-    private static volatile NbGradleTestManager testManager = new VisualNbGradleTestManager();
+    private static final NbGradleTestManager testManager = new VisualNbGradleTestManager();
 
     public static NbGradleTestManager getTestManager() {
         return testManager;
-    }
-
-    public static void setTestManager(NbGradleTestManager newTestManager) {
-        if (newTestManager == null) throw new NullPointerException("newTestManager");
-
-        testManager = newTestManager;
     }
 
     private NbGradleTestManagers() {
