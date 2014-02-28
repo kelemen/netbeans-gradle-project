9e9f0e565b158c6840f53ae339b2b6d84fdbdbef
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-28 23:33:18 +0100

    Added tests for toSafelyPastableToJavaCode method.

---
+++ netbeans-gradle-default-models/src/test/java/org/netbeans/gradle/model/util/BasicFileUtilsTest.java
@@ -0,0 +1,53 @@
+package org.netbeans.gradle.model.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicFileUtilsTest {
+    private static final String LATIN2_STR = "\u00E1rv\u00EDzt\u0171r\u0151t\u00FCk\u00F6rf\u00FAr\u00F3g\u00E9p";
+    private static final String LATIN2_STR_ESCAPED = "\\" + "u00E1rv\\" + "u00EDzt\\" + "u0171r\\" + "u0151t\\" + "u00FCk\\" + "u00F6rf\\" + "u00FAr\\" + "u00F3g\\" + "u00E9p";
+
+    @Test
+    public void testToSafelyPastableToJavaCode0Arg() {
+        assertEquals("", BasicFileUtils.toSafelyPastableToJavaCode(""));
+    }
+
+    @Test
+    public void testToSafelyPastableToJavaCode1ArgLatin2() {
+        assertEquals(LATIN2_STR_ESCAPED, BasicFileUtils.toSafelyPastableToJavaCode(LATIN2_STR));
+    }
+
+    @Test
+    public void testToSafelyPastableToJavaCode2ArgsLatin2() {
+        String[] testStrs = {LATIN2_STR.substring(0, 3), LATIN2_STR.substring(3)};
+
+        assertEquals(LATIN2_STR_ESCAPED, BasicFileUtils.toSafelyPastableToJavaCode(testStrs));
+    }
+
+    @Test
+    public void testToSafelyPastableToJavaCode2ArgsLatin3() {
+        String[] testStrs = {LATIN2_STR.substring(0, 3), LATIN2_STR.substring(3, 7), LATIN2_STR.substring(7)};
+
+        assertEquals(LATIN2_STR_ESCAPED, BasicFileUtils.toSafelyPastableToJavaCode(testStrs));
+    }
+
+    @Test
+    public void testToSafelyPastableToJavaCode1ArgAscii() {
+        String str = "BasicString'\"";
+        assertEquals(str, BasicFileUtils.toSafelyPastableToJavaCode(str));
+    }
+
+    @Test
+    public void testToSafelyPastableToJavaCode2ArgsAscii() {
+        String str = "BasicString'\"";
+        String[] testStrs = {str.substring(0, 3), str.substring(3)};
+
+        assertEquals(str, BasicFileUtils.toSafelyPastableToJavaCode(testStrs));
+    }
+
+    @Test
+    public void testToSafelyPastableToJavaCodeEmptyStrings() {
+        assertEquals("", BasicFileUtils.toSafelyPastableToJavaCode("", "", ""));
+    }
+}
