32985b44f0f9ee79ae596a59961050548fb42061
Attila Kelemen <attila.kelemen85@gmail.com>
2014-02-28 21:45:24 +0100

    Added some tests for GlobalGradleSettings.gradleJvmArgs()

---
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties/GlobalGradleSettingsTest.java
@@ -0,0 +1,61 @@
+package org.netbeans.gradle.project.properties;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GlobalGradleSettingsTest {
+    private GlobalGradleSettings settings;
+
+    @Before
+    public void setUp() {
+        settings = new GlobalGradleSettings("GlobalGradleSettingsTest");
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    private static <String> List<String> staticList(String... elements) {
+        return Collections.unmodifiableList(Arrays.asList(elements));
+    }
+
+    private static <T> void testGetAndSet(MutableProperty<T> property, T value) {
+        testGetAndSet(property, value, value);
+    }
+
+    private static <T> void testGetAndSet(MutableProperty<T> property, T value, T expected) {
+        property.setValue(value);
+        assertEquals(expected, property.getValue());
+    }
+
+    @Test
+    public void testJvmArgs_0Arg() {
+        testGetAndSet(settings.gradleJvmArgs(), Collections.<String>emptyList(), null);
+    }
+
+    @Test
+    public void testJvmArgs_1Arg() {
+        testGetAndSet(settings.gradleJvmArgs(), staticList("arg1"));
+    }
+
+    @Test
+    public void testJvmArgs_2Args() {
+        testGetAndSet(settings.gradleJvmArgs(), staticList("arg1", "arg2"));
+    }
+
+    @Test
+    public void testJvmArgs_ManyArgs() {
+        testGetAndSet(settings.gradleJvmArgs(), staticList("arg1", "arg2", "arg3", "arg4", "arg5"));
+    }
+
+    @Test
+    public void testJvmArgs_NonAsciiChar() {
+        testGetAndSet(settings.gradleJvmArgs(), staticList("arg1\uA356\u0120sd"));
+    }
+}
