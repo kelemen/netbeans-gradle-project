e8f8ca2de428586d37de9b6aac6b9d3b3b91b379
Attila Kelemen <attila.kelemen85@gmail.com>
2014-03-02 02:35:27 +0100

    NbListenerRef now extends ListenerRef.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/event/NbListenerRef.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/event/NbListenerRef.java
@@ -1,5 +1,7 @@
 package org.netbeans.gradle.project.api.event;
 
+import org.jtrim.event.ListenerRef;
+
 /**
  * Defines a reference of an event handler which has been registered to be
  * notified of a certain event. The event handler can be
@@ -22,36 +24,7 @@ package org.netbeans.gradle.project.api.event;
  * <I>synchronization transparent</I>.
  *
  * @see NbListenerRefs
+ * @see ListenerRef
  */
-public interface NbListenerRef {
-    /**
-     * Checks whether the listener is currently registered to receive
-     * notifications of events.
-     * <P>
-     * In case this method returns {@code true}, the method
-     * {@link #unregister() unregister()} can be called to stop the listener
-     * from receiving notifications of the events occurring. If this method
-     * returns {@code false}, the {@code unregister()} method has no useful
-     * effect and listener notifications can be expected to stop at some time
-     * in the future. Note that even if this method returns {@code false}, some
-     * pending event notifications might be forwarded.
-     *
-     * @return {@code true} if the listener is currently registered to be
-     *   notified of occurring events, {@code false} if the listener was
-     *   unregistered, so calling {@code unregister} is no longer necessary
-     */
-    public boolean isRegistered();
-
-    /**
-     * Unregisters the listener, so it does not need to be notified of
-     * subsequent events. Calling this method ensures that there will be a point
-     * in time in the future from when notifications of new events will no
-     * longer be forwarded. That is, it is possible that some subsequent events
-     * will still be forwarded to the associated listener but forwarding of
-     * these events will eventually stop without further interaction.
-     * <P>
-     * This method must be idempotent. That is, invoking it multiple times has
-     * the same effect as invoking it only once.
-     */
-    public void unregister();
+public interface NbListenerRef extends ListenerRef {
 }
