247130e72f5c41560057ffa971857e9d35394eb7
Attila Kelemen <attila.kelemen85@gmail.com>
2014-03-05 23:41:17 +0100

    Added convenience constructor for ConfigKey.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ConfigKey.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ConfigKey.java
@@ -2,10 +2,15 @@ package org.netbeans.gradle.project.properties2;
 
 import java.util.Objects;
 import javax.annotation.Nullable;
+import org.w3c.dom.Node;
 
 public final class ConfigKey {
     private final String name;
     private final String namespace;
+
+    public ConfigKey(Node node) {
+        this(node.getNodeName(), node.getNamespaceURI());
+    }
 
     public ConfigKey(String name, String namespace) {
         this.name = name;
