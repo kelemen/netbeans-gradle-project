e080695c7b9e651943c560c6fd8db2edb94abca3
Attila Kelemen <attila.kelemen85@gmail.com>
2014-03-06 01:17:43 +0100

    Annotated ValueMerger.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ValueMerger.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ValueMerger.java
@@ -1,5 +1,7 @@
 package org.netbeans.gradle.project.properties2;
 
+import javax.annotation.Nonnull;
+
 public interface ValueMerger<ValueType> {
-    public ValueType mergeValues(ValueType child, ValueReference<ValueType> parent);
+    public ValueType mergeValues(ValueType child, @Nonnull ValueReference<ValueType> parent);
 }
