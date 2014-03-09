a0f3ce64e4210c65aafd7362cb58ac505e999af1
Attila Kelemen <attila.kelemen85@gmail.com>
2014-03-09 21:05:18 +0100

    Added a few more tests to ConfigTree

--- netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties2/ConfigTreeTest.java
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties2/ConfigTreeTest.java
@@ -1,5 +1,6 @@
 package org.netbeans.gradle.project.properties2;
 
+import java.util.Map;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -60,5 +61,61 @@ public class ConfigTreeTest {
 
         verifyValue(tree, value);
         verifyNoChildren(tree);
+    }
+
+    @Test
+    public void testSingleValueByFactoryNull() {
+        assertSame(ConfigTree.EMPTY, ConfigTree.singleValue(null));
+    }
+
+    @Test
+    public void testSingleValueByFactory() {
+        final String value = "my-value";
+
+        ConfigTree tree = ConfigTree.singleValue(value);
+        assertTrue(tree.hasValues());
+
+        verifyValue(tree, value);
+        verifyNoChildren(tree);
+    }
+
+    @Test
+    public void testBasicBuilder() {
+        ConfigTree.Builder builder = new ConfigTree.Builder();
+
+        builder.setValue("my-value");
+        ConfigTree.Builder subBuilder = builder.getSubBuilder("sub-key0");
+        subBuilder.setValue("deep-value");
+
+        ConfigTree.Builder subSubBuilder = builder.getDeepSubBuilder("sub-key1", "sub-key2");
+        subSubBuilder.setValue("deeper-value");
+
+        ConfigTree tree = builder.create();
+        verifyTrivial(tree);
+        assertTrue(tree.hasValues());
+        verifyValue(tree, "my-value");
+
+        Map<ConfigKey, ConfigTree> subTrees = tree.getSubTrees();
+        assertEquals(2, subTrees.size());
+
+        ConfigTree subTree1 = subTrees.get(new ConfigKey("sub-key0", null));
+        assertTrue(subTree1.hasValues());
+        verifyTrivial(subTree1);
+        verifyValue(subTree1, "deep-value");
+        verifyNoChildren(subTree1);
+
+        ConfigTree subTree2 = subTrees.get(new ConfigKey("sub-key1", null));
+        assertTrue(subTree2.hasValues());
+        verifyTrivial(subTree2);
+        verifyNoValue(subTree2);
+
+        Map<ConfigKey, ConfigTree> subSubTrees = subTree2.getSubTrees();
+        assertEquals(1, subSubTrees.size());
+
+        ConfigTree subTree3 = subSubTrees.get(new ConfigKey("sub-key2", null));
+        assertTrue(subTree3.hasValues());
+        verifyTrivial(subTree3);
+        verifyValue(subTree3, "deeper-value");
+        verifyNoChildren(subTree3);
     }
 }
