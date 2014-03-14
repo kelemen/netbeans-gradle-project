555fc565fc73f41ec8c5728cd29c3d93c2a419cb
Attila Kelemen <attila.kelemen85@gmail.com>
2014-03-14 22:00:27 +0100

    Added TestResourceUtils.getResourceLines

--- netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties2/TestResourceUtils.java
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties2/TestResourceUtils.java
@@ -2,11 +2,31 @@ package org.netbeans.gradle.project.properties2;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.Reader;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 
 final class TestResourceUtils {
     private static String getResourcePath(String relPath) {
         return TestResourceUtils.class.getPackage().getName().replace('.', '/') + "/" + relPath;
+    }
+
+    public static String[] getResourceLines(String relPath, String encoding) throws IOException {
+        List<String> lines = new LinkedList<>();
+        try (InputStream input = openResource(relPath);
+                Reader reader = new InputStreamReader(input, encoding);
+                LineNumberReader lineReader = new LineNumberReader(reader, 8 * 1024)) {
+
+            String line = lineReader.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = lineReader.readLine();
+            }
+        }
+        return lines.toArray(new String[lines.size()]);
     }
 
     public static InputStream openResource(String relPath) throws IOException {
