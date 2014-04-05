a0c497d47279888563aae47344dd150e6710af3e
Attila Kelemen <attila.kelemen85@gmail.com>
2014-04-05 02:18:10 +0200

    Adjusted NbGradleTestSuite.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/others/test/NbGradleTestSuite.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/others/test/NbGradleTestSuite.java
@@ -4,5 +4,8 @@ import org.netbeans.modules.gsf.testrunner.api.Testcase;
 
 public interface NbGradleTestSuite {
     public Testcase addTestcase(String name);
+    public void setStdOut(String stdOut);
+    public void setStdErr(String stdErr);
+
     public void endSuite(long elapsedTimeInMillis);
 }
