7dc8cefd419f9c523a09a27efdc4ca1b4e5768f8
Attila Kelemen <attila.kelemen85@gmail.com>
2014-05-19 21:36:00 +0200

    Fixed array printing in toString of ProfileSettings.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ProfileSettings.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ProfileSettings.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -494,7 +495,7 @@ public final class ProfileSettings {
 
         @Override
         public String toString() {
-            return "Property{" + configPaths + '}';
+            return "Property{" + Arrays.toString(configPaths) + '}';
         }
     }
 
