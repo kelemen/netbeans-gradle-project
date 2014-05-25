8c03f42f58d5a6b93bd504363803f14a9c9434dc
Attila Kelemen <attila.kelemen85@gmail.com>
2014-05-25 14:06:36 +0200

    Added ProfileSettingsContainer.

---
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ProfileSettingsContainer.java
@@ -0,0 +1,38 @@
+package org.netbeans.gradle.project.properties2;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import org.jtrim.utils.ExceptionHelper;
+import org.netbeans.gradle.project.properties.WeakValueHashMap;
+
+public final class ProfileSettingsContainer {
+    private static final ProfileSettingsContainer DEFAULT = new ProfileSettingsContainer();
+
+    private final Lock mainLock;
+    private final WeakValueHashMap<ProfileSettingsKey, ProjectProfileSettings> loaded;
+
+    private ProfileSettingsContainer() {
+        this.mainLock = new ReentrantLock();
+        this.loaded = new WeakValueHashMap<>();
+    }
+
+    public static ProfileSettingsContainer getDefault() {
+        return DEFAULT;
+    }
+
+    public ProjectProfileSettings getProfileSettings(ProfileSettingsKey key) {
+        ExceptionHelper.checkNotNullArgument(key, "key");
+
+        mainLock.lock();
+        try {
+            ProjectProfileSettings result = loaded.get(key);
+            if (result == null) {
+                result = new ProjectProfileSettings(key);
+                loaded.put(key, result);
+            }
+            return result;
+        } finally {
+            mainLock.unlock();
+        }
+    }
+}
