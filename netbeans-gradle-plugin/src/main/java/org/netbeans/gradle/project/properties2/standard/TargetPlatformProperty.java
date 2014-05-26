58c8fd9d41f3d59086e44126125db908a231b6e2
Attila Kelemen <attila.kelemen85@gmail.com>
2014-05-26 02:09:36 +0200

    Added TargetPlatformProperty.getProperty

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/TargetPlatformProperty.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/TargetPlatformProperty.java
@@ -2,6 +2,8 @@ package org.netbeans.gradle.project.properties2.standard;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Arrays;
+import java.util.List;
 import org.jtrim.property.PropertyFactory;
 import org.jtrim.property.PropertySource;
 import org.jtrim.property.ValueConverter;
@@ -12,8 +14,10 @@ import org.netbeans.api.java.platform.JavaPlatform;
 import org.netbeans.api.java.platform.JavaPlatformManager;
 import org.netbeans.api.java.platform.Specification;
 import org.netbeans.gradle.project.properties.DefaultPropertySources;
+import org.netbeans.gradle.project.properties2.ConfigPath;
 import org.netbeans.gradle.project.properties2.ConfigTree;
 import org.netbeans.gradle.project.properties2.PlatformId;
+import org.netbeans.gradle.project.properties2.ProjectProfileSettings;
 import org.netbeans.gradle.project.properties2.PropertyDef;
 import org.netbeans.gradle.project.properties2.PropertyKeyEncodingDef;
 import org.netbeans.gradle.project.properties2.PropertyValueDef;
@@ -23,8 +27,20 @@ import org.openide.modules.SpecificationVersion;
 
 public final class TargetPlatformProperty {
     private static final String DEFAULT_PLATFORM_VERSION = getDefaultPlatformVersion("1.7");
+    private static final PropertyDef<PlatformId, JavaPlatform> PROPERTY_DEF = createPropertyDef();
+
+    public static PropertySource<JavaPlatform> getProperty(ProjectProfileSettings settings) {
+        List<ConfigPath> paths = Arrays.asList(
+                ConfigPath.fromKeys("target-platform-name"),
+                ConfigPath.fromKeys("target-platform"));
+        return settings.getProperty(paths, getPropertyDef());
+    }
 
     public static PropertyDef<PlatformId, JavaPlatform> getPropertyDef() {
+        return PROPERTY_DEF;
+    }
+
+    private static PropertyDef<PlatformId, JavaPlatform> createPropertyDef() {
         PropertyDef.Builder<PlatformId, JavaPlatform> result = new PropertyDef.Builder<>();
         result.setKeyEncodingDef(getEncodingDef());
         result.setValueDef(getValueDef());
