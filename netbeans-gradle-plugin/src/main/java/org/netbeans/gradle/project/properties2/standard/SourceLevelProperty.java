d7823d8ca58115227b9ff3ac667cf4db83f4e09c
Attila Kelemen <attila.kelemen85@gmail.com>
2014-06-01 18:33:55 +0200

    Added SourceLevelProperty.getProperty

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/SourceLevelProperty.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/SourceLevelProperty.java
@@ -1,11 +1,23 @@
 package org.netbeans.gradle.project.properties2.standard;
 
+import java.util.Arrays;
+import java.util.List;
+import org.jtrim.property.PropertySource;
+import org.netbeans.gradle.project.properties2.ConfigPath;
 import org.netbeans.gradle.project.properties2.ConfigTree;
+import org.netbeans.gradle.project.properties2.ProjectProfileSettings;
 import org.netbeans.gradle.project.properties2.PropertyDef;
 import org.netbeans.gradle.project.properties2.PropertyKeyEncodingDef;
 
+
 public final class SourceLevelProperty {
     private static final PropertyDef<String, String> PROPERTY_DEF = createPropertyDef();
+    private static final String CONFIG_KEY_SOURCE_LEVEL = "source-level";
+
+    public static PropertySource<String> getProperty(ProjectProfileSettings settings) {
+        List<ConfigPath> paths = Arrays.asList(ConfigPath.fromKeys(CONFIG_KEY_SOURCE_LEVEL));
+        return settings.getProperty(paths, getPropertyDef());
+    }
 
     public static PropertyDef<String, String> getPropertyDef() {
         return PROPERTY_DEF;
