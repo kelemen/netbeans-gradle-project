e1a0b91444e0c451ae7108efa1fe71a52954307d
Attila Kelemen <attila.kelemen85@gmail.com>
2014-06-01 18:28:22 +0200

    Added SourceLevelProperty definition.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/SourceLevelProperty.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/SourceLevelProperty.java
@@ -1,9 +1,24 @@
 package org.netbeans.gradle.project.properties2.standard;
 
 import org.netbeans.gradle.project.properties2.ConfigTree;
+import org.netbeans.gradle.project.properties2.PropertyDef;
 import org.netbeans.gradle.project.properties2.PropertyKeyEncodingDef;
 
 public final class SourceLevelProperty {
+    private static final PropertyDef<String, String> PROPERTY_DEF = createPropertyDef();
+
+    public static PropertyDef<String, String> getPropertyDef() {
+        return PROPERTY_DEF;
+    }
+
+    private static PropertyDef<String, String> createPropertyDef() {
+        PropertyDef.Builder<String, String> result = new PropertyDef.Builder<>();
+        result.setKeyEncodingDef(getEncodingDef());
+        result.setValueDef(CommonProperties.<String>getIdentityValueDef());
+        result.setValueMerger(CommonProperties.<String>getParentIfNullValueMerger());
+        return result.create();
+    }
+
     public static PropertyKeyEncodingDef<String> getEncodingDef() {
         return new PropertyKeyEncodingDef<String>() {
             @Override
