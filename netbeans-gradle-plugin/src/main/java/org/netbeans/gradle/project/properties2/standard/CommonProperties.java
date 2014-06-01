b32b5e398099c8792f85978a96a9c9146d444872
Attila Kelemen <attila.kelemen85@gmail.com>
2014-06-01 18:24:23 +0200

    Added IdentityValueDef

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/CommonProperties.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/CommonProperties.java
@@ -1,10 +1,17 @@
 package org.netbeans.gradle.project.properties2.standard;
 
+import org.jtrim.property.PropertyFactory;
+import org.jtrim.property.PropertySource;
 import org.netbeans.api.java.platform.JavaPlatform;
+import org.netbeans.gradle.project.properties2.PropertyValueDef;
 import org.netbeans.gradle.project.properties2.ValueMerger;
 import org.netbeans.gradle.project.properties2.ValueReference;
 
 public final class CommonProperties {
+    public static PropertyValueDef<String, String> getIdentityValueDef() {
+        return IdentityValueDef.INSTANCE;
+    }
+
     public static ValueMerger<JavaPlatform> getParentIfNullValueMerger() {
         return ParentIfNullValueMerger.INSTANCE;
     }
@@ -19,6 +26,20 @@ public final class CommonProperties {
         @Override
         public JavaPlatform mergeValues(JavaPlatform child, ValueReference<JavaPlatform> parent) {
             return child != null ? child : parent.getValue();
+        }
+    }
+
+    private enum IdentityValueDef implements PropertyValueDef<String, String> {
+        INSTANCE;
+
+        @Override
+        public PropertySource<String> property(String valueKey) {
+            return PropertyFactory.constSource(valueKey);
+        }
+
+        @Override
+        public String getKeyFromValue(String value) {
+            return value;
         }
     }
 }
