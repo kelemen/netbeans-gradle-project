edd79d43f726b8212445526a37799b6a5c6a79c1
Attila Kelemen <attila.kelemen85@gmail.com>
2014-06-01 21:50:24 +0200

    Added GradleLocationProperty.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/GradleLocationProperty.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/GradleLocationProperty.java
@@ -3,17 +3,61 @@ package org.netbeans.gradle.project.properties2.standard;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jtrim.property.PropertyFactory;
+import org.jtrim.property.PropertySource;
 import org.jtrim.utils.ExceptionHelper;
 import org.netbeans.gradle.project.properties.GradleLocation;
 import org.netbeans.gradle.project.properties.GradleLocationDefault;
 import org.netbeans.gradle.project.properties.GradleLocationDirectory;
 import org.netbeans.gradle.project.properties.GradleLocationDistribution;
 import org.netbeans.gradle.project.properties.GradleLocationVersion;
+import org.netbeans.gradle.project.properties2.ConfigPath;
+import org.netbeans.gradle.project.properties2.ProjectProfileSettings;
+import org.netbeans.gradle.project.properties2.PropertyDef;
+import org.netbeans.gradle.project.properties2.PropertyValueDef;
 
 public final class GradleLocationProperty {
     private static final Logger LOGGER = Logger.getLogger(GradleLocationProperty.class.getName());
+
+    private static final PropertyDef<String, GradleLocation> PROPERTY_DEF = createPropertyDef();
+    private static final String CONFIG_KEY_SOURCE_ENCODING = "source-encoding";
+
+    public static PropertySource<GradleLocation> getProperty(ProjectProfileSettings settings) {
+        List<ConfigPath> paths = Arrays.asList(ConfigPath.fromKeys(CONFIG_KEY_SOURCE_ENCODING));
+        return settings.getProperty(paths, getPropertyDef());
+    }
+
+    public static PropertyDef<String, GradleLocation> getPropertyDef() {
+        return PROPERTY_DEF;
+    }
+
+    private static PropertyDef<String, GradleLocation> createPropertyDef() {
+        PropertyDef.Builder<String, GradleLocation> result = new PropertyDef.Builder<>();
+        result.setKeyEncodingDef(CommonProperties.getIdentityKeyEncodingDef());
+        result.setValueDef(getValueDef());
+        return result.create();
+    }
+
+    private static GradleLocation tryGetGradleLocationFromString(String gradleLocation) {
+        return gradleLocation != null
+                ? getGradleLocationFromString(gradleLocation)
+                : null;
+    }
+
+    public static String gradleLocationToString(GradleLocation gradleLocation) {
+        String value = gradleLocation.asString();
+        if (value == null) {
+            return "";
+        }
+
+        String typeName = gradleLocation.getUniqueTypeName();
+        return "?" + typeName + "=" + value;
+    }
 
     public static GradleLocation getGradleLocationFromString(String gradleLocation) {
         ExceptionHelper.checkNotNullArgument(gradleLocation, "gradleLocation");
@@ -49,6 +93,22 @@ public final class GradleLocationProperty {
         }
 
         return new GradleLocationDirectory(new File(location));
+    }
+
+    private static PropertyValueDef<String, GradleLocation> getValueDef() {
+        return new PropertyValueDef<String, GradleLocation>() {
+            @Override
+            public PropertySource<GradleLocation> property(String valueKey) {
+                return PropertyFactory.constSource(tryGetGradleLocationFromString(valueKey));
+            }
+
+            @Override
+            public String getKeyFromValue(GradleLocation value) {
+                return value != null
+                        ? gradleLocationToString(value)
+                        : null;
+            }
+        };
     }
 
     private GradleLocationProperty() {
