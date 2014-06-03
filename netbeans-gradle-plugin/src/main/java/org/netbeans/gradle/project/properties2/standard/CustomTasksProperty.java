a3d7b94f482cd422002f4281f8aa45d3c182dd9a
Attila Kelemen <attila.kelemen85@gmail.com>
2014-06-04 00:25:36 +0200

    Added proper ValueMerger for CustomTasksProperty.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/CustomTasksProperty.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/CustomTasksProperty.java
@@ -3,6 +3,7 @@ package org.netbeans.gradle.project.properties2.standard;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.jtrim.collections.CollectionsEx;
 import org.jtrim.property.PropertySource;
 import org.netbeans.gradle.project.properties.PredefinedTask;
 import org.netbeans.gradle.project.properties2.ConfigPath;
@@ -10,6 +11,8 @@ import org.netbeans.gradle.project.properties2.ConfigTree;
 import org.netbeans.gradle.project.properties2.ProjectProfileSettings;
 import org.netbeans.gradle.project.properties2.PropertyDef;
 import org.netbeans.gradle.project.properties2.PropertyKeyEncodingDef;
+import org.netbeans.gradle.project.properties2.ValueMerger;
+import org.netbeans.gradle.project.properties2.ValueReference;
 
 public final class CustomTasksProperty {
     private static final PropertyDef<CustomTasks, CustomTasks> PROPERTY_DEF = createPropertyDef();
@@ -39,6 +42,7 @@ public final class CustomTasksProperty {
         PropertyDef.Builder<CustomTasks, CustomTasks> result = new PropertyDef.Builder<>();
         result.setKeyEncodingDef(getKeyEncodingDef());
         result.setValueDef(CommonProperties.<CustomTasks>getIdentityValueDef());
+        result.setValueMerger(getValueMerger());
         return result.create();
     }
 
@@ -141,6 +145,24 @@ public final class CustomTasksProperty {
             @Override
             public ConfigTree encode(CustomTasks value) {
                 return encodeConfig(value);
+            }
+        };
+    }
+
+    private static ValueMerger<CustomTasks> getValueMerger() {
+        return new ValueMerger<CustomTasks>() {
+            @Override
+            public CustomTasks mergeValues(CustomTasks child, ValueReference<CustomTasks> parent) {
+                if (child == null) {
+                    return parent.getValue();
+                }
+
+                CustomTasks parentValue = parent.getValue();
+                if (parentValue == null) {
+                    return child;
+                }
+
+                return new CustomTasks(CollectionsEx.viewConcatList(child.getTasks(), parentValue.getTasks()));
             }
         };
     }
