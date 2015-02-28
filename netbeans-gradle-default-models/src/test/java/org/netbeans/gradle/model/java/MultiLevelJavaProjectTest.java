81cd7fdd86202e9365518000e440754a67791f23
Attila Kelemen <attila.kelemen85@gmail.com>
2015-02-28 16:19:39 +0100

    Added test for reading the exclude and include (source) patterns from the project model.

--- netbeans-gradle-default-models/src/test/java/org/netbeans/gradle/model/java/MultiLevelJavaProjectTest.java
+++ netbeans-gradle-default-models/src/test/java/org/netbeans/gradle/model/java/MultiLevelJavaProjectTest.java
@@ -332,6 +332,55 @@ public class MultiLevelJavaProjectTest {
         testJavaSourcesModelForJavaProject("libs:lib3:lib2", sourcesOfLib3Lib2());
     }
 
+    private static JavaSourceGroup findSourceGroup(JavaSourceSet sourceSet, JavaSourceGroupName name) {
+        for (JavaSourceGroup group: sourceSet.getSourceGroups()) {
+            if (name.equals(group.getGroupName())) {
+                return group;
+            }
+        }
+
+        return null;
+    }
+
+    private static JavaSourceSet findSourceSet(JavaSourcesModel sourcesModel, String name) {
+        for (JavaSourceSet sourceSet: sourcesModel.getSourceSets()) {
+            if (name.equals(sourceSet.getName())) {
+                return sourceSet;
+            }
+        }
+
+        return null;
+    }
+
+    private static JavaSourceGroup findSourceGroup(
+            JavaSourcesModel sourcesModel,
+            String setName,
+            JavaSourceGroupName groupName) {
+        JavaSourceSet sourceSet = findSourceSet(sourcesModel, setName);
+        if (sourceSet == null) {
+            return null;
+        }
+
+        return findSourceGroup(sourceSet, groupName);
+    }
+
+    @Test
+    public void testExcludeIncludePatterns() throws IOException {
+        runTestForSubProject("apps:app1", new ProjectConnectionTask() {
+            public void doTask(ProjectConnection connection) throws Exception {
+                JavaSourcesModel sourcesModel
+                        = fetchSingleProjectInfo(connection, JavaSourcesModelBuilder.ONLY_COMPILE);
+                assertNotNull("apps:app1 must have a JavaSourcesModel.", sourcesModel);
+
+                JavaSourceGroup group = findSourceGroup(sourcesModel, JavaSourceSet.NAME_MAIN, JavaSourceGroupName.JAVA);
+                assertNotNull("apps:app1 must not have a main/java source group.", group);
+
+                assertEquals("includes", Collections.singleton("**"), group.getIncludes());
+                assertEquals("excludes", Collections.singleton("**/excluded/"), group.getExcludes());
+            }
+        });
+    }
+
     @Test
     public void testJavaSourcesModelForRoot() throws IOException {
         runTestForSubProject("", new ProjectConnectionTask() {
