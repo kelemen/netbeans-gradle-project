34ae989552230d54356cf38bb596e8caf2994c3e
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-08 23:32:33 +0100

    Added new utility method to JavaPlatformUtils.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/JavaPlatformUtils.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/standard/JavaPlatformUtils.java
@@ -5,16 +5,21 @@ import java.beans.PropertyChangeListener;
 import java.util.List;
 import org.jtrim.event.ListenerRef;
 import org.jtrim.event.ListenerRegistries;
+import org.jtrim.property.PropertyFactory;
 import org.jtrim.property.PropertySource;
+import org.jtrim.property.ValueConverter;
 import org.jtrim.property.swing.SwingForwarderFactory;
 import org.jtrim.property.swing.SwingProperties;
 import org.jtrim.property.swing.SwingPropertySource;
 import org.jtrim.utils.ExceptionHelper;
 import org.netbeans.api.java.platform.JavaPlatform;
 import org.netbeans.api.java.platform.JavaPlatformManager;
+import org.netbeans.api.java.platform.Specification;
+import org.netbeans.gradle.project.properties.DefaultPropertySources;
 import org.netbeans.gradle.project.properties.GlobalGradleSettings;
 import org.netbeans.gradle.project.properties.PlatformOrder;
 import org.netbeans.gradle.project.properties.StringBasedProperty;
+import org.netbeans.gradle.project.properties2.PropertyValueDef;
 
 public final class JavaPlatformUtils {
     private static final PropertySource<List<JavaPlatform>> JAVA_PLATFORMS
@@ -22,6 +27,45 @@ public final class JavaPlatformUtils {
 
     public static PropertySource<List<JavaPlatform>> javaPlatforms() {
         return JAVA_PLATFORMS;
+    }
+
+    public static PropertyValueDef<PlatformId, JavaPlatform> getPlatformIdValueDef() {
+        return new PropertyValueDef<PlatformId, JavaPlatform>() {
+            @Override
+            public PropertySource<JavaPlatform> property(PlatformId valueKey) {
+                return javaPlatform(valueKey);
+            }
+
+            @Override
+            public PlatformId getKeyFromValue(JavaPlatform value) {
+                Specification specification = value != null
+                        ? value.getSpecification()
+                        : null;
+
+                if (specification == null) {
+                    return null;
+                }
+
+                String name = specification.getName();
+                String version = specification.getVersion().toString();
+                return new PlatformId(name, version);
+            }
+        };
+    }
+
+    private static PropertySource<JavaPlatform> javaPlatform(final PlatformId valueKey) {
+        if (valueKey == null) {
+            return PropertyFactory.constSource(null);
+        }
+
+        PropertySource<List<JavaPlatform>> javaPlatforms = JavaPlatformUtils.javaPlatforms();
+
+        return PropertyFactory.convert(javaPlatforms, new ValueConverter<List<JavaPlatform>, JavaPlatform>() {
+            @Override
+            public JavaPlatform convert(List<JavaPlatform> input) {
+                return DefaultPropertySources.tryChooseFromPlatforms(valueKey.getName(), valueKey.getVersion(), input);
+            }
+        });
     }
 
     private static SwingPropertySource<JavaPlatform[], PropertyChangeListener> rawUnorderedJavaPlatforms() {
