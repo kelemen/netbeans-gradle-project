bbcb95bf76f40224c9cdff7666fc6142a58bce80
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-10 20:51:34 +0100

    ProfileSettingsKey.key is now allowed to be null. null means the default profile (i.e., .nb-gradle-properties)

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ProfileSettingsKey.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties2/ProfileSettingsKey.java
@@ -2,24 +2,27 @@ package org.netbeans.gradle.project.properties2;
 
 import java.nio.file.Path;
 import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.jtrim.utils.ExceptionHelper;
 
 public final class ProfileSettingsKey {
     private final Path projectDir;
     private final ProfileKey key;
 
-    public ProfileSettingsKey(Path projectDir, ProfileKey key) {
+    public ProfileSettingsKey(@Nonnull Path projectDir, @Nullable ProfileKey key) {
         ExceptionHelper.checkNotNullArgument(projectDir, "projectDir");
-        ExceptionHelper.checkNotNullArgument(key, "key");
 
         this.projectDir = projectDir;
         this.key = key;
     }
 
+    @Nonnull
     public Path getProjectDir() {
         return projectDir;
     }
 
+    @Nullable
     public ProfileKey getKey() {
         return key;
     }
