95bdfedf6f2c211923ef0bfb4ae8b522dcb2f0f0
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-17 10:20:34 +0100

    Revert "GradleAuxiliaryConfiguration now relies on LazyAuxProperties." (Since waiting for project load is now safe, relying on LazyAuxProperties is no longer needed)
    This reverts commit 6f52e1d060926ed047ddc3eab48343b3d5259780.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties/GradleAuxiliaryConfiguration.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/properties/GradleAuxiliaryConfiguration.java
@@ -1,28 +1,66 @@
 package org.netbeans.gradle.project.properties;
 
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicReference;
 import org.jtrim.utils.ExceptionHelper;
 import org.netbeans.gradle.project.NbGradleProject;
-import org.netbeans.gradle.project.properties2.ProfileKey;
+import org.netbeans.gradle.project.properties2.ProjectProfileSettings;
 import org.netbeans.spi.project.AuxiliaryConfiguration;
 import org.w3c.dom.Element;
 
 public final class GradleAuxiliaryConfiguration implements AuxiliaryConfiguration {
-    private final LazyAuxProperties sharedConfig;
-    private final LazyAuxProperties privateConfig;
+    private final NbGradleProject project;
+    private final AtomicReference<ProjectProfileSettings> sharedConfigRef;
+    private final AtomicReference<ProjectProfileSettings> privateConfigRef;
 
     public GradleAuxiliaryConfiguration(NbGradleProject project) {
         ExceptionHelper.checkNotNullArgument(project, "project");
 
+        this.project = project;
+
         // This object is created before the lookup of the project is created
         // and therefore we cannot yet request the profiles because the profile
         // retrieval requires the lookup to be laready built.
-        this.sharedConfig = new LazyAuxProperties(project, null);
-        this.privateConfig = new LazyAuxProperties(project, ProfileKey.PRIVATE_PROFILE);
+        this.sharedConfigRef = new AtomicReference<>(null);
+        this.privateConfigRef = new AtomicReference<>(null);
     }
 
-    private LazyAuxProperties getConfig(boolean shared) {
-        return shared ? sharedConfig : privateConfig;
+    private static ProjectProfileSettings getSharedProperties(NbGradleProject project) {
+        return project.getPropertiesForProfile(null);
+    }
+
+    private static ProjectProfileSettings getPrivateProperties(NbGradleProject project) {
+        return project.getPrivateProfile();
+    }
+
+    private ProjectProfileSettings getSharedProperties() {
+        ProjectProfileSettings result = sharedConfigRef.get();
+        if (result == null) {
+            result = getSharedProperties(project);
+            if (!sharedConfigRef.compareAndSet(null, result)) {
+                result = sharedConfigRef.get();
+            }
+        }
+
+        return result;
+    }
+
+    private ProjectProfileSettings getPrivateProperties() {
+        ProjectProfileSettings result = privateConfigRef.get();
+        if (result == null) {
+            result = getPrivateProperties(project);
+            if (!privateConfigRef.compareAndSet(null, result)) {
+                result = privateConfigRef.get();
+            }
+        }
+
+        return result;
+    }
+
+    private ProjectProfileSettings getConfig(boolean shared) {
+        ProjectProfileSettings result = shared ? getSharedProperties() : getPrivateProperties();
+        result.ensureLoadedAndWait();
+        return result;
     }
 
     @Override
@@ -32,7 +70,7 @@ public final class GradleAuxiliaryConfiguration implements AuxiliaryConfiguratio
 
     @Override
     public void putConfigurationFragment(Element fragment, boolean shared) throws IllegalArgumentException {
-        LazyAuxProperties config = getConfig(shared);
+        ProjectProfileSettings config = getConfig(shared);
         config.setAuxConfigValue(keyFromElement(fragment), fragment);
     }
 
@@ -42,7 +80,7 @@ public final class GradleAuxiliaryConfiguration implements AuxiliaryConfiguratio
             String namespace,
             boolean shared) throws IllegalArgumentException {
 
-        LazyAuxProperties config = getConfig(shared);
+        ProjectProfileSettings config = getConfig(shared);
         return config.setAuxConfigValue(new DomElementKey(elementName, namespace), null);
     }
 
