a5295c30094c7ebd341df927b8854a4d0e38c48b
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-19 16:39:34 +0100

    Added missing javadoc to GlobalConfig.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/GlobalConfig.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/GlobalConfig.java
@@ -32,6 +32,18 @@ public final class GlobalConfig {
         return SKIP_TESTS;
     }
 
+    /**
+     * Returns the property indicating if the check task should be skipped when executing
+     * tasks not directly related to check (such as build). Extensions should
+     * consider this value when providing their own
+     * {@link org.netbeans.gradle.project.api.task.BuiltInGradleCommandQuery custom built-in tasks}.
+     * <P>
+     * The value of the returned property is never {@code null}.
+     *
+     * @return the property indicating if the check task should be skipped when executing
+     *   tasks not directly related to testing (such as build). This method
+     *   never returns {@code null}.
+     */
     @Nonnull
     public static NbPropertySource<Boolean> skipCheck() {
       return SKIP_CHECK;
