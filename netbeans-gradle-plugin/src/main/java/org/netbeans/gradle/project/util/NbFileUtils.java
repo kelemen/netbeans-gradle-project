a0399901fabd50d424a0828c3385ec107e581e46
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-19 15:18:36 +0100

    Added NbFileUtils.writeLinesToFile

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/util/NbFileUtils.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/util/NbFileUtils.java
@@ -1,16 +1,22 @@
 package org.netbeans.gradle.project.util;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collection;
 import org.jtrim.cancel.CancellationToken;
 import org.jtrim.utils.ExceptionHelper;
+import org.netbeans.api.project.Project;
+import org.netbeans.gradle.project.others.ChangeLFPlugin;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 
@@ -115,6 +121,46 @@ public final class NbFileUtils {
         }
 
         deleteDirectory(cancelToken, asFile.toPath());
+    }
+
+    public static void writeLinesToFile(
+            Path file,
+            Collection<String> lines,
+            Charset charset) throws IOException {
+        writeLinesToFile(file, lines, charset, (Project)null);
+    }
+
+    public static void writeLinesToFile(
+            Path file,
+            Collection<String> lines,
+            Charset charset,
+            Project ownerProject) throws IOException {
+        String lineSeparator = ChangeLFPlugin.getPreferredLineSeparator(ownerProject);
+        writeLinesToFile(file, lines, charset, lineSeparator);
+    }
+
+    public static void writeLinesToFile(
+            Path file,
+            Collection<String> lines,
+            Charset charset,
+            String lineSeparator) throws IOException {
+        ExceptionHelper.checkNotNullArgument(file, "file");
+        ExceptionHelper.checkNotNullElements(lines, "lines");
+        ExceptionHelper.checkNotNullArgument(charset, "charset");
+
+        if (lineSeparator == null) {
+            Files.write(file, lines, charset);
+        }
+        else {
+            byte[] lineSeparatorBytes = lineSeparator.getBytes(charset);
+            try (OutputStream fileOutput = Files.newOutputStream(file);
+                    OutputStream output = new BufferedOutputStream(fileOutput, 16 * 1024)) {
+                for (String line: lines) {
+                    output.write(line.getBytes(charset));
+                    output.write(lineSeparatorBytes);
+                }
+            }
+        }
     }
 
     private NbFileUtils() {
