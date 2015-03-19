aee14b1eec42b518f8147ad4c53cbd0420b37f30
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-19 23:50:19 +0100

    Increased the thread count and the idle timeout of DEFAULT_EXECUTOR.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/NbTaskExecutors.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/NbTaskExecutors.java
@@ -14,23 +14,37 @@ public final class NbTaskExecutors {
     private static final Logger LOGGER = Logger.getLogger(NbTaskExecutors.class.getName());
 
     public static final MonitorableTaskExecutorService DEFAULT_EXECUTOR
-            = newExecutor("Gradle-Default", 2);
+            = newExecutor("Gradle-Default", getDefaultThreadCount(), 5000);
 
-    private static final long IDLE_TIMEOUT_MS = 1000;
+    private static final long DEFAULT_IDLE_TIMEOUT_MS = 1000;
 
     public static MonitorableTaskExecutorService newExecutor(String name, int threadCount) {
+        return newExecutor(name, threadCount, DEFAULT_IDLE_TIMEOUT_MS);
+    }
+
+    public static MonitorableTaskExecutorService newExecutor(String name, int threadCount, long timeoutMs) {
         return new Unstoppable(newStoppableExecutor(name, threadCount));
     }
 
     public static MonitorableTaskExecutorService newStoppableExecutor(String name, int threadCount) {
+        return newStoppableExecutor(name, threadCount, DEFAULT_IDLE_TIMEOUT_MS);
+    }
+
+    public static MonitorableTaskExecutorService newStoppableExecutor(String name, int threadCount, long timeoutMs) {
         ExceptionHelper.checkArgumentInRange(threadCount, 1, Integer.MAX_VALUE, "threadCount");
 
         if (threadCount == 1) {
-            return new SingleThreadedExecutor(name, Integer.MAX_VALUE, IDLE_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+            return new SingleThreadedExecutor(name, Integer.MAX_VALUE, timeoutMs, TimeUnit.MILLISECONDS);
         }
         else {
-            return new ThreadPoolTaskExecutor(name, threadCount, Integer.MAX_VALUE, IDLE_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+            return new ThreadPoolTaskExecutor(name, threadCount, Integer.MAX_VALUE, timeoutMs, TimeUnit.MILLISECONDS);
         }
+    }
+
+    private static int getDefaultThreadCount() {
+        // We don't want too much thread, because there is little benefit
+        // and many threads might need much more memory.
+        return Math.min(Runtime.getRuntime().availableProcessors(), 8);
     }
 
     public static void defaultCleanup(boolean canceled, Throwable error) {
