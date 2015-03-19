a1f53a4f2d468b4591674ccfa874283adf29bcc6
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-19 14:58:19 +0100

    Relaxed some constraints in MultiMapUtils.

--- netbeans-gradle-default-models/src/main/java/org/netbeans/gradle/model/util/MultiMapUtils.java
+++ netbeans-gradle-default-models/src/main/java/org/netbeans/gradle/model/util/MultiMapUtils.java
@@ -1,16 +1,17 @@
 package org.netbeans.gradle.model.util;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public final class MultiMapUtils {
-    public static <Key, Value> void addToMultiMap(Key key, Value value, Map<Key, List<Value>> result) {
+    public static <Key, Value> void addToMultiMap(Key key, Value value, Map<? super Key, List<Value>> result) {
         addAllToMultiMap(key, Collections.singletonList(value), result);
     }
 
-    public static <Key, Value> void addAllToMultiMap(Key key, List<? extends Value> values, Map<Key, List<Value>> result) {
+    public static <Key, Value> void addAllToMultiMap(Key key, Collection<? extends Value> values, Map<? super Key, List<Value>> result) {
         List<Value> container = result.get(key);
         if (container == null) {
             container = new LinkedList<Value>();
