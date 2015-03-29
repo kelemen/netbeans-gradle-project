2ba783601ad57fd513171574ff12539e7b3e58e8
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-29 18:26:33 +0200

    Added new StandardTaskVariable values.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/tasks/StandardTaskVariable.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/tasks/StandardTaskVariable.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import org.netbeans.api.java.project.JavaProjectConstants;
 import org.netbeans.api.project.ProjectUtils;
 import org.netbeans.api.project.SourceGroup;
+import org.netbeans.gradle.model.ProjectId;
 import org.netbeans.gradle.model.util.CollectionUtils;
 import org.netbeans.gradle.project.NbGradleProject;
 import org.netbeans.gradle.project.api.entry.ProjectPlatform;
@@ -96,6 +97,34 @@ public enum StandardTaskVariable {
             return new VariableValue(value != null
                     ? StringUtils.capitalizeFirstCharacter(value)
                     : null);
+        }
+    }),
+    PROJECT_PATH("project.path", new ValueGetter<NbGradleProject>() {
+        @Override
+        public VariableValue getValue(TaskVariableMap variables, NbGradleProject project, Lookup actionContext) {
+            String path = project.currentModel().getValue().getMainProject().getProjectFullName();
+            return new VariableValue(path);
+        }
+    }),
+    PROJECT_GROUP("project.group", new ValueGetter<NbGradleProject>() {
+        @Override
+        public VariableValue getValue(TaskVariableMap variables, NbGradleProject project, Lookup actionContext) {
+            ProjectId projectId = project.currentModel().getValue().getProjectId();
+            return new VariableValue(projectId.getGroup());
+        }
+    }),
+    PROJECT_ARTIFACT_ID("project.name", new ValueGetter<NbGradleProject>() {
+        @Override
+        public VariableValue getValue(TaskVariableMap variables, NbGradleProject project, Lookup actionContext) {
+            ProjectId projectId = project.currentModel().getValue().getProjectId();
+            return new VariableValue(projectId.getName());
+        }
+    }),
+    PROJECT_VERSION("project.version", new ValueGetter<NbGradleProject>() {
+        @Override
+        public VariableValue getValue(TaskVariableMap variables, NbGradleProject project, Lookup actionContext) {
+            ProjectId projectId = project.currentModel().getValue().getProjectId();
+            return new VariableValue(projectId.getVersion());
         }
     });
 
