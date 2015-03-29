fe739f846cbc251d615fd2a9484d280f931d0524
Attila Kelemen <attila.kelemen85@gmail.com>
2015-03-29 19:07:43 +0200

    DisplayableTaskVariable is now more sensible for buildSrc.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/view/DisplayableTaskVariable.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/view/DisplayableTaskVariable.java
@@ -6,6 +6,7 @@ import org.netbeans.gradle.model.util.CollectionUtils;
 import org.netbeans.gradle.project.api.task.TaskVariable;
 import org.netbeans.gradle.project.api.task.TaskVariableMap;
 import org.netbeans.gradle.project.model.NbGradleModel;
+import org.netbeans.gradle.project.model.NbGradleProjectTree;
 import org.netbeans.gradle.project.tasks.CachingVariableMap;
 import org.netbeans.gradle.project.tasks.CachingVariableMap.ValueGetter;
 import org.netbeans.gradle.project.tasks.CachingVariableMap.VariableDef;
@@ -17,13 +18,7 @@ public enum DisplayableTaskVariable {
     PROJECT_PATH("project.path", new ValueGetter<NbGradleModel>() {
         @Override
         public VariableValue getValue(TaskVariableMap variables, NbGradleModel model, Lookup actionContext) {
-            String path = model.getMainProject().getProjectFullName();
-            if (path.isEmpty() || path.equals(":")) {
-                return new VariableValue(getSafeProjectName(model));
-            }
-            else {
-                return new VariableValue(path);
-            }
+            return new VariableValue(getSafeProjectPath(model));
         }
     }),
     PROJECT_GROUP("project.group", new ValueGetter<NbGradleModel>() {
@@ -47,14 +42,42 @@ public enum DisplayableTaskVariable {
         }
     });
 
-    private static String getSafeProjectName(NbGradleModel model) {
-        ProjectId projectId = model.getProjectId();
+    private static String getProjectName(NbGradleProjectTree model) {
+        ProjectId projectId = model.getGenericProperties().getProjectId();
         String name = projectId.getName();
         if (name.isEmpty()) {
             return model.getProjectDir().getName();
         }
         else {
             return name;
+        }
+    }
+
+    private static String getSafeProjectName(NbGradleModel model) {
+        if (model.isBuildSrc()) {
+            return getProjectName(model.getProjectDef().getRootProject());
+        }
+        else {
+            return getProjectName(model.getMainProject());
+        }
+    }
+
+    private static String getProjectPath(NbGradleProjectTree model) {
+        String path = model.getProjectFullName();
+        if (path.isEmpty() || path.equals(":")) {
+            return getProjectName(model);
+        }
+        else {
+            return path;
+        }
+    }
+
+    private static String getSafeProjectPath(NbGradleModel model) {
+        if (model.isBuildSrc()) {
+            return getProjectPath(model.getProjectDef().getRootProject());
+        }
+        else {
+            return getProjectPath(model.getMainProject());
         }
     }
 
