4cde64ce53ee35c83489ece975af49078b9b67d5
Attila Kelemen <attila.kelemen85@gmail.com>
2015-04-08 21:22:22 +0200

    The SingleNodeFactory returned by NodeUtils.tryGetFileNode now has an equals/hashCode method to avoid unnecessary node refresh.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/view/NodeUtils.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/view/NodeUtils.java
@@ -1,6 +1,7 @@
 package org.netbeans.gradle.project.view;
 
 import java.awt.Image;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.netbeans.gradle.project.api.nodes.SingleNodeFactory;
@@ -36,32 +37,70 @@ public final class NodeUtils {
             return null;
         }
 
-        return new SingleNodeFactory() {
-            @Override
-            public Node createNode() {
-                return new FilterNode(fileData.getNodeDelegate().cloneNode()) {
-                    @Override
-                    public boolean canRename() {
-                        return false;
-                    }
+        return new FileObjNodeFactory(file.toURI(), fileData, name, icon);
+    }
 
-                    @Override
-                    public String getDisplayName() {
-                        return name;
-                    }
+    private static final class FileObjNodeFactory implements SingleNodeFactory {
+        private final Object fileDataKey;
+        private final DataObject fileData;
+        private final String name;
+        private final Image icon;
 
-                    @Override
-                    public Image getIcon(int type) {
-                        return icon != null ? icon : super.getIcon(type);
-                    }
+        public FileObjNodeFactory(
+                Object fileDataKey,
+                DataObject fileData,
+                String name,
+                Image icon) {
+            this.fileDataKey = fileDataKey;
+            this.fileData = fileData;
+            this.name = name;
+            this.icon = icon;
+        }
 
-                    @Override
-                    public Image getOpenedIcon(int type) {
-                        return icon != null ? icon : super.getOpenedIcon(type);
-                    }
-                };
-            }
-        };
+        @Override
+        public Node createNode() {
+            return new FilterNode(fileData.getNodeDelegate().cloneNode()) {
+                @Override
+                public boolean canRename() {
+                    return false;
+                }
+
+                @Override
+                public String getDisplayName() {
+                    return name;
+                }
+
+                @Override
+                public Image getIcon(int type) {
+                    return icon != null ? icon : super.getIcon(type);
+                }
+
+                @Override
+                public Image getOpenedIcon(int type) {
+                    return icon != null ? icon : super.getOpenedIcon(type);
+                }
+            };
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 7;
+            hash = 83 * hash + Objects.hashCode(this.fileDataKey);
+            hash = 83 * hash + Objects.hashCode(this.name);
+            hash = 83 * hash + Objects.hashCode(this.icon);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+
+            final FileObjNodeFactory other = (FileObjNodeFactory)obj;
+            return this.icon == other.icon
+                    && Objects.equals(this.name, other.name)
+                    && Objects.equals(this.fileDataKey, other.fileDataKey);
+        }
     }
 
     private NodeUtils() {
