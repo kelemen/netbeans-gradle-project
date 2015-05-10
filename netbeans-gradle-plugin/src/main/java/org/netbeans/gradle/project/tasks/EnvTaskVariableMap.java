03af3c21dc6fd9a2d16ec35b83a0ab2df4504f41
Attila Kelemen <attila.kelemen85@gmail.com>
2015-05-10 22:28:40 +0200

    Added EnvTaskVariableMap.

---
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/tasks/EnvTaskVariableMap.java
@@ -0,0 +1,19 @@
+package org.netbeans.gradle.project.tasks;
+
+import org.netbeans.gradle.project.api.task.TaskVariable;
+import org.netbeans.gradle.project.api.task.TaskVariableMap;
+
+public final class EnvTaskVariableMap implements TaskVariableMap {
+    private static final String ENV_PREFIX = "env.";
+
+    @Override
+    public String tryGetValueForVariable(TaskVariable variable) {
+        String name = variable.getVariableName();
+        if (!name.startsWith(ENV_PREFIX)) {
+            return null;
+        }
+
+        String envVarName = name.substring(ENV_PREFIX.length());
+        return System.getenv(envVarName);
+    }
+}
