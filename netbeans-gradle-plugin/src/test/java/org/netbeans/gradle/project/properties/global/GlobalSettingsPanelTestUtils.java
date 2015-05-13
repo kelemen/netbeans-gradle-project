f2d2bcc2e4fcc49088c5e256039fe37a2b298544
Attila Kelemen <attila.kelemen85@gmail.com>
2015-05-13 23:59:04 +0200

    Added basic test for GradleDaemonPanel.

---
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties/global/GlobalSettingsPanelTestUtils.java
@@ -0,0 +1,47 @@
+package org.netbeans.gradle.project.properties.global;
+
+import java.util.Map;
+import javax.swing.SwingUtilities;
+import junit.framework.Assert;
+import org.netbeans.gradle.model.util.Exceptions;
+import org.netbeans.gradle.project.util.NbConsumer;
+
+public final class GlobalSettingsPanelTestUtils {
+    public static void testInitAndReadBack(
+            final Class<? extends GlobalSettingsEditor> panelClass,
+            final NbConsumer<? super GlobalGradleSettings> initializer) throws Exception {
+
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                GlobalSettingsEditor panel;
+                try {
+                    panel = panelClass.newInstance();
+                } catch (Exception ex) {
+                    throw Exceptions.throwUnchecked(ex);
+                }
+
+                GlobalGradleSettings.PreferenceContainer preference
+                        = GlobalGradleSettings.setCleanMemoryPreference();
+
+                GlobalGradleSettings input = new GlobalGradleSettings("input");
+
+                initializer.accept(input);
+                Map<String, String> inputValues = preference.getKeyValues("input");
+
+                panel.updateSettings(input);
+
+                GlobalGradleSettings output = new GlobalGradleSettings("output");
+
+                panel.saveSettings(output);
+                Map<String, String> outputValues = preference.getKeyValues("output");
+
+                Assert.assertEquals(inputValues, outputValues);
+            }
+        });
+    }
+
+    private GlobalSettingsPanelTestUtils() {
+        throw new AssertionError();
+    }
+}
