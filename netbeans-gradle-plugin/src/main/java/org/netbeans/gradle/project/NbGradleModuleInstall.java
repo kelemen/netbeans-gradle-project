b32a7ef9adff802bebaafe8e7ee61dd204efdcf0
Attila Kelemen <attila.kelemen85@gmail.com>
2015-08-12 10:21:50 +0200

    Fixed the name "org.netbeans.modules.gsf.testrunner.ui" in Yenta.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/NbGradleModuleInstall.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/NbGradleModuleInstall.java
@@ -9,7 +9,7 @@ public final class NbGradleModuleInstall extends Yenta {
     private static final long serialVersionUID = 1L;
     private static final Set<String> FRIENDS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
             "org.netbeans.modules.gsf.testrunner",
-            "org.netbeans.modules.gsf.testrunner-ui")));
+            "org.netbeans.modules.gsf.testrunner.ui")));
 
     @Override
     protected Set<String> friends() {
