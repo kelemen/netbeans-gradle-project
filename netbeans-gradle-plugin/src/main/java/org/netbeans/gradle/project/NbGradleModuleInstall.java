b164aae58321e83d8d46a39873287756f953b187
Attila Kelemen <attila.kelemen85@gmail.com>
2015-08-12 09:48:25 +0200

    Added "org.netbeans.modules.gsf.testrunner-ui" to Yenta.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/NbGradleModuleInstall.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/NbGradleModuleInstall.java
@@ -1,13 +1,18 @@
 package org.netbeans.gradle.project;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public final class NbGradleModuleInstall extends Yenta {
     private static final long serialVersionUID = 1L;
+    private static final Set<String> FRIENDS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+            "org.netbeans.modules.gsf.testrunner",
+            "org.netbeans.modules.gsf.testrunner-ui")));
 
     @Override
     protected Set<String> friends() {
-        return Collections.singleton("org.netbeans.modules.gsf.testrunner");
+        return FRIENDS;
     }
 }
