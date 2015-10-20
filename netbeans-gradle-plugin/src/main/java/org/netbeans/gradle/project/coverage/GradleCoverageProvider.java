37b7d1a908ef725bb895bc3b0cdbb207a05c9c20
Attila Kelemen <attila.kelemen85@gmail.com>
2015-10-20 15:54:19 +0200

    Minor formatting fixes in GradleCoverageProvider.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/coverage/GradleCoverageProvider.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/coverage/GradleCoverageProvider.java
@@ -18,7 +18,6 @@ import org.netbeans.gradle.project.NbGradleProject;
 import org.netbeans.gradle.project.java.query.GradleClassPathProvider;
 import org.netbeans.gradle.project.model.NbGradleModel;
 import org.netbeans.modules.gsf.codecoverage.api.CoverageManager;
-
 import org.netbeans.modules.gsf.codecoverage.api.CoverageProvider;
 import org.netbeans.modules.gsf.codecoverage.api.CoverageType;
 import org.netbeans.modules.gsf.codecoverage.api.FileCoverageDetails;
@@ -46,15 +45,18 @@ import org.xml.sax.SAXException;
 public class GradleCoverageProvider implements CoverageProvider {
 
     private static final Logger LOG = Logger.getLogger(GradleCoverageProvider.class.getName());
-    
-    
+
+
     private final Project p;
     private Map<String, GradleSummary> summaryCache;
+    private FileChangeListener listener;
 
     public GradleCoverageProvider(Project p) {
         this.p = p;
+        this.summaryCache = null;
+        this.listener = null;
     }
-    
+
     @Override
     public boolean supportsHitCounts() {
         return true;
@@ -64,7 +66,7 @@ public class GradleCoverageProvider implements CoverageProvider {
     public boolean supportsAggregation() {
         return false;
     }
-    
+
     private boolean hasPlugin() {
         // To do: figure out if jacoco is available
         return true;
@@ -87,21 +89,21 @@ public class GradleCoverageProvider implements CoverageProvider {
 
     @Override
     public Set<String> getMimeTypes() {
-        return Collections.singleton("text/x-java"); 
+        return Collections.singleton("text/x-java");
     }
 
     @Override
     public void setEnabled(boolean bln) {
         //TODO: add jacoco to build.gradle
     }
-    
+
     private @CheckForNull File report() {
         NbGradleProject prj = p.getLookup().lookup(NbGradleProject.class);
         NbGradleModel nbGradleModel = prj.currentModel().getValue();
         File buildDir = nbGradleModel.getGenericInfo().getBuildDir();
         // Todo - determine real path from gradle model
         return FileUtil.normalizeFile(new File(buildDir, "reports/jacoco/test/jacocoTestReport.xml"));
-    }    
+    }
 
     public @Override synchronized void clear() {
         File r = report();
@@ -111,8 +113,6 @@ public class GradleCoverageProvider implements CoverageProvider {
         }
     }
 
-    private FileChangeListener listener;
-    
     @Override
     public FileCoverageDetails getDetails(FileObject fo, Document doc) {
         NbGradleProject prj = p.getLookup().lookup(NbGradleProject.class);
@@ -129,9 +129,9 @@ public class GradleCoverageProvider implements CoverageProvider {
                 det.lineCount = doc.getDefaultRootElement().getElementCount();
             }
         }
-        return det;    
+        return det;
     }
-    
+
     private @CheckForNull Pair<File, org.w3c.dom.Document> parse() {
         File r = report();
         if (r == null) {
@@ -170,16 +170,19 @@ public class GradleCoverageProvider implements CoverageProvider {
         }
         try {
             org.w3c.dom.Document report = XMLUtil.parse(new InputSource(r.toURI().toString()), true, false, XMLUtil.defaultErrorHandler(), new EntityResolver() {
-                                     public @Override InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-                                         if (systemId.equals("http://cobertura.sourceforge.net/xml/coverage-04.dtd")) {
-                                             return new InputSource(GradleCoverageProvider.class.getResourceAsStream("coverage-04.dtd")); // NOI18N
-                                         } else if (publicId.equals("-//JACOCO//DTD Report 1.0//EN")) {
-                                             return new InputSource(GradleCoverageProvider.class.getResourceAsStream("jacoco-1.0.dtd"));
-                                         } else {
-                                             return null;
-                                         }
-                                     }
-                                 });
+                public @Override
+                InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+                    if (systemId.equals("http://cobertura.sourceforge.net/xml/coverage-04.dtd")) {
+                        return new InputSource(GradleCoverageProvider.class.getResourceAsStream("coverage-04.dtd")); // NOI18N
+                    }
+                    else if (publicId.equals("-//JACOCO//DTD Report 1.0//EN")) {
+                        return new InputSource(GradleCoverageProvider.class.getResourceAsStream("jacoco-1.0.dtd"));
+                    }
+                    else {
+                        return null;
+                    }
+                }
+            });
             LOG.log(Level.FINE, "parsed {0}", r);
             return Pair.of(r, report);
         } catch (/*IO,SAX*/Exception x) {
@@ -187,15 +190,15 @@ public class GradleCoverageProvider implements CoverageProvider {
             return null;
         }
     }
-    
-    
+
+
     private ClassPath srcPath() {
         GradleClassPathProvider gcp = p.getLookup().lookup(GradleClassPathProvider.class);
         assert gcp != null;
         ClassPath cp = gcp.getClassPaths(ClassPath.SOURCE);
         assert cp != null;
         return cp;
-    }       
+    }
 
     @Override
     public List<FileCoverageSummary> getResults() {
@@ -204,25 +207,26 @@ public class GradleCoverageProvider implements CoverageProvider {
             return null;
         }
         ClassPath src = srcPath();
-        List<FileCoverageSummary> summs = new ArrayList<FileCoverageSummary>();
-        Map<String, GradleSummary> summaries = new HashMap<String, GradleSummary>();
+        List<FileCoverageSummary> summs = new ArrayList<>();
+        Map<String, GradleSummary> summaries = new HashMap<>();
         boolean jacoco = hasPlugin();
         NodeList nl = r.second().getElementsByTagName(jacoco ? "sourcefile" : "class"); // NOI18N
         for (int i = 0; i < nl.getLength(); i++) {
-            Element clazz = (Element) nl.item(i);
+            Element clazz = (Element)nl.item(i);
             String filename;
             List<Element> lines;
             String name;
             if (jacoco) {
-                filename = ((Element) clazz.getParentNode()).getAttribute("name") + '/' + clazz.getAttribute("name");
-                lines = new ArrayList<Element>();
-                for (Element line : XMLUtil.findSubElements(clazz)) {
+                filename = ((Element)clazz.getParentNode()).getAttribute("name") + '/' + clazz.getAttribute("name");
+                lines = new ArrayList<>();
+                for (Element line: XMLUtil.findSubElements(clazz)) {
                     if (line.getTagName().equals("line")) {
                         lines.add(line);
                     }
                 }
                 name = filename.replaceFirst("[.]java$", "").replace('/', '.');
-            } else {
+            }
+            else {
                 filename = clazz.getAttribute("filename");
                 Element linesE = XMLUtil.findElement(clazz, "lines", null); // NOI18N
                 lines = linesE != null ? XMLUtil.findSubElements(linesE) : Collections.<Element>emptyList();
@@ -242,12 +246,12 @@ public class GradleCoverageProvider implements CoverageProvider {
         }
         return summs;
     }
-    
+
     private GradleSummary summaryOf(FileObject java, String name, List<Element> lines, boolean jacoco, long lastUpdated) {
         // Not really the total number of lines in the file at all, but close enough - the ones Cobertura recorded.
         int lineCount = 0;
         int executedLineCount = 0;
-        Map<Integer, Integer> detLines = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> detLines = new HashMap<>();
         for (Element line : lines) {
             lineCount++;
             String attr = line.getAttribute(jacoco ? "ci" : "hits");
@@ -260,13 +264,13 @@ public class GradleCoverageProvider implements CoverageProvider {
         GradleDetails det = new GradleDetails(java, lastUpdated, lineCount, detLines);
         GradleSummary s = new GradleSummary(java, name, det, executedLineCount);
         return s;
-    }    
+    }
 
     @Override
     public String getTestAllAction() {
             return "test";
     }
-    
+
     private static class GradleSummary extends FileCoverageSummary {
         private final GradleDetails details;
 
@@ -275,13 +279,13 @@ public class GradleCoverageProvider implements CoverageProvider {
             this.details = details;
             details.setSummary(this);
         }
-        
+
         GradleDetails getDetails() {
             return details;
         }
-        
+
     }
-    
+
     private static class GradleDetails implements FileCoverageDetails {
         private final FileObject fileObject;
         private final long lastUpdated;
@@ -295,8 +299,8 @@ public class GradleCoverageProvider implements CoverageProvider {
             this.lineHitCounts = lineHitCounts;
             this.lineCount = lineCount;
         }
-        
-        
+
+
         @Override
         public FileObject getFile() {
             return fileObject;
@@ -339,8 +343,8 @@ public class GradleCoverageProvider implements CoverageProvider {
             }
             return ret;
         }
-    
+
     }
-    
-    
+
+
 }
