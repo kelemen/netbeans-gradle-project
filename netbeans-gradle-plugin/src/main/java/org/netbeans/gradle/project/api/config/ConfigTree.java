a4d2b84cc333c5e1141645ff67c2d8a5064d2cae
Attila Kelemen <attila.kelemen85@gmail.com>
2015-12-15 00:52:27 +0100

    Documented ConfigTree.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ConfigTree.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ConfigTree.java
@@ -12,16 +12,47 @@ import javax.annotation.Nullable;
 import org.jtrim.collections.CollectionsEx;
 import org.jtrim.utils.ExceptionHelper;
 
+/**
+ * Defines a tree based configuration store. The edges in the tree are identified
+ * by a string and also each node may store a string value (there is no
+ * restriciton on what characters the strings may contain). In most practical
+ * use-cases, you can think of edges as {@literal XML} tags and values as their
+ * {@literal CDATA} content. All strings are treated as case sensitive.
+ * <P>
+ * It is possible to have multiple edges with the same name with the given
+ * parent, in this case all the subtrees can be requested as a list
+ * (order is maintained).
+ * <P>
+ * Instances of {@code ConfigTree} can be created through its {@link Builder}.
+ * <P>
+ * Instances of this class are immutable and as such can be shared without any
+ * further synchronization.
+ */
 public final class ConfigTree {
     private static final int DEFAULT_LIST_SIZE = 10;
 
+    /**
+     * Defines an empty tree with a single node with no value.
+     */
     public static final ConfigTree EMPTY = new Builder().create();
 
+    /**
+     * Defines a builder to create {@code ConfigTree} instances.
+     */
     public static final class Builder {
         private String value;
         private Map<String, List<TreeOrBuilder>> childTrees;
         private ConfigTree cachedBuilt;
 
+        /**
+         * Creates a new {@code Builder} with the given configuration tree as
+         * its initial value. That is, if you call {@link #create() create} right
+         * after creating the new {@code Builder}, you will get effectively the
+         * same {@code ConfigTree} what was specified in the argument.
+         *
+         * @param initialValue the base configuration tree of this {@code Builder}.
+         *   This argument cannot be {@code null}.
+         */
         public Builder(@Nonnull ConfigTree initialValue) {
             this();
 
@@ -45,6 +76,10 @@ public final class ConfigTree {
             }
         }
 
+        /**
+         * Creates a new {@code Builder} with only a single root node without
+         * any value.
+         */
         public Builder() {
             this.value = null;
             this.childTrees = null;
@@ -87,11 +122,32 @@ public final class ConfigTree {
             return result;
         }
 
+        /**
+         * Sets the string value associated with the root node. Note that,
+         * usually it is not recommended (though it is possible) for non-leaf
+         * nodes to have a value.
+         *
+         * @param value the value of the root node. This argument can be
+         *   {@code null}, meaning that this node has no value.
+         */
         public void setValue(@Nullable String value) {
             cachedBuilt = null;
             this.value = value;
         }
 
+        /**
+         * Connects the root node of this {@code Builder} with the specied
+         * configuration tree with the given edge.
+         * <P>
+         * <B>Note</B>: This method will effectievly detach all subtrees along
+         * the given edge. So builders previously created with the given edge
+         * will no longer affect this builder.
+         *
+         * @param key the string identifying the edge connecting the given
+         *   subtree with the root. This argument cannot be {@code null}.
+         * @param tree the subtree to be connected with the root node. This
+         *   argument cannot be {@code null}.
+         */
         public void setChildTree(@Nonnull String key, @Nonnull ConfigTree tree) {
             ExceptionHelper.checkNotNullArgument(key, "key");
             ExceptionHelper.checkNotNullArgument(tree, "tree");
@@ -102,6 +158,33 @@ public final class ConfigTree {
             valueList.add(new TreeOrBuilder(tree));
         }
 
+        /**
+         * Returns a {@code Builder} which can be used to build subtree of this
+         * {@code Builder}. Non-existent subtrees in the path will be created,
+         * however note that already created subtree builders will be reused
+         * (will not add new subtrees).
+         * <P>
+         * <B>Note</B>: The returned {@code Builder} will keep affecting
+         * its parent {@code Builder} instances as long as
+         * {@link #detachChildTreeBuilders() detachChildTreeBuilders} is not
+         * called. Once {@code detachChildTreeBuilders} gets called on a tree,
+         * none of its previously created subtree builders can affect that parent.
+         * <P>
+         * If you need multiple subtrees connected with an edge with the same
+         * name, you have to use the {@link #addChildBuilder(String) addChildBuilder}
+         * method instead.
+         * <P>
+         * This method is effectively the same as repeatedly calling
+         * {@link #getChildBuilder(String) getChildBuilder} on each returned
+         * {@code Builder} with the elements of the given array.
+         *
+         * @param keyNames the names of the edges identifying the subtree to
+         *   be edited. This argument cannot be {@code null} and neither of its
+         *   elements can be {@code null}.
+         * @return the {@code Builder} which can be used to edit the given
+         *   subtree. This method never returns {@code null}.
+         */
+        @Nonnull
         public Builder getDeepChildBuilder(@Nonnull String... keyNames) {
             Builder result = this;
             for (String keyName: keyNames) {
@@ -110,6 +193,32 @@ public final class ConfigTree {
             return result;
         }
 
+        /**
+         * Returns a {@code Builder} which can be used to build subtree of this
+         * {@code Builder}. Non-existent subtrees in the path will be created,
+         * however note that already created subtree builders will be reused
+         * (will not add new subtrees).
+         * <P>
+         * <B>Note</B>: The returned {@code Builder} will keep affecting
+         * its parent {@code Builder} instances as long as
+         * {@link #detachChildTreeBuilders() detachChildTreeBuilders} is not
+         * called. Once {@code detachChildTreeBuilders} gets called on a tree,
+         * none of its previously created subtree builders can affect that parent.
+         * <P>
+         * If you need multiple subtrees connected with an edge with the same
+         * name, you have to use the {@link #addChildBuilder(String) addChildBuilder}
+         * method instead.
+         * <P>
+         * This method is effectively the same as repeatedly calling
+         * {@link #getChildBuilder(String) getChildBuilder} on each returned
+         * {@code Builder} with the keys of the given {@code ConfigPath}.
+         *
+         * @param path the path identifying the subtree to be edited. This
+         *   argument cannot be {@code null}.
+         * @return the {@code Builder} which can be used to edit the given
+         *   subtree. This method never returns {@code null}.
+         */
+        @Nonnull
         public Builder getDeepChildBuilder(@Nonnull ConfigPath path) {
             Builder result = this;
             for (String key: path.getKeys()) {
@@ -118,6 +227,28 @@ public final class ConfigTree {
             return result;
         }
 
+        /**
+         * Returns a {@code Builder} which can be used to build subtree of this
+         * {@code Builder}. If a subtree identified by the given edge does not
+         * exist it will be created. If it already exist, the first builder
+         * with the given name will be returned.
+         * <P>
+         * <B>Note</B>: The returned {@code Builder} will keep affecting
+         * its parent {@code Builder} instances as long as
+         * {@link #detachChildTreeBuilders() detachChildTreeBuilders} is not
+         * called. Once {@code detachChildTreeBuilders} gets called on a tree,
+         * none of its previously created subtree builders can affect that parent.
+         * <P>
+         * If you need multiple subtrees connected with an edge with the same
+         * name, you have to use the {@link #addChildBuilder(String) addChildBuilder}
+         * method instead.
+         *
+         * @param key the key identifying the subtree to be edited. This
+         *   argument cannnot be {@code null}.
+         * @return the {@code Builder} which can be used to edit the given
+         *   subtree. This method never returns {@code null}.
+         */
+        @Nonnull
         public Builder getChildBuilder(@Nonnull String key) {
             ExceptionHelper.checkNotNullArgument(key, "key");
 
@@ -134,6 +265,22 @@ public final class ConfigTree {
             }
         }
 
+        /**
+         * Adds a subtree builder connected to the root node with the given name.
+         * If there is no subtree connected with this name, a new one is created.
+         * If there is already a subtree connected with this name, a new one
+         * is added with the same name.
+         * <P>
+         * <B>Note</B>: {@code getChildBuilder} and similar methods will only
+         * use the first child added and will ignore subsequently added subtrees
+         * with the same name.
+         *
+         * @param key the key identifying the subtree to be edited. This
+         *   argument cannnot be {@code null}.
+         * @return the {@code Builder} which can be used to edit the given
+         *   subtree. This method never returns {@code null}.
+         */
+        @Nonnull
         public Builder addChildBuilder(@Nonnull String key) {
             ExceptionHelper.checkNotNullArgument(key, "key");
 
@@ -144,6 +291,14 @@ public final class ConfigTree {
             return result;
         }
 
+        /**
+         * Remove all subtrees connected to the root node with an edge with the
+         * given name. Further editing child subtrees connected with this name
+         * will no longer affect this builder.
+         *
+         * @param key the string identifying the edge connecting the subtrees
+         *   to be removed. This argument cannot be {@code null}.
+         */
         public void removeChild(@Nonnull String key) {
             ExceptionHelper.checkNotNullArgument(key, "key");
 
@@ -156,6 +311,12 @@ public final class ConfigTree {
             }
         }
 
+        /**
+         * Makes so the previously created subtree builders no longer affect
+         * this {@code Builder}. Note that this method does not remove child
+         * trees, simply makes so that subsequent edits to <I>previously</I>
+         * created builders can no longer affect this builder.
+         */
         public void detachChildTreeBuilders() {
             if (childTrees == null || childTrees.isEmpty()) {
                 return;
@@ -171,6 +332,14 @@ public final class ConfigTree {
             cachedBuilt = create();
         }
 
+        /**
+         * Creates a new immutable snapshot of the configuration tree built
+         * by this builder.
+         *
+         * @return a new immutable snapshot of the configuration tree built
+         *   by this builder. This method never returns {@code null}.
+         */
+        @Nonnull
         public ConfigTree create() {
             ConfigTree result = cachedBuilt;
             if (result == null) {
@@ -231,35 +400,101 @@ public final class ConfigTree {
         this.childTrees = childTrees;
     }
 
-    public static ConfigTree singleValue(String value) {
+    /**
+     * Creates a configuration tree with only a single node with the given
+     * value. If the specified value is {@code null}, the return value is the
+     * {@link #EMPTY empty tree}.
+     *
+     * @param value the value of the root (and only) node of the returned
+     *   configuration tree.
+     * @return a configuration tree with only a single node with the given
+     *   value. This method never returns {@code null}.
+     */
+    @Nonnull
+    public static ConfigTree singleValue(@Nullable String value) {
         return value != null
                 ? new ConfigTree(value, Collections.<String, List<ConfigTree>>emptyMap())
                 : EMPTY;
     }
 
+    /**
+     * Checks whether this node has any child or value at all. That is, this
+     * method returns {@code true}, if and only, if this configuration tree
+     * is equivalent of the {@link #EMPTY empty tree}.
+     *
+     * @return {@code true} if this node is equivalent of the {@link #EMPTY empty tree},
+     *   {@code false} otherwise
+     */
     public boolean hasValues() {
         // We don't store childtrees with no values at all.
         return value != null || !childTrees.isEmpty();
     }
 
+    /**
+     * Returns the value of this node or the speficied default value if this
+     * node has no associate value.
+     *
+     * @param defaultValue the value to return if the root node has no associated
+     *   value. This argument can be {@code null}.
+     * @return the value of this node or the speficied default value if this
+     *   node has no associate value. Notice that this method may only return
+     *   {@code null}, if the specified default value is {@code null}.
+     */
     public String getValue(@Nullable String defaultValue) {
         return value != null ? value : defaultValue;
     }
 
+    /**
+     * Returns all the child trees as a map where keys are the name of the
+     * edges connecting the subtrees (values).
+     * <P>
+     * This method returns a normalized value in such way that none of the
+     * returned {@code ConfigTree} instances are equivalent to the
+     * {@link #EMPTY empty tree}.
+     *
+     * @return all the child trees as a map where keys are the name of the
+     *   edges connecting the subtrees (values). This method never returns
+     *   {@code null}.
+     */
     @Nonnull
     public Map<String, List<ConfigTree>> getChildTrees() {
         return childTrees;
     }
 
+    /**
+     * Returns the subtree along the given path. This method never fails, if
+     * this configuration tree does not contain any subtree along this path,
+     * this method returns an empty tree.
+     * <P>
+     * If the given path contains no keys, this {@code ConfigTree} is returned.
+     *
+     * @param path the {@code ConfigPath} identifying the subtree to be
+     *   returned. This argument cannot be {@code null}.
+     * @return the subtree along the given path. This method never returns
+     *   {@code null}.
+     */
     @Nonnull
-    public ConfigTree getDeepChildTree(ConfigPath path) {
+    public ConfigTree getDeepChildTree(@Nonnull ConfigPath path) {
         ExceptionHelper.checkNotNullArgument(path, "path");
 
         return getDeepChildTree(path.getKeys().iterator());
     }
 
+     /**
+     * Returns the subtree along the given path. This method never fails, if
+     * this configuration tree does not contain any subtree along this path,
+     * this method returns an empty tree.
+     * <P>
+     * If the given path contains no keys, this {@code ConfigTree} is returned.
+     *
+     * @param keys the name of edges identifying the subtree to be
+     *   returned. This argument cannot be {@code null} and the array cannot
+     *   contain {@code null} elements.
+     * @return the subtree along the given path. This method never returns
+     *   {@code null}.
+     */
     @Nonnull
-    public ConfigTree getDeepChildTree(String... keys) {
+    public ConfigTree getDeepChildTree(@Nonnull String... keys) {
         ExceptionHelper.checkNotNullElements(keys, "keys");
 
         ConfigTree result = this;
@@ -288,16 +523,39 @@ public final class ConfigTree {
         return result;
     }
 
+    /**
+     * Returns all the child trees connected with an edge with the given name.
+     * If there are no such subtrees, an empty list is returned.
+     * <P>
+     * This method returns a normalized value in such way that none of the
+     * returned {@code ConfigTree} instances are equivalent to the
+     * {@link #EMPTY empty tree}.
+     *
+     * @param key the name of the edge connecting the subtrees to be returned.
+     *   This argument cannot be {@code null}.
+     * @return all the child trees connected with an edge with the given name.
+     *   This method never returns {@code null} and none of the elements of
+     *   list are {@code null}.
+     */
     @Nonnull
-    public List<ConfigTree> getChildTrees(String key) {
+    public List<ConfigTree> getChildTrees(@Nonnull String key) {
         ExceptionHelper.checkNotNullArgument(key, "key");
 
         List<ConfigTree> result = childTrees.get(key);
         return result != null ? result : Collections.<ConfigTree>emptyList();
     }
 
+    /**
+     * Returns the first child tree connected with an edge with the given name.
+     * If there are no such subtree, an {@link #EMPTY empty tree} is returned.
+     *
+     * @param key the name of the edge connecting the subtree to be returned.
+     *   This argument cannot be {@code null}.
+     * @return the first child tree connected with an edge with the given name.
+     *   This method never returns {@code null}.
+     */
     @Nonnull
-    public ConfigTree getChildTree(String key) {
+    public ConfigTree getChildTree(@Nonnull String key) {
         ExceptionHelper.checkNotNullArgument(key, "key");
 
         List<ConfigTree> result = childTrees.get(key);
