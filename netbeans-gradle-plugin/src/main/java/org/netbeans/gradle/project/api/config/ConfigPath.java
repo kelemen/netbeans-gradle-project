a234092166893bcce45635a119aa35fa09655960
Attila Kelemen <attila.kelemen85@gmail.com>
2015-12-14 17:48:52 +0100

    Documented ConfigPath.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ConfigPath.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ConfigPath.java
@@ -5,9 +5,22 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import javax.annotation.Nonnull;
 import org.jtrim.collections.ArraysEx;
 import org.jtrim.utils.ExceptionHelper;
 
+/**
+ * Defines a path to a subtree in a configuration tree. For most practical
+ * purposes, you can consider keys in this path as nested {@literal XML} tags.
+ * The keys in the path are case-sensitive.
+ * <P>
+ * Instances of this class are immutable and as such can be shared without any
+ * further synchronization.
+ * <P>
+ * Instances of this class can be created through one of its factory methods.
+ *
+ * @see ConfigTree
+ */
 public final class ConfigPath {
     private static final String[] NO_KEYS = new String[0];
     public static final ConfigPath ROOT = new ConfigPath(NO_KEYS, Collections.<String>emptyList());
@@ -29,45 +42,126 @@ public final class ConfigPath {
         this.keysAsList = keysAsList;
     }
 
-    public static ConfigPath fromKeys(String... keys) {
+    /**
+     * Returns a {@code ConfigPath} with the given keys in the path.
+     *
+     * @param keys the keys pointing to a subtree in a configuration tree.
+     *   This argument cannot be {@code null} and the keys cannot be {@code null}.
+     * @return a {@code ConfigPath} with the given keys in the path. This
+     *   method never returns {@code null}.
+     */
+    @Nonnull
+    public static ConfigPath fromKeys(@Nonnull String... keys) {
         return keys.length > 0
                 ? new ConfigPath(keys.clone())
                 : ROOT;
     }
 
-    public static ConfigPath fromKeys(List<String> keys) {
+    /**
+     * Returns a {@code ConfigPath} with the given keys in the path.
+     *
+     * @param keys the keys pointing to a subtree in a configuration tree.
+     *   This argument cannot be {@code null} and the keys cannot be {@code null}.
+     * @return a {@code ConfigPath} with the given keys in the path. This
+     *   method never returns {@code null}.
+     */
+    @Nonnull
+    public static ConfigPath fromKeys(@Nonnull List<String> keys) {
         return keys.isEmpty()
                 ? ROOT
                 : new ConfigPath(keys.toArray(NO_KEYS));
     }
 
-    public ConfigPath withParentPath(String... parentKeys) {
+    /**
+     * Returns a {@code ConfigPath} which has the starts with the given keys
+     * and ends with the keys of this {@code ConfigPath}.
+     *
+     * @param parentKeys the keys to be prepended to the keys of this
+     *   {@code ConfigPath}. This argument cannot be {@code null} and no keys
+     *   can be {@code null}.
+     * @return a {@code ConfigPath} which has the starts with the given keys
+     *   and ends with the keys of this {@code ConfigPath}. This method never
+     *   returns {@code null}.
+     */
+    @Nonnull
+    public ConfigPath withParentPath(@Nonnull String... parentKeys) {
         List<String> newKeys = new ArrayList<>(keys.length + parentKeys.length);
         newKeys.addAll(Arrays.asList(parentKeys));
         newKeys.addAll(keysAsList);
         return ConfigPath.fromKeys(newKeys);
     }
 
-    public ConfigPath getChildPath(String... childKeys) {
+    /**
+     * Returns a {@code ConfigPath} which has the starts with the keys of this
+     * {@code ConfigPath} and ends with the given keys.
+     *
+     * @param childKeys the keys to be appended to the keys of this
+     *   {@code ConfigPath}. This argument cannot be {@code null} and no keys
+     *   can be {@code null}.
+     * @return a {@code ConfigPath} which has the starts with the keys of this
+     *   {@code ConfigPath} and ends with the given keys. This method never
+     *   returns {@code null}.
+     */
+    @Nonnull
+    public ConfigPath getChildPath(@Nonnull String... childKeys) {
         List<String> newKeys = new ArrayList<>(keys.length + childKeys.length);
         newKeys.addAll(keysAsList);
         newKeys.addAll(Arrays.asList(childKeys));
         return ConfigPath.fromKeys(newKeys);
     }
 
+    /**
+     * Returns the number of keys found in this {@code ConfigPath}. That is,
+     * the same number as {@code getKeys().size()}.
+     *
+     * @return the number of keys found in this {@code ConfigPath}. This method
+     *   always returns a value greater than or equal to zero.
+     */
     public int getKeyCount() {
         return keys.length;
     }
 
+    /**
+     * Returns the key at the given index in the {@link #getKeys() keys list}
+     * of this {@code ConfigPath}.
+     *
+     * @param index the index of the requested key. This index must be greater
+     *   or equal to zero and less than {@link #getKeyCount() getKeyCount()}.
+     * @return the key at the given index in the {@link #getKeys() keys list}
+     *   of this {@code ConfigPath}. This method never returns {@code null}.
+     */
+    @Nonnull
     public String getKeyAt(int index) {
         return keys[index];
     }
 
+    /**
+     * Returns the keys of this {@code ConfigPath} defining the edges in a
+     * {@link ConfigTree}.
+     *
+     * @return the keys of this {@code ConfigPath} defining the edges in a
+     *   {@link ConfigTree}. This method never returns {@code null} and none
+     *   of the keys are {@code null} as well.
+     */
+    @Nonnull
     public List<String> getKeys() {
         return keysAsList;
     }
 
-    public boolean isParentOfOrEqual(ConfigPath childCandidate) {
+    /**
+     * Returns {@code true} if the given {@code ConfigPath} is the child path
+     * of this {@code ConfigPath}, {@code false} otherwise. That is, if the keys
+     * of the given {@code ConfigPath} starts with the keys of this
+     * {@code ConfigPath}. If the given {@code ConfigPath} specifies the same
+     * path as this {@code ConfigPath}, this method returns {@code true}.
+     *
+     * @param childCandidate the {@code ConfigPath} to be tested if it is a
+     *   child path of this {@code ConfigPath}. This argument cannot be
+     *   {@code null}.
+     * @return {@code true} if the given {@code ConfigPath} is the child path
+     *   of this {@code ConfigPath}, {@code false} otherwise
+     */
+    public boolean isParentOfOrEqual(@Nonnull ConfigPath childCandidate) {
         String[] childCandidateKeys = childCandidate.keys;
         if (childCandidateKeys.length < keys.length) {
             return false;
@@ -81,6 +175,11 @@ public final class ConfigPath {
         return true;
     }
 
+    /**
+     * Returns a hash code compatible with the {@code equals} method.
+     *
+     * @return a hash code compatible with the {@code equals} method
+     */
     @Override
     public int hashCode() {
         int hash = hashCache;
@@ -92,6 +191,17 @@ public final class ConfigPath {
         return hash;
     }
 
+    /**
+     * Returns {@code true} if the passed object is a {@code ConfigPath} and
+     * it has the same {@link #getKeys() keys} as this path, {@code false}
+     * otherwise.
+     *
+     * @param obj the object to be tested for equality. This argument might
+     *   be {@code null}, in which case the return value is {@code false}.
+     * @return {@code true} if the passed object is a {@code ConfigPath} and
+     *   it has the same {@link #getKeys() keys} as this path, {@code false}
+     *   otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) return false;
@@ -101,6 +211,13 @@ public final class ConfigPath {
         return Arrays.equals(this.keys, other.keys);
     }
 
+    /**
+     * Returns the string representation of this {@code ConfigPath} in no
+     * particular format. The return value is intended to be used for debugging.
+     *
+     * @return the string representation of this {@code ConfigPath} in no
+     *   particular format. This method never returns {@code null}.
+     */
     @Override
     public String toString() {
         return Arrays.toString(keys);
