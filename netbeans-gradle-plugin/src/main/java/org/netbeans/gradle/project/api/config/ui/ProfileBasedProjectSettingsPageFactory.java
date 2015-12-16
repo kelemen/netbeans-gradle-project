7a527e47f00e97a44843d852c110a1c96848ea33
Attila Kelemen <attila.kelemen85@gmail.com>
2015-12-16 22:29:27 +0100

    Documented ProfileBasedProjectSettingsPageFactory.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ui/ProfileBasedProjectSettingsPageFactory.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ui/ProfileBasedProjectSettingsPageFactory.java
@@ -1,5 +1,21 @@
 package org.netbeans.gradle.project.api.config.ui;
 
+/**
+ * Defines a factory to create the component and to display the editor of the
+ * properties and the logic of setting the properties in the settings.
+ *
+ * @see ProfileBasedConfigurations
+ */
 public interface ProfileBasedProjectSettingsPageFactory {
+    /**
+     * Creates a new project properties category page which is able to adjust
+     * configuration of specific profiles.
+     * <P>
+     * This method is always called on the <I>Event Dispatch Thread</I>.
+     *
+     * @return a new project properties category page which is able to adjust
+     *   configuration of specific profiles. This method may never return
+     *   {@code null}.
+     */
     public ProfileBasedProjectSettingsPage createSettingsPage();
 }
