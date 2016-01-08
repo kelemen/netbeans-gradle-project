8b94338531af3916618c32ae8352739de832f598
Attila Kelemen <attila.kelemen85@gmail.com>
2016-01-08 17:52:43 +0100

    Do not write to the output tab after it has been closed.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/output/IOTabRef.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/output/IOTabRef.java
@@ -1,10 +1,11 @@
 package org.netbeans.gradle.project.output;
 
 import java.io.Closeable;
+import java.io.IOException;
 
 public interface IOTabRef<IOTab> extends Closeable {
     public IOTab getTab();
 
     @Override
-    public void close();
+    public void close() throws IOException;
 }
