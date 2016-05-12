2f02b2b9f46a67a330ca3881acc99b364d9eb7d8
Attila Kelemen <attila.kelemen85@gmail.com>
2016-05-12 20:52:14 +0200

    The name of jar artifacts are now used to guess project dependencies.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/query/ProjectClassPathResourceBuilder.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/java/query/ProjectClassPathResourceBuilder.java
@@ -2,6 +2,7 @@ package org.netbeans.gradle.project.java.query;
 
 import java.io.File;
 import java.net.URL;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -10,15 +11,23 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jtrim.collections.CollectionsEx;
 import org.jtrim.utils.ExceptionHelper;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ui.OpenProjects;
+import org.netbeans.gradle.model.ProjectId;
 import org.netbeans.gradle.model.java.JavaOutputDirs;
 import org.netbeans.gradle.model.java.JavaSourceGroup;
 import org.netbeans.gradle.model.java.JavaSourceSet;
+import org.netbeans.gradle.project.NbGradleProject;
 import org.netbeans.gradle.project.api.entry.ProjectPlatform;
+import org.netbeans.gradle.project.java.JavaExtension;
 import org.netbeans.gradle.project.java.model.JavaProjectReference;
 import org.netbeans.gradle.project.java.model.NbJavaModel;
 import org.netbeans.gradle.project.java.model.NbJavaModule;
@@ -26,6 +35,7 @@ import org.netbeans.gradle.project.util.ExcludeIncludeRules;
 import org.netbeans.spi.java.classpath.PathResourceImplementation;
 import org.netbeans.spi.java.classpath.support.ClassPathSupport;
 import org.openide.filesystems.FileUtil;
+import org.openide.util.Lookup;
 
 // TODO: Optimize this class by merging equivalent class path lists into a single one (or maybe concat lists?)
 public final class ProjectClassPathResourceBuilder {
@@ -37,6 +47,8 @@ public final class ProjectClassPathResourceBuilder {
     private Set<File> missing;
     private Map<ClassPathKey, List<PathResourceImplementation>> classpathResources;
 
+    private Map<ProjectArtifactId, File> openedProjectsOutput;
+
     public ProjectClassPathResourceBuilder(NbJavaModel projectModel, ProjectPlatform currentPlatform) {
         ExceptionHelper.checkNotNullArgument(projectModel, "projectModel");
         ExceptionHelper.checkNotNullArgument(currentPlatform, "currentPlatform");
@@ -45,9 +57,11 @@ public final class ProjectClassPathResourceBuilder {
         this.currentPlatform = currentPlatform;
         this.classpathResources = null;
         this.missing = null;
+        this.openedProjectsOutput = null;
     }
 
     public void build() {
+        openedProjectsOutput = findOpenedProjectsOutput();
         classpathResources = new HashMap<>();
         missing = new HashSet<>();
 
@@ -64,6 +78,52 @@ public final class ProjectClassPathResourceBuilder {
         loadRuntimeForGlobalClassPath(projectModel);
     }
 
+    /**
+     * Returns all the currently opened Gradle Java projects, including the projects
+     * referenced by them (project dependencies).
+     */
+    private static Collection<JavaExtension> getAllOpenedGradleJavaProjects() {
+        Project[] openedProjects = OpenProjects.getDefault().getOpenProjects();
+        Map<Path, JavaExtension> result = new HashMap<>();
+
+        for (Project project: openedProjects) {
+            Lookup lookup = project.getLookup();
+            NbGradleProject gradleProject = lookup.lookup(NbGradleProject.class);
+            JavaExtension javaExt = lookup.lookup(JavaExtension.class);
+
+            if (javaExt != null && gradleProject != null) {
+                result.put(gradleProject.getProjectDirectoryAsPath(), javaExt);
+
+                for (JavaProjectReference projectRef: javaExt.getCurrentModel().getAllDependencies()) {
+                    Project dependency = projectRef.tryGetProject();
+                    if (dependency != null) {
+                        JavaExtension dependencyJavaExt = dependency.getLookup().lookup(JavaExtension.class);
+                        if (dependencyJavaExt != null) {
+                            result.put(dependencyJavaExt.getProjectDirectoryAsFile().toPath(), dependencyJavaExt);
+                        }
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(result.values());
+    }
+
+    private static Map<ProjectArtifactId, File> findOpenedProjectsOutput() {
+        // TODO: Check if this feature is enabled in the global settings.
+        //       If not, return null.
+        Collection<JavaExtension> javaExts = getAllOpenedGradleJavaProjects();
+        Map<ProjectArtifactId, File> result = CollectionsEx.newHashMap(javaExts.size());
+        for (JavaExtension javaExt: javaExts) {
+            NbJavaModule mainModule = javaExt.getCurrentModel().getMainModule();
+            File outputDir = mainModule.getMainSourceSet().getOutputDirs().getClassesDir();
+
+            ProjectId projectId = mainModule.getProperties().getProjectId();
+            result.put(new ProjectArtifactId(projectId.getName(), projectId.getVersion()), outputDir);
+        }
+        return result;
+    }
+
     private static List<PathResourceImplementation> getBuildOutputDirsAsPathResources(JavaSourceSet sourceSet) {
         JavaOutputDirs outputDirs = sourceSet.getOutputDirs();
         PathResourceImplementation classesDir = toPathResource(outputDirs.getClassesDir());
@@ -75,15 +135,54 @@ public final class ProjectClassPathResourceBuilder {
         return result;
     }
 
+    private File updateDependency(File original) {
+        if (openedProjectsOutput == null) {
+            return original;
+        }
+
+        File translated = openedProjectsOutput.get(new ProjectArtifactId(original.getName()));
+        return translated != null ? translated : original;
+    }
+
+    private Collection<File> updateDependencies(Collection<File> original) {
+        if (openedProjectsOutput == null) {
+            return original;
+        }
+
+        boolean changed = false;
+        Collection<File> result = new ArrayList<>(original.size());
+        for (File file: original) {
+            File translated = updateDependency(file);
+            if (translated != file) {
+                // Reference comparison is fine because updateDependency
+                // will return the same reference if it cannot update the
+                // dependency.
+                changed = true;
+                System.out.println("Replacing dependency " + file + " with " + translated);
+            }
+            result.add(translated);
+        }
+
+        return changed ? result : original;
+    }
+
+    private Collection<File> getFixedCompileClasspaths(JavaSourceSet sourceSet) {
+        return updateDependencies(sourceSet.getClasspaths().getCompileClasspaths());
+    }
+
+    private Collection<File> getFixedRuntimeClasspaths(JavaSourceSet sourceSet) {
+        return updateDependencies(sourceSet.getClasspaths().getRuntimeClasspaths());
+    }
+
     private void loadCompilePathResources(JavaSourceSet sourceSet, Set<File> invalid) {
-        Set<File> compileCP = sourceSet.getClasspaths().getCompileClasspaths();
+        Collection<File> compileCP = getFixedCompileClasspaths(sourceSet);
         setClassPathResources(
                 new SourceSetClassPathType(sourceSet.getName(), ClassPathType.COMPILE),
                 getPathResources(compileCP, invalid));
     }
 
     private void loadRuntimePathResources(JavaSourceSet sourceSet, Set<File> invalid) {
-        Set<File> runtimeCP = sourceSet.getClasspaths().getRuntimeClasspaths();
+        Collection<File> runtimeCP = getFixedRuntimeClasspaths(sourceSet);
         setClassPathResources(
                 new SourceSetClassPathType(sourceSet.getName(), ClassPathType.RUNTIME),
                 getPathResources(runtimeCP, invalid),
@@ -124,7 +223,7 @@ public final class ProjectClassPathResourceBuilder {
 
         for (JavaSourceSet sourceSet: mainModule.getSources()) {
             classPaths.add(sourceSet.getOutputDirs().getClassesDir());
-            classPaths.addAll(sourceSet.getClasspaths().getRuntimeClasspaths());
+            classPaths.addAll(getFixedRuntimeClasspaths(sourceSet));
         }
 
         setClassPathResources(
@@ -136,6 +235,7 @@ public final class ProjectClassPathResourceBuilder {
         Set<File> classPaths = new HashSet<>();
 
         for (JavaSourceSet sourceSet: projectModel.getMainModule().getSources()) {
+            // There is no reason to update the classpath for the global registry.
             classPaths.addAll(sourceSet.getClasspaths().getRuntimeClasspaths());
         }
 
@@ -150,6 +250,7 @@ public final class ProjectClassPathResourceBuilder {
         Set<File> classPaths = new HashSet<>();
 
         for (JavaSourceSet sourceSet: projectModel.getMainModule().getSources()) {
+            // There is no reason to update the classpath for the global registry.
             classPaths.addAll(sourceSet.getClasspaths().getCompileClasspaths());
         }
 
@@ -247,6 +348,22 @@ public final class ProjectClassPathResourceBuilder {
         return result;
     }
 
+    private static ProjectArtifactId tryExtractArtifactIdFromName(String fileName) {
+        int versionSepIndex = fileName.indexOf('-');
+        if (versionSepIndex < 0) {
+            return null;
+        }
+
+        final String expectedExtension = ".jar";
+        if (!fileName.toLowerCase(Locale.ROOT).endsWith(expectedExtension)) {
+            return null;
+        }
+
+        String artifactName = fileName.substring(0, versionSepIndex);
+        String version = fileName.substring(versionSepIndex + 1, fileName.length() - expectedExtension.length());
+        return new ProjectArtifactId(artifactName, version);
+    }
+
     private static void throwUnbuilt() {
         throw new IllegalStateException("Call build() first before calling this method.");
     }
@@ -312,6 +429,35 @@ public final class ProjectClassPathResourceBuilder {
 
             return this.sourceSetName.equals(other.sourceSetName)
                     && this.classPathType == other.classPathType;
+        }
+    }
+
+    private static final class ProjectArtifactId {
+        private final String expectedName;
+
+        public ProjectArtifactId(String name, String version) {
+            this(name + "-" + version + ".jar");
+        }
+
+        public ProjectArtifactId(String expectedName) {
+            this.expectedName = expectedName.toLowerCase(Locale.ROOT);
+        }
+
+        @Override
+        public int hashCode() {
+            int hash = 5;
+            hash = 29 * hash + Objects.hashCode(expectedName);
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+
+            final ProjectArtifactId other = (ProjectArtifactId)obj;
+            return Objects.equals(this.expectedName, other.expectedName);
         }
     }
 }
