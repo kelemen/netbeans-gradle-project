1c99fb75a36264412e7c625e9d3517b1d0faf332
Attila Kelemen <attila.kelemen85@gmail.com>
2016-09-16 11:42:24 +0200

    Added javadoc to StoredSettings.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ui/StoredSettings.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ui/StoredSettings.java
@@ -1,6 +1,16 @@
 package org.netbeans.gradle.project.api.config.ui;
 
+/**
+ * Defines an immutable snapshot of some settings entries. These settings are always
+ * associated with a {@link ProfileEditor}.
+ *
+ * @see ProfileEditor
+ */
 public interface StoredSettings {
+    /**
+     * Defines an implementation of {@code StoredSettings} whose methods do nothing but
+     * throw an {@code IllegalStateException}.
+     */
     public static final StoredSettings NOT_INITIALIZED = new StoredSettings() {
         @Override
         public void displaySettings() {
@@ -13,6 +23,17 @@ public interface StoredSettings {
         }
     };
 
+    /**
+     * Displays these settings on the associated UI component.
+     * <P>
+     * This method is always called on the <I>Event Dispatch Thread</I>.
+     */
     public void displaySettings();
+
+    /**
+     * Persists these settings into the associated profile.
+     * <P>
+     * This method might be called on any thread.
+     */
     public void saveSettings();
 }
