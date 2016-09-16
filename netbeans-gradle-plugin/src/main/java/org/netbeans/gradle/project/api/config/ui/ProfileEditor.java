da92a7cebd30cf46a42ba4af34df780367cfd7aa
Attila Kelemen <attila.kelemen85@gmail.com>
2016-09-16 11:42:29 +0200

    Added javadoc to ProfileEditor.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ui/ProfileEditor.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/api/config/ui/ProfileEditor.java
@@ -9,9 +9,26 @@ import javax.annotation.Nonnull;
  * @see ProfileEditorFactory
  */
 public interface ProfileEditor {
+    /**
+     * Reads the settings from the associated profile and stores them immutably in the
+     * returned {@code StoredSettings}.
+     * <P>
+     * This method can be called from any thread.
+     *
+     * @return the settings read from the associated profile. This method never returns {@code null}.
+     */
     @Nonnull
     public StoredSettings readFromSettings();
 
+    /**
+     * Reads the settings from the associated UI component and stores them immutably in the
+     * returned {@code StoredSettings}.
+     * <P>
+     * <P>
+     * This method is always called on the <I>Event Dispatch Thread</I>.
+     *
+     * @return the settings read from the associated UI component. This method never returns {@code null}.
+     */
     @Nonnull
     public StoredSettings readFromGui();
 }
