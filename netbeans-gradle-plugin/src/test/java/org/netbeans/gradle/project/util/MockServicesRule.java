5ac8f0e0379a67e6e9767233f15f6eb020828273
Attila Kelemen <attila.kelemen85@gmail.com>
2016-09-20 14:19:08 +0200

    Added MockServicesRule.

---
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/util/MockServicesRule.java
@@ -0,0 +1,27 @@
+package org.netbeans.gradle.project.util;
+
+import org.jtrim.utils.ExceptionHelper;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.netbeans.junit.MockServices;
+
+public final class MockServicesRule implements TestRule {
+    private final Class<?>[] services;
+
+    public MockServicesRule(Class<?>... services) {
+        this.services = services.clone();
+        ExceptionHelper.checkNotNullElements(this.services, "services");
+    }
+
+    @Override
+    public Statement apply(final Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                MockServices.setServices(services);
+                base.evaluate();
+            }
+        };
+    }
+}
