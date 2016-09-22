75c64c7b90a718b4aa619b173095b7293755fe6b
Attila Kelemen <attila.kelemen85@gmail.com>
2016-09-22 17:13:34 +0200

    Added utility class LazyValue.

---
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/util/LazyValue.java
@@ -0,0 +1,27 @@
+package org.netbeans.gradle.project.util;
+
+import java.util.concurrent.atomic.AtomicReference;
+import org.jtrim.utils.ExceptionHelper;
+
+public final class LazyValue<T> {
+    private final NbSupplier<? extends T> valueFactory;
+    private final AtomicReference<T> valueRef;
+
+    public LazyValue(NbSupplier<? extends T> valueFactory) {
+        ExceptionHelper.checkNotNullArgument(valueFactory, "valueFactory");
+
+        this.valueFactory = valueFactory;
+        this.valueRef = new AtomicReference<>(null);
+    }
+
+    public T get() {
+        T result = valueRef.get();
+        if (result == null) {
+            result = valueFactory.get();
+            if (!valueRef.compareAndSet(null, result)) {
+                result = valueRef.get();
+            }
+        }
+        return result;
+    }
+}
