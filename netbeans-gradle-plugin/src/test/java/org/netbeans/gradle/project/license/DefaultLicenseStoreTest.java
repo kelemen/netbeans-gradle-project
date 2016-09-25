d67732924b115955e8dda56e74c6f64c3564317a
Attila Kelemen <attila.kelemen85@gmail.com>
2016-09-25 23:58:54 +0200

    Added tests for DefaultLicenseStore.

---
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/license/DefaultLicenseStoreTest.java
@@ -0,0 +1,44 @@
+package org.netbeans.gradle.project.license;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.netbeans.gradle.project.util.TemporaryFileRule;
+
+import static org.junit.Assert.*;
+
+public class DefaultLicenseStoreTest {
+    @ClassRule
+    public static final TemporaryFileRule TMP_FILE = new TemporaryFileRule("TestLicenseContent");
+
+    @Test
+    public void testAddAndRemove() throws IOException {
+        Path src = TMP_FILE.getFile();
+
+        DefaultLicenseDef licenseDef = new DefaultLicenseDef(src, "TestLicense1", "TestDisplayName1");
+        DefaultLicenseStore store = new DefaultLicenseStore();
+
+        store.addLicense(licenseDef);
+        try {
+            assertTrue(store.containsLicense(licenseDef.getLicenseId()));
+        } finally {
+            store.removeLicense(licenseDef);
+        }
+
+        assertFalse(store.containsLicense(licenseDef.getLicenseId()));
+    }
+
+    @Test
+    public void testRemoveNonExistantDoesNotFail() throws IOException {
+        Path src = TMP_FILE.getFile();
+
+        DefaultLicenseDef licenseDef = new DefaultLicenseDef(src, "TestLicense2", "TestDisplayName2");
+
+        DefaultLicenseStore store = new DefaultLicenseStore();
+
+        assertFalse(store.containsLicense(licenseDef.getLicenseId()));
+        store.removeLicense(licenseDef);
+        assertFalse(store.containsLicense(licenseDef.getLicenseId()));
+    }
+}
