e1da8f0fa32825d1f39325069dccde8dd059bccc
Attila Kelemen <attila.kelemen85@gmail.com>
2016-09-27 11:01:55 +0200

    Added few more tests for LicenseHeaderPanel.

--- netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties/ui/LicenseHeaderPanelTest.java
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/properties/ui/LicenseHeaderPanelTest.java
@@ -3,6 +3,7 @@ package org.netbeans.gradle.project.properties.ui;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import org.junit.Test;
@@ -16,7 +17,6 @@ import org.netbeans.gradle.project.properties.global.CommonGlobalSettings;
 import org.netbeans.gradle.project.util.NbConsumer;
 import org.netbeans.gradle.project.util.NbSupplier;
 
-
 public class LicenseHeaderPanelTest {
     private static final NbSupplier<Path> NULL_PATH = new NbSupplier<Path>() {
         @Override
@@ -25,20 +25,28 @@ public class LicenseHeaderPanelTest {
         }
     };
 
-    private static final LicenseSource LICENSE_SOURCE = new LicenseSource() {
-        @Override
-        public Collection<LicenseRef> getAllLicense() throws IOException {
-            return Collections.emptyList();
-        }
-    };
+    private static final LicenseSource EMPTY_LICENSE_SOURCE = licenseSource(Collections.<LicenseRef>emptyList());
 
-    private static NbSupplier<ProfileBasedSettingsPage> settingsPageFactory() {
+    private static LicenseSource licenseSource(final Collection<LicenseRef> licenses) {
+        return new LicenseSource() {
+            @Override
+            public Collection<LicenseRef> getAllLicense() throws IOException {
+                return licenses;
+            }
+        };
+    }
+
+    private static NbSupplier<ProfileBasedSettingsPage> settingsPageFactory(final LicenseSource licenseSource) {
         return new NbSupplier<ProfileBasedSettingsPage>() {
             @Override
             public ProfileBasedSettingsPage get() {
-                return LicenseHeaderPanel.createSettingsPage(NULL_PATH, LICENSE_SOURCE);
+                return LicenseHeaderPanel.createSettingsPage(NULL_PATH, licenseSource);
             }
         };
+    }
+
+    private static NbSupplier<ProfileBasedSettingsPage> settingsPageFactory() {
+        return settingsPageFactory(EMPTY_LICENSE_SOURCE);
     }
 
     private static PropertyReference<LicenseHeaderInfo> licenseHeaderInfo(CommonGlobalSettings input) {
@@ -73,5 +81,33 @@ public class LicenseHeaderPanelTest {
                 "MyLicense",
                 Collections.singletonMap("organization", "MyTestOrg"),
                 null));
+    }
+
+    private void testInitAndReadBackWithKnownLicense(boolean dynamic) throws Exception {
+        licenseSource(Arrays.asList(
+                new LicenseRef("AAAA", "DisplayNameOfAAA", false),
+                new LicenseRef("MyBuiltInLicense", "DisplayNameOfLicense", dynamic),
+                new LicenseRef("ZZZZ", "DisplayNameOfZZZZ", false)
+        ));
+        GlobalSettingsPanelTestUtils.testGenericInitAndReadBack(settingsPageFactory(), new NbConsumer<CommonGlobalSettings>() {
+            @Override
+            public void accept(CommonGlobalSettings input) {
+                LicenseHeaderInfo headerInfo = new LicenseHeaderInfo(
+                        "MyBuiltInLicense",
+                        Collections.singletonMap("organization", "MyTestOrg"),
+                        null);
+                licenseHeaderInfo(input).setValue(headerInfo);
+            }
+        });
+    }
+
+    @Test
+    public void testInitAndReadBackWithKnownLicense1() throws Exception {
+        testInitAndReadBackWithKnownLicense(true);
+    }
+
+    @Test
+    public void testInitAndReadBackWithKnownLicense2() throws Exception {
+        testInitAndReadBackWithKnownLicense(false);
     }
 }
