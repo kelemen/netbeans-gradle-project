872feabb4feb8ad6ea7c3a388849ffb4f6fbaf11
Attila Kelemen <attila.kelemen85@gmail.com>
2016-09-27 21:51:00 +0200

    Moved all StringUtilsTest to the proper test class.

--- netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/util/StringUtilsTest.java
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/util/StringUtilsTest.java
@@ -72,4 +72,130 @@ public class StringUtilsTest {
     public void testSplitArgsQutedWithEscapes() {
         testSplitArgs("\" my\\\"arg1 \" \"my\\Xarg2\"", " my\"arg1 ", "myXarg2");
     }
+
+    @Test
+    public void testUnescapedIndexOfWithoutEscapeChar() {
+        assertEquals(0, StringUtils.unescapedIndexOf(":", 0, ':'));
+        assertEquals(0, StringUtils.unescapedIndexOf(":hello", 0, ':'));
+        assertEquals(0, StringUtils.unescapedIndexOf(":he:llo", 0, ':'));
+        assertEquals(5, StringUtils.unescapedIndexOf("hello:", 0, ':'));
+        assertEquals(3, StringUtils.unescapedIndexOf(":he:llo", 1, ':'));
+    }
+
+    @Test
+    public void testUnescapedIndexOfSkipsEscaped1() {
+        assertEquals(8, StringUtils.unescapedIndexOf("0123\\:67:9", 0, ':'));
+    }
+
+    @Test
+    public void testUnescapedIndexOfSkipsEscaped2() {
+        assertEquals(10, StringUtils.unescapedIndexOf("0123\\\\\\:89:X", 0, ':'));
+    }
+
+    @Test
+    public void testUnescapedIndexOfEscapedSlashBeforeMatch() {
+        assertEquals(6, StringUtils.unescapedIndexOf("0123\\\\:78", 0, ':'));
+    }
+
+    @Test
+    public void testUnescapedIndexOfNotFound() {
+        assertEquals(-1, StringUtils.unescapedIndexOf("012378", 0, ':'));
+        assertEquals(-1, StringUtils.unescapedIndexOf("", 0, ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitSimple() {
+        assertArrayEquals(
+                new String[]{"one", "two"},
+                StringUtils.unescapedSplit("one:two", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitEmptyEnd() {
+        assertArrayEquals(
+                new String[]{"one", "two", ""},
+                StringUtils.unescapedSplit("one:two:", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitEmptyStart() {
+        assertArrayEquals(
+                new String[]{"", "one", "two"},
+                StringUtils.unescapedSplit(":one:two", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitNoSplit() {
+        assertArrayEquals(
+                new String[]{"single"},
+                StringUtils.unescapedSplit("single", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitEmpty() {
+        assertArrayEquals(
+                new String[]{""},
+                StringUtils.unescapedSplit("", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitWithEscaped1() {
+        assertArrayEquals(
+                new String[]{"one", "two\\:three"},
+                StringUtils.unescapedSplit("one:two\\:three", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitWithEscaped2() {
+        assertArrayEquals(
+                new String[]{"one", "two\\\\\\:three"},
+                StringUtils.unescapedSplit("one:two\\\\\\:three", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitSimpleWithEscapedEscape() {
+        assertArrayEquals(
+                new String[]{"one\\\\", "two"},
+                StringUtils.unescapedSplit("one\\\\:two", ':'));
+    }
+
+    @Test
+    public void testUnescapedSplitLimitedToOne() {
+        assertArrayEquals(
+                new String[]{"one:two:three"},
+                StringUtils.unescapedSplit("one:two:three", ':', 1));
+    }
+
+    @Test
+    public void testUnescapedSplitLimitedSimple() {
+        assertArrayEquals(
+                new String[]{"one", "two:three"},
+                StringUtils.unescapedSplit("one:two:three", ':', 2));
+    }
+
+    @Test
+    public void testUnescapedSplitLimitedEndsWithSeparator1() {
+        assertArrayEquals(
+                new String[]{"one", ""},
+                StringUtils.unescapedSplit("one:", ':', 2));
+    }
+
+    @Test
+    public void testUnescapedSplitLimitedEndsWithSeparator2() {
+        assertArrayEquals(
+                new String[]{"one", "two:"},
+                StringUtils.unescapedSplit("one:two:", ':', 2));
+    }
+
+    @Test
+    public void testReplaceLFWithPreferredLineSeparator1() {
+        String sep = System.getProperty("line.separator");
+        String str = "\nFirst Line\nSecond Line\nThird Line";
+        assertEquals(str.replace("\n", sep), StringUtils.replaceLFWithPreferredLineSeparator(str));
+    }
+
+    @Test
+    public void testReplaceLFWithPreferredLineSeparator2() {
+        assertEquals("", StringUtils.replaceLFWithPreferredLineSeparator(""));
+    }
 }
