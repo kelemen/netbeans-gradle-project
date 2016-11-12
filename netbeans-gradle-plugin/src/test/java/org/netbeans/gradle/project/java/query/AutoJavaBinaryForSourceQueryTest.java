5dd8dde2363be10d5d9498193a43ec26419aae4b
Attila Kelemen <attila.kelemen85@gmail.com>
2016-11-12 17:36:03 +0100

    Added tests for AutoJavaBinaryForSourceQuery.

---
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/java/query/AutoJavaBinaryForSourceQueryTest.java
@@ -0,0 +1,52 @@
+package org.netbeans.gradle.project.java.query;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.netbeans.api.java.queries.BinaryForSourceQuery;
+import org.netbeans.gradle.project.util.TestBinaryUtils;
+import org.netbeans.spi.java.queries.BinaryForSourceQueryImplementation;
+import org.openide.util.Utilities;
+
+import static org.junit.Assert.*;
+import static org.netbeans.gradle.project.query.TestSourceQueryUtils.*;
+
+public class AutoJavaBinaryForSourceQueryTest {
+    @ClassRule
+    public static final TemporaryFolder TMP_DIR_ROOT = new TemporaryFolder();
+
+    @Test
+    public void testFindBinaryOfZipSource() throws IOException {
+        BinaryForSourceQueryImplementation query = new AutoJavaBinaryForSourceQuery();
+
+        File rootDir = TMP_DIR_ROOT.newFolder();
+
+        File jarPath = new File(rootDir, "myapp.jar");
+        TestBinaryUtils.createTestJar(jarPath);
+
+        File sourcesPath = new File(rootDir, "myapp-sources.zip");
+        TestBinaryUtils.createTestZip(sourcesPath);
+
+        URL sourceUrl = Utilities.toURI(sourcesPath).toURL();
+
+        BinaryForSourceQuery.Result result = query.findBinaryRoots(sourceUrl);
+        assertNotNull("result1", result);
+        assertEquals("sourcesPath", jarPath, expectedSingleFile(result));
+    }
+
+    @Test
+    public void testNoBinary() throws IOException {
+        BinaryForSourceQueryImplementation query = new AutoJavaBinaryForSourceQuery();
+
+        File rootDir = TMP_DIR_ROOT.newFolder();
+
+        File sourcesPath = new File(rootDir, "myapp-sources.zip");
+        TestBinaryUtils.createTestZip(sourcesPath);
+
+        URL sourceUrl = Utilities.toURI(sourcesPath).toURL();
+        assertNull("result1", query.findBinaryRoots(sourceUrl));
+    }
+}
--- netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/query/TestSourceQueryUtils.java
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/query/TestSourceQueryUtils.java
@@ -3,6 +3,8 @@ package org.netbeans.gradle.project.query;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import org.netbeans.api.java.queries.BinaryForSourceQuery;
 import org.netbeans.api.java.queries.SourceForBinaryQuery;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
@@ -22,8 +24,17 @@ public final class TestSourceQueryUtils {
         assertEquals(name, expectedUri, actualUri);
     }
 
+    public static File expectedSingleFile(BinaryForSourceQuery.Result result) {
+        return expectedSingleFile(result.getRoots());
+    }
+
     public static FileObject expectedSingleFile(SourceForBinaryQuery.Result result) {
         return expectedSingleFile(result.getRoots());
+    }
+
+    public static File expectedSingleFile(URL[] urls) {
+        assertEquals("Expected exactly one url", 1, urls.length);
+        return FileUtil.archiveOrDirForURL(urls[0]);
     }
 
     public static FileObject expectedSingleFile(FileObject[] fileObjs) {
