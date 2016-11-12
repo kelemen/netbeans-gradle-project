ac1f9d335b42bed02bad330dd100f8cb7d77ab89
Attila Kelemen <attila.kelemen85@gmail.com>
2016-11-12 02:13:28 +0100

    Added tests for GradleSourceForBinaryQuery.

---
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/java/query/GradleSourceForBinaryQueryTest.java
@@ -0,0 +1,119 @@
+package org.netbeans.gradle.project.java.query;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.netbeans.api.java.queries.SourceForBinaryQuery;
+import org.netbeans.gradle.model.java.JavaSourceSet;
+import org.netbeans.gradle.project.java.model.NbJavaModule;
+import org.netbeans.gradle.project.util.JavaModelTestUtils;
+import org.netbeans.gradle.project.util.NbSupplier;
+import org.netbeans.spi.java.queries.SourceForBinaryQueryImplementation2;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.Utilities;
+
+import static org.junit.Assert.*;
+
+public class GradleSourceForBinaryQueryTest {
+    @ClassRule
+    public static final TemporaryFolder TMP_DIR_ROOT = new TemporaryFolder();
+
+    public GradleSourceForBinaryQueryTest() {
+    }
+
+    private NbSupplier<NbJavaModule> testModule(File rootDir) throws IOException {
+        final NbJavaModule module = JavaModelTestUtils.createModule(rootDir);
+        return new NbSupplier<NbJavaModule>() {
+            @Override
+            public NbJavaModule get() {
+                return module;
+            }
+        };
+    }
+
+    private void verifyAllClassesDirHaveSources(SourceForBinaryQueryImplementation2 query, NbJavaModule module) throws IOException {
+        for (JavaSourceSet sourceSet: module.getSources()) {
+            try {
+                verifyAllClassesDirHaveSources(query, sourceSet);
+            } catch (Throwable ex) {
+                throw new AssertionError("Test failed for source set: " + sourceSet.getName(), ex);
+            }
+        }
+    }
+
+    private static Set<File> toFiles(FileObject[] objs) {
+        Set<File> result = new HashSet<>();
+        for (FileObject obj: objs) {
+            File file = FileUtil.toFile(obj);
+            if (file == null) {
+                throw new AssertionError(obj.getPath() + " does not exist");
+            }
+            result.add(file);
+        }
+        return result;
+    }
+
+    private void verifyAllClassesDirHaveSources(SourceForBinaryQueryImplementation2 query, JavaSourceSet sourceSet) throws IOException {
+        Set<File> expectedSourceRoots = JavaModelTestUtils.getAllSourceDirs(sourceSet);
+
+        File classesDir = sourceSet.getOutputDirs().getClassesDir();
+        verifySourceRoots(query, classesDir, expectedSourceRoots);
+        verifySourceRoots(query, new File(classesDir, "subdir"), expectedSourceRoots);
+    }
+
+    private void verifySourceRoots(
+            SourceForBinaryQueryImplementation2 query,
+            File binaryDir,
+            Set<File> expectedSourceRoots) throws IOException {
+
+        URL binaryDirUrl = Utilities.toURI(binaryDir).toURL();
+
+        SourceForBinaryQuery.Result queryResult = query.findSourceRoots(binaryDirUrl);
+        assertNotNull("result for classes dir", queryResult);
+        assertEquals("srcDirs", expectedSourceRoots, toFiles(queryResult.getRoots()));
+
+        SourceForBinaryQueryImplementation2.Result queryResult2 = query.findSourceRoots2(binaryDirUrl);
+        assertNotNull("result2 for classes dir", queryResult2);
+        assertEquals("srcDirs", expectedSourceRoots, toFiles(queryResult2.getRoots()));
+        assertTrue("prefer sources", queryResult2.preferSources());
+    }
+
+    @Test
+    public void testSourcesForClassesDirs() throws IOException {
+        File rootDir = TMP_DIR_ROOT.newFolder();
+        NbSupplier<NbJavaModule> moduleRef = testModule(rootDir);
+        NbJavaModule module = moduleRef.get();
+
+        GradleSourceForBinaryQuery query = new GradleSourceForBinaryQuery(moduleRef);
+        verifyAllClassesDirHaveSources(query, module);
+    }
+
+    private void verifyDoesNotHaveSourceRoot(
+            SourceForBinaryQueryImplementation2 query,
+            File dir) throws IOException {
+
+        URL dirUrl = Utilities.toURI(dir).toURL();
+
+        SourceForBinaryQuery.Result queryResult = query.findSourceRoots(dirUrl);
+        assertNull("result for classes dir", queryResult);
+
+        SourceForBinaryQueryImplementation2.Result queryResult2 = query.findSourceRoots2(dirUrl);
+        assertNull("result2 for classes dir", queryResult2);
+    }
+
+    @Test
+    public void verifyDoesNotHaveSourceRoot() throws IOException {
+        File rootDir = TMP_DIR_ROOT.newFolder();
+        NbSupplier<NbJavaModule> moduleRef = testModule(rootDir);
+        NbJavaModule module = moduleRef.get();
+
+        GradleSourceForBinaryQuery query = new GradleSourceForBinaryQuery(moduleRef);
+        verifyDoesNotHaveSourceRoot(query, module.getModuleDir());
+    }
+}
---
+++ netbeans-gradle-plugin/src/test/java/org/netbeans/gradle/project/util/JavaModelTestUtils.java
@@ -0,0 +1,104 @@
+package org.netbeans.gradle.project.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.netbeans.gradle.model.GenericProjectProperties;
+import org.netbeans.gradle.model.java.JavaCompatibilityModel;
+import org.netbeans.gradle.model.java.JavaOutputDirs;
+import org.netbeans.gradle.model.java.JavaSourceGroup;
+import org.netbeans.gradle.model.java.JavaSourceGroupName;
+import org.netbeans.gradle.model.java.JavaSourceSet;
+import org.netbeans.gradle.model.java.JavaTestModel;
+import org.netbeans.gradle.model.java.JavaTestTask;
+import org.netbeans.gradle.model.util.BasicFileUtils;
+import org.netbeans.gradle.project.java.model.NbCodeCoverage;
+import org.netbeans.gradle.project.java.model.NbJarOutput;
+import org.netbeans.gradle.project.java.model.NbJavaModule;
+import org.netbeans.gradle.project.java.model.NbListedDir;
+
+public final class JavaModelTestUtils {
+    private static JavaSourceSet createDefaultSourceSet(File rootDir, String name) throws IOException {
+        File srcRoot = BasicFileUtils.getSubPath(rootDir, "src", name, "java");
+        File srcClassDir = BasicFileUtils.getSubPath(srcRoot, "testpckg", name);
+        srcClassDir.mkdirs();
+
+        String packageName = "testpckg." + name;
+        String className = "TestClass" + name;
+        File srcClassFile = new File(srcClassDir, className + ".java");
+
+        List<String> lines = Arrays.asList(
+                "package " + packageName + ";",
+                "public class " + className + " { }");
+        Files.write(srcClassFile.toPath(), lines, StandardCharsets.UTF_8);
+
+        File buildDir = new File(rootDir, "build");
+        File classesDir = new File(buildDir, "classes");
+        File resourcesDir = new File(buildDir, "resources");
+
+        JavaSourceSet.Builder result = new JavaSourceSet.Builder(name, new JavaOutputDirs(
+                new File(classesDir, name),
+                new File(resourcesDir, name),
+                Collections.<File>emptyList()));
+        result.addSourceGroup(new JavaSourceGroup(JavaSourceGroupName.JAVA, Collections.singletonList(srcRoot)));
+
+        return result.create();
+    }
+
+    private static List<NbJarOutput> createJarOutputs(File rootDir, Collection<JavaSourceSet> sources) {
+        File buildDir = new File(rootDir, "build");
+        File jarDir = new File(buildDir, "jars");
+
+        List<NbJarOutput> result = new ArrayList<>(sources.size());
+        for (JavaSourceSet sourceSet: sources) {
+            File jar = new File(jarDir, sourceSet.getName() + ".jar");
+            File classesDir = sourceSet.getOutputDirs().getClassesDir();
+            result.add(new NbJarOutput(sourceSet.getName() + "Jar", jar, Collections.singletonList(classesDir)));
+        }
+        return result;
+    }
+
+    public static NbJavaModule createModule(File rootDir) throws IOException {
+        GenericProjectProperties properties = new GenericProjectProperties(
+                "testProject",
+                ":parent:testProject",
+                rootDir);
+        JavaCompatibilityModel compatibilityModel = new JavaCompatibilityModel("1.8", "1.8");
+        Collection<JavaSourceSet> sources = Arrays.asList(
+                createDefaultSourceSet(rootDir, "main"),
+                createDefaultSourceSet(rootDir, "customSourceSet"),
+                createDefaultSourceSet(rootDir, "test"));
+        List<NbListedDir> listedDirs = Collections.emptyList();
+        List<NbJarOutput> jarOutputs = createJarOutputs(rootDir, sources);
+        JavaTestModel testTasks = new JavaTestModel(Collections.<JavaTestTask>emptyList());
+
+        return new NbJavaModule(
+                properties,
+                compatibilityModel,
+                sources,
+                listedDirs,
+                jarOutputs,
+                testTasks,
+                NbCodeCoverage.NO_CODE_COVERAGE);
+    }
+
+    public static Set<File> getAllSourceDirs(JavaSourceSet sourceSet) {
+        Set<File> result = new HashSet<>();
+        for (JavaSourceGroup group: sourceSet.getSourceGroups()) {
+            result.addAll(group.getSourceRoots());
+        }
+        return result;
+    }
+
+    private JavaModelTestUtils() {
+        throw new AssertionError();
+    }
+}
