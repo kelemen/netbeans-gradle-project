1fe41c7ff79f8b05a2bac2c396229416276c81a2
Attila Kelemen <attila.kelemen85@gmail.com>
2018-12-28 20:26:24 +0100

    Fixes possible NPE when reporting error.

--- netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/model/issue/ModelLoadIssueReporter.java
+++ netbeans-gradle-plugin/src/main/java/org/netbeans/gradle/project/model/issue/ModelLoadIssueReporter.java
@@ -265,7 +265,11 @@ public final class ModelLoadIssueReporter {
             cause = cause.getCause();
         }
 
-        return Exceptions.getActualMessage(rootCause)
+        String message = Exceptions.getActualMessage(rootCause);
+        if (message == null) {
+            return "?";
+        }
+        return message
                 .replace("\r\n", " ")
                 .replace('\r', ' ')
                 .replace('\n', ' ');
